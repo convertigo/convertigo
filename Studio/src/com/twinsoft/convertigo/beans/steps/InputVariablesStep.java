866c289c13be6c5073fd3459f47a4e8bc1802fd4
Jibril Khedimallah <jibrilk@convertigo.com>
2011-10-19 09:37:58 +0000

    close #247 - Second implementation of "!InputVars" step. Please let me now if the name of the step or if the implementation behavior is wrong.

--- Studio/src/com/twinsoft/convertigo/beans/steps/InputVariablesStep.java
+++ Studio/src/com/twinsoft/convertigo/beans/steps/InputVariablesStep.java
@@ -1,19 +1,21 @@
 package com.twinsoft.convertigo.beans.steps;
 
-import java.util.List;
-import java.util.Vector;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.NativeJavaArray;
+import org.mozilla.javascript.Scriptable;
+import org.mozilla.javascript.Undefined;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
-import com.twinsoft.convertigo.beans.core.DatabaseObject;
-import com.twinsoft.convertigo.beans.core.Sequence;
+import com.twinsoft.convertigo.beans.common.XMLVector;
 import com.twinsoft.convertigo.beans.core.Step;
 import com.twinsoft.convertigo.beans.core.StepSource;
-import com.twinsoft.convertigo.beans.core.Variable;
-import com.twinsoft.convertigo.beans.variables.RequestableMultiValuedVariable;
 import com.twinsoft.convertigo.beans.variables.RequestableVariable;
+import com.twinsoft.convertigo.engine.Engine;
 import com.twinsoft.convertigo.engine.EngineException;
 import com.twinsoft.convertigo.engine.util.GenericUtils;
 
@@ -24,7 +26,8 @@ public class InputVariablesStep extends Step {
 	protected String expression = "";
 	protected String nodeName = "inputVars";
 	protected String nodeText = "";
-	protected Sequence parentSequence = null;
+	
+	transient Map<String, Object> variables = new HashMap<String, Object>(); 
 	
 	public InputVariablesStep() throws EngineException {
 		super();
@@ -74,35 +77,80 @@ public class InputVariablesStep extends Step {
 		return null;
 	}
 	
+	
+	
+	@Override
+	protected boolean stepExcecute(Context javascriptContext, Scriptable scope) throws EngineException {
+		variables.clear();
+
+		if (isEnable) {
+			for(RequestableVariable var : getParentSequence().getAllVariables()) {
+				try {
+					evaluate(javascriptContext, scope, var.getName(), "expression", true);
+					if (evaluated != null && !(evaluated instanceof Undefined)) {
+						
+						if (evaluated instanceof NativeJavaArray) {
+							NativeJavaArray jsArray = (NativeJavaArray) evaluated; 
+							Object javaArray = jsArray.unwrap();
+							
+							if (javaArray.getClass().isArray()) {
+								String[] objects = (String[]) javaArray;
+								variables.put(var.getName(), objects);
+							}
+						}
+						else {
+							variables.put(var.getName(), evaluated);
+						}						
+					}
+				}
+				catch (Exception e) {
+					evaluated = null;
+					Engine.logBeans.warn(e.getMessage());
+				}
+			}
+			return super.stepExcecute(javascriptContext, scope);
+		}
+		return false;
+	}
+
 	protected void createStepNodeValue(Document doc, Element stepNode) throws EngineException {
 		String nodeValue = nodeText;
-		Vector<String> nodeValues = null;
-		
-		Sequence parentSequence = this.getParentSequence();
-		List<DatabaseObject> childrens = parentSequence.getAllChildren();
-		
+		XMLVector<Object> nodeValues = null;
+		String [] sNodevalues = null;
+
 		Element var = null;
 		Node text = null;
 
-		for (DatabaseObject child: childrens) {
-			if (child instanceof Variable) {
-				if (child instanceof RequestableMultiValuedVariable) {
-					nodeValues = GenericUtils.cast(((Variable) child).getValueOrNull());
-					for (String value: nodeValues) {
-						var = doc.createElement(child.getName());
-						text = doc.createTextNode(value);
-						var.appendChild(text);
-						stepNode.appendChild(var);
-					}
-				}
-				else if (child instanceof RequestableVariable) {
-					nodeValue = ((Variable) child).getValueOrNull().toString();
-					var = doc.createElement(child.getName());
-					text = doc.createTextNode(nodeValue);
+		for(Map.Entry<String, Object> entry : variables.entrySet()){
+		   String key = entry.getKey();
+		   Object value = entry.getValue();
+
+		   if (value instanceof XMLVector) {
+			   	nodeValues = GenericUtils.cast(value);
+			   	
+			   	for (Object object :nodeValues) {
+					var = doc.createElement(key.toString());
+					text = doc.createTextNode(object.toString());
 					var.appendChild(text);
 					stepNode.appendChild(var);
-				}
-			}
+			   	}
+		   }
+		   else if (value instanceof String[]) {
+			   sNodevalues = GenericUtils.cast(value);
+			   for (Object object :sNodevalues) {
+					var = doc.createElement(key.toString());
+					text = doc.createTextNode(object.toString());
+					var.appendChild(text);
+					stepNode.appendChild(var);
+			   	}	   
+		   }
+		   else {
+			   	nodeValue = value.toString();
+				var = doc.createElement(key.toString());
+				text = doc.createTextNode(nodeValue);
+				var.appendChild(text);
+				stepNode.appendChild(var);
+		   }
 		}
-	}
+	}	
 }
