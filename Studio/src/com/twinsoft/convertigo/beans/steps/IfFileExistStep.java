3c8596aedc2cccfdbf417f8fcc73e0364c747a53
Jibril Khedimallah <jibrilk@convertigo.com>
2011-10-24 09:47:12 +0000

    close #2454 - Added !IfFileExists step. This step tests if the file or directory exists.

---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/IfFileExistStep.java
@@ -0,0 +1,102 @@
+package com.twinsoft.convertigo.beans.steps;
+
+import java.io.File;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import com.twinsoft.convertigo.beans.common.XMLVector;
+import com.twinsoft.convertigo.beans.core.StepSource;
+import com.twinsoft.convertigo.engine.Engine;
+import com.twinsoft.convertigo.engine.EngineException;
+
+public class IfFileExistStep extends BlockStep {
+
+	private static final long serialVersionUID = 1615428447261700976L;
+	
+	private String sourcePath = "";		
+
+	protected XMLVector<XMLVector<String>> testDefinition = new XMLVector<XMLVector<String>>();
+	
+	public IfFileExistStep() {
+		super();
+	}
+	
+	public IfFileExistStep(String sourcePath) {
+		super(sourcePath);
+	}
+
+	public String toString() {
+		String text = this.getComment();
+		return "ifFileExists("+ (sourcePath.equals("")?"??":sourcePath) +")" + (!text.equals("") ? " // "+text:"");
+	}
+	
+	public String toJsString() {
+		String code = "";
+		if (!sourcePath.equals("")) {
+			code += " ifFileExists ("+ sourcePath +") {\n";
+			code += super.toString();
+			code += " \n}\n";
+		}
+		return code;
+	}
+
+	protected boolean workOnSource() {
+		return false;
+	}
+	
+	protected StepSource getSource() {
+		return null;
+	}
+	
+	protected boolean hasToEvaluateBeforeNextStep() throws EngineException {
+		return true;
+	}
+	
+	private String evaluateSourcePath(Context javascriptContext, Scriptable scope) throws EngineException {
+		return evaluateToString(javascriptContext, scope, sourcePath, "sourcePath", false);
+	}
+	
+	protected boolean executeNextStep(Context javascriptContext, Scriptable scope) throws EngineException {
+		if (isEnable) {
+			boolean test = evaluateStep(javascriptContext, scope);
+			return super.executeNextStep(test, javascriptContext, scope);
+		}
+		return false;
+	}
+	
+	@Override
+	protected boolean evaluateStep(Context javascriptContext, Scriptable scope) throws EngineException {
+		try {	
+			String sourceFilePath = getAbsoluteFilePath(evaluateSourcePath(javascriptContext, scope));
+
+			File sourcefile = new File(sourceFilePath);
+			
+			if (sourcefile.exists()) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		} catch (Exception e) {
+			setErrorStatus(true);
+            Engine.logBeans.error("An error occured while testing if the file or directory exists.", e);
+		}		       
+		return false;
+	}
+
+	protected String getAbsoluteFilePath(String entry) throws EngineException {
+		if (entry.equals(""))
+			throw new EngineException("Please fill the Source property field.");
+				
+		return Engine.theApp.filePropertyManager.getFilepathFromProperty(entry, getProject().getName());
+	}
+
+	public void setSourcePath(String sourcePath) {
+		this.sourcePath = sourcePath;
+	}
+
+	public String getSourcePath() {
+		return sourcePath;
+	}
+}
---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/IfFileExistStepBeanInfo.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: http://sourceus/svn/CEMS/trunk/Studio/src/com/twinsoft/convertigo/beans/steps/BlockStepBeanInfo.java $
+ * $Author: fabienb $
+ * $Revision: 28379 $
+ * $Date: 2011-09-27 11:38:59 +0200 (mar., 27 sept. 2011) $
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import java.beans.PropertyDescriptor;
+
+import com.twinsoft.convertigo.beans.core.MySimpleBeanInfo;
+
+public class IfFileExistStepBeanInfo extends MySimpleBeanInfo {
+    
+	public IfFileExistStepBeanInfo() {
+		try {
+			beanClass = IfFileExistStep.class;
+			additionalBeanClass = com.twinsoft.convertigo.beans.steps.BlockStep.class;
+
+			iconNameC16 = "/com/twinsoft/convertigo/beans/core/images/step_color_16x16.gif";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/core/images/step_color_32x32.gif";
+			
+			resourceBundle = java.util.ResourceBundle.getBundle("com/twinsoft/convertigo/beans/steps/res/IfFileExistStep");
+			
+			displayName = resourceBundle.getString("display_name");
+			shortDescription = resourceBundle.getString("short_description");
+
+			properties = new PropertyDescriptor[1];
+			
+			properties[0] = new PropertyDescriptor("sourcePath", beanClass, "getSourcePath", "setSourcePath");
+			properties[0].setExpert(true);
+			properties[0].setDisplayName(getExternalizedString("property.sourcePath.display_name"));
+	        properties[0].setShortDescription(getExternalizedString("property.sourcePath.short_description"));            
+	        properties[0].setValue("scriptable", Boolean.TRUE);		
+		}
+		catch(Exception e) {
+			com.twinsoft.convertigo.engine.Engine.logBeans.error("Exception with bean info; beanClass=" + beanClass.toString(), e);
+		}
+	}
+
+}
