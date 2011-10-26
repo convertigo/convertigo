c59b79f56c05504e5e9df1ea43d95543fa81fee5
Jibril Khedimallah <jibrilk@convertigo.com>
2011-10-26 13:09:40 +0000

    close #2454 - Added Create directory Step.

---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/CreateDirectoryStep.java
@@ -0,0 +1,109 @@
+package com.twinsoft.convertigo.beans.steps;
+
+import java.io.File;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import com.twinsoft.convertigo.beans.core.Step;
+import com.twinsoft.convertigo.beans.core.StepSource;
+import com.twinsoft.convertigo.engine.Engine;
+import com.twinsoft.convertigo.engine.EngineException;
+
+public class CreateDirectoryStep extends Step {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2873578590344942963L;
+	
+	private String destinationPath = "";
+	
+	private boolean createNonExistentParentDirectories = true;
+	
+	public CreateDirectoryStep() {
+		super();
+	}
+
+	private String evaluateDestinationPath(Context javascriptContext, Scriptable scope) throws EngineException {
+		return evaluateToString(javascriptContext, scope, destinationPath, "destinationPath", false);
+	}
+
+	@Override
+	protected boolean stepExcecute(Context javascriptContext, Scriptable scope) throws EngineException {
+		// TODO Auto-generated method stub
+		if (isEnable) {
+			if (super.stepExcecute(javascriptContext, scope)) {
+				try {
+					String destinationFilePath = getAbsoluteFilePath(evaluateDestinationPath(javascriptContext, scope));
+
+					File destinationFile = new File(destinationFilePath);
+					
+					boolean directoryCreated = false;
+					
+					if (isCreateNonExistentParentDirectories()) {
+						directoryCreated = destinationFile.mkdirs();
+					}
+					else {
+						directoryCreated = destinationFile.mkdir();
+					}
+
+					if (!directoryCreated) {
+						throw new Exception("An error occured while creating the directory.");
+					}
+		        } catch (NullPointerException e) {
+					setErrorStatus(true);
+		            Engine.logBeans.error("An error occured while creating the directory.", e);
+				} catch (Exception e) {
+					setErrorStatus(true);
+		            Engine.logBeans.error("An error occured while creating the directory.", e);
+				}		       
+		        return true;
+			}
+		}
+		return false;
+	}
+
+	protected String getAbsoluteFilePath(String entry) throws EngineException {
+		if (entry.equals(""))
+			throw new EngineException("Please fill the Destination property field.");
+				
+		return Engine.theApp.filePropertyManager.getFilepathFromProperty(entry, getProject().getName());
+	}
+
+	@Override
+	protected boolean workOnSource() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected StepSource getSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String toJsString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDestinationPath() {
+		return destinationPath;
+	}
+
+
+	public void setDestinationPath(String destinationPath) {
+		this.destinationPath = destinationPath;
+	}
+
+	public void setCreateNonExistentParentDirectories(
+			boolean createNonExistentParentDirectories) {
+		this.createNonExistentParentDirectories = createNonExistentParentDirectories;
+	}
+
+	public boolean isCreateNonExistentParentDirectories() {
+		return createNonExistentParentDirectories;
+	}
+}
