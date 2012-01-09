2dfc98e4bc86bf94dabb9f9ddba270b5d6d40e4e
Laetitia Mouloud <laetitiam@convertigo.com>
2012-01-09 15:20:51 +0000

    CLOSED #2657 * on Branches 6.0.x * create 2 icons for jsimplesource, jsource , 16x16 and 32x32 : jsource_16x16.gif and jsource_32x32.gif, jsimplesource_16x16.gif and jsimplesource_32x32.gif * rename the icon sequence js name : simple to jsimple : jsimple_16x16.gif and jsimple_32x32.gif * modify beanInfo files to write the good link to images.

--- Studio/src/com/twinsoft/convertigo/beans/steps/SimpleSourceStepBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/steps/SimpleSourceStepBeanInfo.java
@@ -33,8 +33,8 @@ public class SimpleSourceStepBeanInfo extends MySimpleBeanInfo {
 			beanClass = SimpleSourceStep.class;
 			additionalBeanClass = com.twinsoft.convertigo.beans.steps.SourceStep.class;
 
-			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/simple_16x16.gif";
-			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/simple_32x32.gif";
+			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/jsimplesource_16x16.gif";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/jsimplesource_32x32.gif";
 			
 			resourceBundle = java.util.ResourceBundle.getBundle("com/twinsoft/convertigo/beans/steps/res/SimpleSourceStep");
 			
