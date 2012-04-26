4feb81e1b2a94f4724792f562fe4d66cbea9e111
Fabien Balageas <fabienb@convertigo.com>
2012-04-26 13:37:14 +0000

    Fixed compilation issues

--- Studio/src/com/twinsoft/convertigo/eclipse/views/references/ViewRefLabelProvider.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/views/references/ViewRefLabelProvider.java
@@ -54,24 +54,19 @@ import com.twinsoft.convertigo.eclipse.views.references.model.TransactionStepNod
 
 class ViewRefLabelProvider implements ILabelProvider {
 
-	@Override
 	public void addListener(ILabelProviderListener listener) {
 	}
 
-	@Override
 	public void dispose() {
 	}
 
-	@Override
 	public boolean isLabelProperty(Object element, String property) {
 		return false;
 	}
 
-	@Override
 	public void removeListener(ILabelProviderListener listener) {
 	}
 
-	@Override
 	public Image getImage(Object element) {
 
 		String iconName = null;
@@ -135,7 +130,6 @@ class ViewRefLabelProvider implements ILabelProvider {
 		return image;
 	}
 	
-	@Override
 	public String getText(Object element) {
 		return ((AbstractNode) element).getName();
 	}
