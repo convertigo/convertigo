1b38d3f544e9559d760c752a2e13f4853b2d2e2f
Nicolas Albert <nicolasa@convertigo.com>
2012-07-11 11:23:28 +0000

    Ref #2910 - Enable HTML synchronizers to handle an alert response
    Ported in 6.1.x branch !

--- Studio/src/com/twinsoft/convertigo/eclipse/property_editors/HttpTriggerDocumentCompletedEditorComposite.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/property_editors/HttpTriggerDocumentCompletedEditorComposite.java
@@ -25,15 +25,16 @@ package com.twinsoft.convertigo.eclipse.property_editors;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
 
 import com.twinsoft.convertigo.engine.parsers.triggers.DocumentCompletedTrigger;
 import com.twinsoft.convertigo.engine.parsers.triggers.TriggerXMLizer;
 
-public class HttpTriggerDocumentCompletedEditorComposite extends AbstractHttpTriggerCustomEditorComposite{
-	private Label nbdoc_label = null;
-	private Spinner nbdoc_spin = null;
+public class HttpTriggerDocumentCompletedEditorComposite extends AbstractHttpTriggerCustomEditorComposite {
+	protected Spinner nbdoc_spin;
+	protected Button check;
 
 	public HttpTriggerDocumentCompletedEditorComposite(HttpTriggerEditorComposite parent) {
 		super(parent);
@@ -45,11 +46,11 @@ public class HttpTriggerDocumentCompletedEditorComposite extends AbstractHttpTri
 	 * 
 	 */
 	private void initialize() {
-		DocumentCompletedTrigger trigger = (parent.getTrigger() instanceof DocumentCompletedTrigger)?(DocumentCompletedTrigger)parent.getTrigger():null;
+		DocumentCompletedTrigger trigger = (parent.getTrigger() instanceof DocumentCompletedTrigger) ? (DocumentCompletedTrigger) parent.getTrigger() : null;
         GridLayout gridLayout = new GridLayout();
         gridLayout.numColumns = 2;
         this.setLayout(gridLayout);
-        nbdoc_label = new Label(this, SWT.NONE);
+        Label nbdoc_label = new Label(this, SWT.NONE);
         nbdoc_label.setText("Number of document completed");
         
         GridData gridData = new GridData();
@@ -60,11 +61,17 @@ public class HttpTriggerDocumentCompletedEditorComposite extends AbstractHttpTri
         nbdoc_spin = new Spinner(this, SWT.BORDER);
         nbdoc_spin.setLayoutData(gridData);
         nbdoc_spin.setMinimum(1);
-        nbdoc_spin.setSelection( (trigger!=null)? trigger.getCount():1);
+        nbdoc_spin.setSelection(trigger != null ? trigger.getCount() : 1);
+        
+        nbdoc_label = new Label(this, SWT.NONE);
+        nbdoc_label.setText("Stop on alert");
+        
+        check = new Button(this, SWT.CHECK);
+        check.setSelection(trigger != null ? trigger.isStopOnAlert() : false);
 	}
 
 	public TriggerXMLizer getTriggerXMLizer(){
-		return new TriggerXMLizer(new DocumentCompletedTrigger(nbdoc_spin.getSelection(),parent.getTimeout()));
+		return new TriggerXMLizer(new DocumentCompletedTrigger(nbdoc_spin.getSelection(), parent.getTimeout(), check.getSelection()));
 	}
 
 	public String getHelp() {
