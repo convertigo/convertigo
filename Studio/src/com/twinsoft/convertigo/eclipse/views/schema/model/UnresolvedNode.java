afc10b517e49f030c376c08c852da32555a88215
Nathalie Lammelin <nathaliel@convertigo.com>
2012-09-25 14:18:47 +0000

    inprogress #3094 - Added new files

---
+++ Studio/src/com/twinsoft/convertigo/eclipse/views/schema/model/UnresolvedNode.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: $
+ * $Author: $
+ * $Revision: $
+ * $Date: $
+ */
+
+package com.twinsoft.convertigo.eclipse.views.schema.model;
+
+import java.util.List;
+
+public class UnresolvedNode extends TreeNode {
+
+	public UnresolvedNode(TreeParentNode parent, String name) {
+		super(parent, name, null);
+	}
+
+	@Override
+	public List<? extends TreeNode> getChildren() {
+		return null;
+	}
+
+	@Override
+	public boolean hasChildren() {
+		return false;
+	}
+
+}
