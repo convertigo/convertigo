5c0590dd0a72490b94aa8646d9ac8227d03adbc8
Nathalie Lammelin <nathaliel@convertigo.com>
2012-10-30 09:22:01 +0000

    inprogress #3104 - Fixed reference for static XSD include

--- Studio/src/com/twinsoft/convertigo/beans/core/Reference.java
+++ Studio/src/com/twinsoft/convertigo/beans/core/Reference.java
@@ -1,7 +1,7 @@
 package com.twinsoft.convertigo.beans.core;
 
 
-public abstract class Reference extends DatabaseObject {
+public abstract class Reference extends DatabaseObject implements ISchemaGenerator {
 	private static final long serialVersionUID = -1201316885732909011L;
 
 }
