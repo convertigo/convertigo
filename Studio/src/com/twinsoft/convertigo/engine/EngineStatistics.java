4e3480c4223b8169103d64ef8b5e070a4fbe96bd
Fabien Balageas <fabienb@convertigo.com>
2012-11-15 15:22:23 +0000

    inprogress #3190 - fixed NPE in 6.2.x

--- Studio/src/com/twinsoft/convertigo/engine/EngineStatistics.java
+++ Studio/src/com/twinsoft/convertigo/engine/EngineStatistics.java
@@ -413,7 +413,7 @@ public class EngineStatistics extends Statistics {
 	}
 
 	public static Object addStatisticsAsXML(Context context, Object result) {
-		if (result != null && context.requestedObject.getAddStatistics()) {
+		if (result != null && context.requestedObject != null && context.requestedObject.getAddStatistics()) {
 			if (result instanceof Document) {
 				Document document = (Document) result;
 				context.statistics.printStatisticsXML(document);
