ff05b83eee1a493a9f5b193d6e89a2d333a077b2
Laetitia Mouloud <laetitiam@convertigo.com>
2012-12-26 15:16:20 +0000

    CLOSED #3281  * branch 6.2.x and trunk 6.3.x  * add new icons for multi valued variables png 16 32  * modify the name of file  * modify the beanInfo : url of icons

--- Studio/src/com/twinsoft/convertigo/beans/variables/RequestableHttpMultiValuedVariableBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/variables/RequestableHttpMultiValuedVariableBeanInfo.java
@@ -31,8 +31,8 @@ public class RequestableHttpMultiValuedVariableBeanInfo extends MySimpleBeanInfo
 			beanClass = RequestableHttpMultiValuedVariable.class;
 			additionalBeanClass = com.twinsoft.convertigo.beans.variables.RequestableHttpVariable.class;
 
-			iconNameC16 = "/com/twinsoft/convertigo/beans/variables/images/variable_color_16x16.png";
-			iconNameC32 = "/com/twinsoft/convertigo/beans/variables/images/variable_color_32x32.png";
+			iconNameC16 = "/com/twinsoft/convertigo/beans/variables/images/multivaluedvariable_color_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/variables/images/multivaluedvariable_color_32x32.png";
 
 			resourceBundle = java.util.ResourceBundle.getBundle("com/twinsoft/convertigo/beans/variables/res/RequestableHttpMultiValuedVariable");
 
