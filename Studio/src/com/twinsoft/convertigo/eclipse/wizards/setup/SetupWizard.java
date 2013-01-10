8723430080e1351af67e6940fd3c9830839cfc1e
Nicolas Albert <nicolasa@convertigo.com>
2013-01-10 10:51:36 +0000

    Close #3313 - Change c8o_dev.convertigo.net by c8o-dev.convertigo.net for the 6.2 beta
    Done

--- Studio/src/com/twinsoft/convertigo/eclipse/wizards/setup/SetupWizard.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/wizards/setup/SetupWizard.java
@@ -32,8 +32,7 @@ import com.twinsoft.convertigo.engine.ProxyManager;
 import com.twinsoft.convertigo.engine.util.XMLUtils;
 
 public class SetupWizard extends Wizard {
-	public static final String registrationServiceUrl = "https://c8o_dev.convertigo.net/cems/projects/studioRegistration/.xml";
-//	public static final String registrationServiceUrl = "https://cosinus.twinsoft.fr:18081/convertigo/projects/studioRegistration/.xml";
+	public static final String registrationServiceUrl = "https://c8o-dev.convertigo.net/cems/projects/studioRegistration/.xml";
 	
 	private static final Pattern scheme_host_pattern = Pattern.compile("https://(.*?)(?::([\\d]*))?(/.*|$)");
 	
