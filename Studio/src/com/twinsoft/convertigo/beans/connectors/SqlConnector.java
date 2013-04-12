0608891bbabb79a0dc8f8c0d37f404ee65942b44
Julien De Almeida <julienda@convertigo.com>
2013-04-12 13:30:19 +0000

    INPROGRESS #3498 - (6.2.x) - "Prevent SQL injection": Now, we use the "setString" method of the prepared statement class to securise parameters values (cf : http://en.wikipedia.org/wiki/Prepared_statement )

--- Studio/src/com/twinsoft/convertigo/beans/connectors/SqlConnector.java
+++ Studio/src/com/twinsoft/convertigo/beans/connectors/SqlConnector.java
@@ -30,6 +30,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -253,7 +254,7 @@ public class SqlConnector extends Connector {
 		}
 	}
 
-	public PreparedStatement prepareStatement(String sqlQuery) throws SQLException, ClassNotFoundException, EngineException {
+	public PreparedStatement prepareStatement(String sqlQuery, ArrayList<String> params) throws SQLException, ClassNotFoundException, EngineException {
 		PreparedStatement preparedStatement = null;
 		if (isClosed() || needReset)
 			open();
@@ -276,8 +277,17 @@ public class SqlConnector extends Connector {
 				}
 			}
 		}
+		// Call the method prepareParameters with the preparedStatement and the ArrayList
+		prepareParameters(preparedStatement, params);
 		
 		return preparedStatement;
+	}
+
+	private void prepareParameters(PreparedStatement preparedStatement, ArrayList<String> params) throws SQLException{
+		// We loop and set parameters of the preparedStatement
+		for (int x = 0; x < params.size() ; x++){
+			preparedStatement.setString(x+1, params.get(x));	
+		}
 	}
 
 	public void prepareForTransaction(Context context) throws EngineException {
