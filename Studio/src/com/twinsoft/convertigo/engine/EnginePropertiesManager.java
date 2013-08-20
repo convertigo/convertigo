a9bd282806de5869b0b3673c9748ad2297e2214e
Élodie Eberhardt <elodiee@convertigo.com>
2013-08-20 22:23:53 +0000

    INPROGRESS #3606
    In 6.3.x : another update in the engine properties labels in source code

--- Studio/src/com/twinsoft/convertigo/engine/EnginePropertiesManager.java
+++ Studio/src/com/twinsoft/convertigo/engine/EnginePropertiesManager.java
@@ -293,7 +293,7 @@ public class EnginePropertiesManager {
 		@PropertyOptions(advance = true, propertyType = PropertyType.Boolean)
 		THROW_HTTP_500 ("throw_http_500", "true", "Throw HTTP 500 in case of unrecoverable servlet error", PropertyCategory.Main),
 		@PropertyOptions(advance = true, propertyType = PropertyType.Boolean)
-		HIDING_ERROR_INFORMATION ("hiding_error_information", "false", "Hide detailed information in case of uncoverable servlet error", PropertyCategory.Main),
+		HIDING_ERROR_INFORMATION ("hiding_error_information", "false", "Hide detailed information in case of unrecoverable servlet error", PropertyCategory.Main),
 		@PropertyOptions(advance = true, propertyType = PropertyType.Boolean)
 		THROW_HTTP_500_SOAP_FAULT ("throw_http_500.soap_fault", "true", "Throw HTTP 500 in case of SOAP fault", PropertyCategory.Main),
 		@PropertyOptions(advance = true, propertyType = PropertyType.Boolean, visibility = Visibility.HIDDEN)
