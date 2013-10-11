97af475570902ea0f0b5112ecb86e3b9078d8113
Fabien Balageas <fabienb@convertigo.com>
2013-10-11 12:06:40 +0000

    6.3.0 official release

--- Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -28,7 +28,7 @@ public class ProductVersion {
     public static String majorProductVersion = "6";
     public static String minorProductVersion = "3";
     public static String servicePack = "0";
-    public static String tag = "RC";
+    public static String tag = null;
     public static String revision = null;
     
     public static String productVersion =
