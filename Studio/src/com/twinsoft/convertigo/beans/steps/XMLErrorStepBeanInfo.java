a8b8830c72813055ee8d9f284833a26d73c87d24
Laetitia Mouloud <laetitiam@convertigo.com>
2014-03-28 09:33:32 +0000

    CLOSED #3953  * Trunk 7.1  * png, 16, 32  * create a new icon XML Error Structure step  * add icon in the images folder and modify images link

--- Studio/src/com/twinsoft/convertigo/beans/steps/XMLErrorStepBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/steps/XMLErrorStepBeanInfo.java
@@ -33,8 +33,8 @@ public class XMLErrorStepBeanInfo extends MySimpleBeanInfo {
 			beanClass = XMLErrorStep.class;
 			additionalBeanClass = com.twinsoft.convertigo.beans.core.StepWithExpressions.class;
 
-			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/complex_16x16.png";
-			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/complex_32x32.png";
+			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/xmlErrorStructure_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/xmlErrorStructure_32x32.png";
 			
 			resourceBundle = java.util.ResourceBundle.getBundle("com/twinsoft/convertigo/beans/steps/res/XMLErrorStep");
 			
