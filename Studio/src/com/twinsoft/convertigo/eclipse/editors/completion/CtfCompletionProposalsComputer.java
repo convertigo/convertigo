e8a778f54e021f064cefa584ecf0738745ae7ce0
Jean-Michel Cohen <jmc@convertigo.com>
2014-06-24 13:45:16 +0000

    INPROGRESS #4185 added data-c8o-call-action attribute in xml completion dictionnary

--- Studio/src/com/twinsoft/convertigo/eclipse/editors/completion/CtfCompletionProposalsComputer.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/editors/completion/CtfCompletionProposalsComputer.java
@@ -296,7 +296,7 @@ public class CtfCompletionProposalsComputer implements ICompletionProposalComput
 						String alreadyTyped = document.get(oFound + 1, len);
 
 						/*
-						 * deal with c8o-call
+						 * deal with c8o-call (find related transactions and sequences)
 						 */
 						if (alreadyTyped.startsWith("data-c8o-call=\"") || alreadyTyped.startsWith("data-c8o-listen=\"")) {
 							// We are typing in the content of a data-c8o-call
@@ -496,3 +496,4 @@ public class CtfCompletionProposalsComputer implements ICompletionProposalComput
 		// TODO Auto-generated method stub
 	}
 }
+
