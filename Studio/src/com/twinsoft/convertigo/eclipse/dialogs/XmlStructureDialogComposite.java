8a645cb3efc09cf7d39c7e688b0dd6f90a127c7e
Nathalie Lammelin <nathaliel@convertigo.com>
2014-06-25 15:26:55 +0000

    closed #4216 - In Trunk : Added new action & dialog to create a step's structure from XML

---
+++ Studio/src/com/twinsoft/convertigo/eclipse/dialogs/XmlStructureDialogComposite.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: $
+ * $Author: $
+ * $Revision: $
+ * $Date: $
+ */
+
+package com.twinsoft.convertigo.eclipse.dialogs;
+
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.eclipse.swt.widgets.Text;
+
+public class XmlStructureDialogComposite extends MyAbstractDialogComposite {
+
+	public ProgressBar progressBar = null;
+	public Label labelProgression = null;
+	public Text xml = null;
+	
+	public XmlStructureDialogComposite(Composite parent, int style, Object parentObject) {
+		super(parent, style);
+		
+		initialize();
+	}
+
+	protected void initialize() {
+		Label label0 = new Label (this, SWT.NONE);
+		label0.setText ("Please enter the XML structure to import into a sequence's step:");
+		
+		GridData data = new GridData ();
+		data.horizontalAlignment = GridData.FILL;
+		data.verticalAlignment = GridData.FILL;
+		data.grabExcessHorizontalSpace = true;
+		data.grabExcessVerticalSpace = true;
+		data.heightHint = 200;
+		xml = new Text(this, SWT.BORDER | SWT.V_SCROLL);
+		xml.setLayoutData (data);
+
+        GridData gridData2 = new GridData();
+		gridData2.horizontalSpan = 2;
+		gridData2.verticalAlignment = GridData.CENTER;
+		gridData2.horizontalAlignment = GridData.FILL;
+		labelProgression = new Label(this, SWT.NONE);
+		labelProgression.setText("Progression");
+		labelProgression.setLayoutData(gridData2);
+		
+        GridData gridData4 = new GridData();
+		gridData4.horizontalSpan = 2;
+		gridData4.verticalAlignment = GridData.CENTER;
+		gridData4.horizontalAlignment = GridData.FILL;
+        progressBar = new ProgressBar(this, SWT.NONE);
+        //progressBar.setBounds(new Rectangle(16, 349, 571, 17));
+        progressBar.setLayoutData(gridData4);
+        
+		GridLayout gridLayout = new GridLayout();
+		setLayout(gridLayout);
+		setSize(new Point(408, 251));
+	}
+	
+	public Object getValue(String name) {
+		return xml.getText();
+	}
+}
---
+++ Studio/src/com/twinsoft/convertigo/eclipse/popup/actions/SequenceImportFromXmlAction.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: $
+ * $Author: $
+ * $Revision: $
+ * $Date: $
+ */
+
+package com.twinsoft.convertigo.eclipse.popup.actions;
+
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Cursor;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+import com.twinsoft.convertigo.beans.core.DatabaseObject;
+import com.twinsoft.convertigo.beans.core.Sequence;
+import com.twinsoft.convertigo.beans.core.Step;
+import com.twinsoft.convertigo.beans.core.StepWithExpressions;
+import com.twinsoft.convertigo.eclipse.ConvertigoPlugin;
+import com.twinsoft.convertigo.eclipse.dialogs.XmlStructureDialog;
+import com.twinsoft.convertigo.eclipse.editors.CompositeEvent;
+import com.twinsoft.convertigo.eclipse.views.projectexplorer.ProjectExplorerView;
+import com.twinsoft.convertigo.eclipse.views.projectexplorer.model.DatabaseObjectTreeObject;
+import com.twinsoft.convertigo.eclipse.views.projectexplorer.model.SequenceTreeObject;
+
+public class SequenceImportFromXmlAction extends MyAbstractAction {
+
+	public SequenceImportFromXmlAction() {
+		super();
+	}
+	
+	public void run() {
+		Display display = Display.getDefault();
+		Cursor waitCursor = new Cursor(display, SWT.CURSOR_WAIT);		
+		
+		Shell shell = getParentShell();
+		shell.setCursor(waitCursor);
+		
+        try {
+    		ProjectExplorerView explorerView = getProjectExplorerView();
+    		if (explorerView != null) {
+    			DatabaseObjectTreeObject databaseObjectTreeObject = (DatabaseObjectTreeObject)explorerView.getFirstSelectedTreeObject();
+    			DatabaseObject databaseObject = databaseObjectTreeObject.getObject();
+    			SequenceTreeObject sequenceTreeObject = (SequenceTreeObject) ((databaseObject instanceof Sequence) ? databaseObjectTreeObject:databaseObjectTreeObject.getParentDatabaseObjectTreeObject());
+    			Sequence sequence = (databaseObject instanceof Sequence) ? (Sequence)databaseObject:((StepWithExpressions)databaseObject).getSequence();
+    			
+    			XmlStructureDialog dlg = new XmlStructureDialog(shell, sequence);
+				if (dlg.open() == Window.OK) {
+					if (dlg.result instanceof Throwable) {
+						throw (Throwable)dlg.result;
+					}
+					else {
+						Step step = (Step)dlg.result;
+						if (step != null) {
+							if (databaseObject instanceof Sequence) {
+								sequence.addStep(step);
+								sequence.hasChanged = true;
+							}
+							else {
+								StepWithExpressions swe = (StepWithExpressions)databaseObject;
+								swe.addStep(step);
+								swe.hasChanged = true;
+							}
+							
+							sequence.hasChanged = true;
+							
+							// Reload sequence in tree without updating its schema for faster reload
+							ConvertigoPlugin.logDebug("Reload sequence: start");
+							explorerView.reloadTreeObject(sequenceTreeObject);
+							ConvertigoPlugin.logDebug("Reload sequence: end");
+							
+							// Select target dbo in tree
+							explorerView.objectSelected(new CompositeEvent(databaseObject));
+						}
+					}
+	        	}
+    		}
+        }
+        catch (Throwable e) {
+        	ConvertigoPlugin.logException(e, "Unable to import step from xml!");
+        }
+        finally {
+			shell.setCursor(null);
+			waitCursor.dispose();
+        }
+	}
+}
