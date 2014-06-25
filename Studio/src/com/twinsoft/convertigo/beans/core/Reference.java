0800e030bf8411b44ca6cac8c313daed521bc073
Julien De Almeida <julienda@convertigo.com>
2014-06-25 13:26:33 +0000

    CLOSE #4208 - (7.1.x & trunk) - References folder badly displayed in Admin console   - Added database type to permit to precise and show the "References" folder instead of "Undefineds"

--- Studio/src/com/twinsoft/convertigo/beans/core/Reference.java
+++ Studio/src/com/twinsoft/convertigo/beans/core/Reference.java
@@ -3,5 +3,9 @@ package com.twinsoft.convertigo.beans.core;
 
 public abstract class Reference extends DatabaseObject {
 	private static final long serialVersionUID = -1201316885732909011L;
-
+	
+	public Reference() {
+		super();
+		databaseType = "Reference";
+	}
 }
