ad12b307df85f5638e00ec4cd5f6c52b8af6b286
Élodie Eberhardt <elodiee@convertigo.com>
2014-07-31 13:55:11 +0000

    INPROGRESS #4280
    in trunk: updated icons in beaninfos for ifIsIn and ifFileExists steps

--- Studio/src/com/twinsoft/convertigo/beans/steps/IfFileExistThenElseStepBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/steps/IfFileExistThenElseStepBeanInfo.java
@@ -31,8 +31,8 @@ public class IfFileExistThenElseStepBeanInfo extends MySimpleBeanInfo {
 			beanClass = IfFileExistThenElseStep.class;
 			additionalBeanClass = com.twinsoft.convertigo.beans.steps.IfFileExistStep.class;
 
-			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/if_16x16.png";
-			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/if_32x32.png";
+			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/ifFileExists_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/ifFileExists_32x32.png";
 			
 			resourceBundle = getResourceBundle("res/IfFileExistThenElseStep");
 			
