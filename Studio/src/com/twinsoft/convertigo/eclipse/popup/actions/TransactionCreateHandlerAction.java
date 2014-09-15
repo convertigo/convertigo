280efa7e31ef9664817f034fb15934d4bdd99a9b
Victor Nea <victorn@convertigo.com>
2014-09-15 15:11:08 +0000

    CLOSED #3903 - Fixed pop-up error

--- Studio/src/com/twinsoft/convertigo/eclipse/popup/actions/TransactionCreateHandlerAction.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/popup/actions/TransactionCreateHandlerAction.java
@@ -28,17 +28,12 @@ import java.util.Vector;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.jface.viewers.StructuredSelection;
-import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Tree;
-import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPart;
 
 import com.twinsoft.convertigo.beans.core.Statement;
 import com.twinsoft.convertigo.beans.core.Transaction;
@@ -59,6 +54,7 @@ public class TransactionCreateHandlerAction extends MyAbstractAction {
 		super();
 	}
 
+	@Override
 	public void selectionChanged(IAction action, ISelection selection) {
 		try {
 			boolean enable = false;
@@ -73,7 +69,8 @@ public class TransactionCreateHandlerAction extends MyAbstractAction {
 		}
 		catch (Exception e) {}
 	}
-	
+
+	@Override
 	public void run() {
 		Display display = Display.getDefault();
 		Cursor waitCursor = new Cursor(display, SWT.CURSOR_WAIT);
@@ -82,18 +79,18 @@ public class TransactionCreateHandlerAction extends MyAbstractAction {
 		shell.setCursor(waitCursor);
 		
 		Statement lastStatement = null;
-		int index = 0;
 
 		try {
     		ProjectExplorerView explorerView = getProjectExplorerView();
     		if (explorerView != null) {
 				TreeObject treeObject = explorerView.getFirstSelectedTreeObject();
-				if (treeObject != null) {
+				if (treeObject != null) {					
 					Transaction transaction = null;
-					if (treeObject instanceof TransactionTreeObject)
-						transaction = (Transaction)treeObject.getObject();
-					else if (treeObject instanceof ObjectsFolderTreeObject)
-						transaction = (Transaction)treeObject.getParent().getObject();
+					if (treeObject instanceof TransactionTreeObject) {
+						transaction = (Transaction) treeObject.getObject();
+					} else if (treeObject instanceof ObjectsFolderTreeObject) {
+						transaction = (Transaction) treeObject.getParent().getObject();
+					}
 					
 					if (transaction != null) {
 						CreateHandlerDialog createHandlerDialog = new CreateHandlerDialog(shell, transaction);
@@ -118,15 +115,14 @@ public class TransactionCreateHandlerAction extends MyAbstractAction {
 				    						handler = (String)result.elementAt(i);
 					    					transaction.handlers += handler;
 					    					transaction.hasChanged = true;
-					    					
-											// Update the opened handlers editor if any
-											IEditorPart jspart = ConvertigoPlugin.getDefault().getJscriptTransactionEditor(transaction);
-											if ((jspart != null) && (jspart instanceof JscriptTransactionEditor)) {
-												JscriptTransactionEditor jscriptTransactionEditor = (JscriptTransactionEditor)jspart;
-												jscriptTransactionEditor.reload();
-											}
-											index = i;
 				    					}
+				    					
+										// Update the opened handlers editor if any
+										IEditorPart jspart = ConvertigoPlugin.getDefault().getJscriptTransactionEditor(transaction);
+										if ((jspart != null) && (jspart instanceof JscriptTransactionEditor)) {
+											JscriptTransactionEditor jscriptTransactionEditor = (JscriptTransactionEditor)jspart;
+											jscriptTransactionEditor.reload();
+										}
 				    				}
 				    				
 				    				// Reload transaction in tree and select last created Statement.
@@ -134,43 +130,14 @@ public class TransactionCreateHandlerAction extends MyAbstractAction {
 	            						ProjectExplorerView projectExplorerView = ConvertigoPlugin.getDefault().getProjectExplorerView();
 	            						projectExplorerView.reloadDatabaseObject(transaction);
 	            						
-	            						
 	            						if (transaction instanceof HtmlTransaction) {
 		            						if (lastStatement != null) {
 		            							projectExplorerView.objectSelected(new CompositeEvent(lastStatement));
 		            						}
+	            						} else {
+	            							projectExplorerView.objectSelected(new CompositeEvent(transaction));
 	            						}
-		            					else {
-		            						TreeViewer treeViewer = explorerView.viewer;
-		            						Tree tree = treeViewer.getTree();				
-		            						TreeItem[] treeItems = tree.getSelection();
-		            						treeItems[0].setExpanded(true);
-		            						treeViewer.refresh();
-		            					    TreeItem[] transactionItems = treeItems[0].getItems();
-		            					    transactionItems[0].setExpanded(true);
-		            					    treeViewer.refresh();
-		            					    TreeItem[] handlersItems = transactionItems[0].getItems();
-		            					    tree.deselect(treeItems[0]);
-		            					    boolean found = false;
-		            					    int i=0;
-		            					    while (!found) {
-		            					    	if (((String)result.elementAt(index)).contains(handlersItems[i].getText())) {
-		            					    		tree.select(handlersItems[i]);
-		            					    		found = true;
-		            					    	}
-		            					    	i++;
-		            					    }		            					 
-		            					    treeViewer.refresh();     					    
-
-		            					    IStructuredSelection selection = (IStructuredSelection) treeViewer.getSelection();
-		            						TreeObject treeObjects = (TreeObject) selection.getFirstElement();
-		            						
-		            						StructuredSelection structuredSelection = new StructuredSelection(treeObjects);
-		            						ConvertigoPlugin.getDefault().getPropertiesView().selectionChanged((IWorkbenchPart)projectExplorerView, structuredSelection);
-		            					}
-	            						
 									} catch (IOException e) {}
-	    	        				
 			    				}
 			    			}
 			    		}
