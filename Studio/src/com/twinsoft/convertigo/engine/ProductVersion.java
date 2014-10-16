0c812c504ea3e32e3021f0c84644e25b775ece84
Élodie Eberhardt <elodiee@convertigo.com>
2014-10-16 10:33:53 +0000

    7.0.5 official release

--- Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -28,7 +28,7 @@ public class ProductVersion {
     public static String majorProductVersion = "7";
     public static String minorProductVersion = "0";
     public static String servicePack = "5";
-    public static String tag = "beta";
+    public static String tag = null;
     public static String revision = null;
     
     public static String productVersion =
