948c7a72fc5df1b71b514dbfc742298e78585c02
Nathalie Lammelin <nathaliel@convertigo.com>
2015-01-08 10:01:18 +0000

    inprogress #4547 - In trunk : Fixed ClassLoader issue : modified CreateDocumentBeanInfo.java file

--- Studio/src/com/twinsoft/convertigo/beans/transactions/couchdb/CreateDocumentTransactionBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/transactions/couchdb/CreateDocumentTransactionBeanInfo.java
@@ -35,8 +35,8 @@ public class CreateDocumentTransactionBeanInfo extends MySimpleBeanInfo {
 			displayName = getExternalizedString("display_name");
 			shortDescription = getExternalizedString("short_description");
 
-			iconNameC16 = "/com/twinsoft/convertigo/beans/transactions/images/couchdb/databasetransaction_color_16x16.png";
-			iconNameC32 = "/com/twinsoft/convertigo/beans/transactions/images/couchdb/databasetransaction_color_32x32.png";
+			iconNameC16 = "/com/twinsoft/convertigo/beans/transactions/couchdb/images/databasetransaction_color_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/transactions/couchdb/images/databasetransaction_color_32x32.png";
 			
 		}
 		catch(Exception e) {
