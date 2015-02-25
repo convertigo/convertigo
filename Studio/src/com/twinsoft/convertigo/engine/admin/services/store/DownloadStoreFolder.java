c7c64294245ba41d8cb0338937ed96ed8e14f52f
Victor Nea <victorn@convertigo.com>
2015-02-25 15:38:31 +0000

    CLOSED #4610 - (trunk) Download store functionality added  - New store package created under the admin/services package  - DownloadStoreFolder Servlet created (in the store package)  - The store folder is download in a .zip file

---
+++ Studio/src/com/twinsoft/convertigo/engine/admin/services/store/DownloadStoreFolder.java
@@ -0,0 +1,43 @@
+package com.twinsoft.convertigo.engine.admin.services.store;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.IOUtils;
+
+import com.twinsoft.convertigo.engine.AuthenticatedSessionManager.Role;
+import com.twinsoft.convertigo.engine.Engine;
+import com.twinsoft.convertigo.engine.admin.services.DownloadService;
+import com.twinsoft.convertigo.engine.admin.services.at.ServiceDefinition;
+import com.twinsoft.convertigo.engine.util.ZipUtils;
+
+@ServiceDefinition(
+		name = "DownloadStoreFolder",
+		roles = { Role.WEB_ADMIN },
+		parameters = {},
+		returnValue = ""
+	)
+public class DownloadStoreFolder extends DownloadService {
+
+	@Override
+	protected void writeResponseResult(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		File storeArchiveFile = File.createTempFile("store", ".zip");
+		ZipUtils.makeZip(storeArchiveFile.getPath(), Engine.WEBAPP_PATH + "/store", null);
+		
+		FileInputStream archiveInputStream = null;
+		try {
+			archiveInputStream = new FileInputStream(storeArchiveFile);
+			
+			response.setHeader("Content-Disposition", "attachment; filename=" + "\"store.zip\"");
+			response.setContentType("application/octet-stream");
+			IOUtils.copy(archiveInputStream, response.getOutputStream());	
+		}
+		finally {
+			storeArchiveFile.delete();
+			archiveInputStream.close();
+		}
+	}
+}
--- Studio/tomcat/webapps/convertigo/admin/widgets/store.js
+++ Studio/tomcat/webapps/convertigo/admin/widgets/store.js
@@ -1,15 +1,4 @@
 function store_init() {
-	$("#downloadStore").click(function () {
-
-	})
-	
-	$("#uploadStore").click(function () {
-
-	})
-	
-	$("#deleteStore").click(function () {
-
-	})
 }
 
 function store_update() {
