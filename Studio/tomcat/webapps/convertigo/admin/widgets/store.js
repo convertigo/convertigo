887f2d5fcfb1a2c861ba7703f89bee91924604b3
Victor Nea <victorn@convertigo.com>
2015-02-25 14:19:02 +0000

    CLOSED #4609 - (trunk) New "Store" section added to the web admin console  - This section contains three buttons : "Download Store", "Upload Store" and "Delete custom Store"  - These functionalities are not implemented yet

---
+++ Studio/tomcat/webapps/convertigo/admin/widgets/store.js
@@ -0,0 +1,16 @@
+function store_init() {
+	$("#downloadStore").click(function () {
+
+	})
+	
+	$("#uploadStore").click(function () {
+
+	})
+	
+	$("#deleteStore").click(function () {
+
+	})
+}
+
+function store_update() {
+}
