1251457480fab35deffb8965168ad01254919dc6
Nicolas Albert <nicolasa@convertigo.com>
2015-05-20 08:56:50 +0000

    7.2.5 RC

--- Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -28,7 +28,7 @@ public class ProductVersion {
     public static String majorProductVersion = "7";
     public static String minorProductVersion = "2";
     public static String servicePack = "5";
-    public static String tag = "beta";		/** release = null */
+    public static String tag = null;		/** release = null */
     public static String revision = null;	/** release = null */
     
     public static String productVersion =
