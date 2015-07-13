1c41878819a0d26ef62436189ebccfb04e14b876
Nathalie Lammelin <nathaliel@convertigo.com>
2015-07-13 09:44:13 +0000

    inprogress #4806
    * Added automatic variable's creation/deletion/rename when '''!SubDir''' property is modified
    * Added automatic !SubDir property update when a '''Variable''' is renamed/deleted

--- Studio/src/com/twinsoft/convertigo/eclipse/views/projectexplorer/model/TransactionTreeObject.java
+++ Studio/src/com/twinsoft/convertigo/eclipse/views/projectexplorer/model/TransactionTreeObject.java
@@ -47,10 +47,15 @@ import com.twinsoft.convertigo.beans.core.DatabaseObject;
 import com.twinsoft.convertigo.beans.core.Project;
 import com.twinsoft.convertigo.beans.core.ScreenClass;
 import com.twinsoft.convertigo.beans.core.Transaction;
+import com.twinsoft.convertigo.beans.core.Variable;
+import com.twinsoft.convertigo.beans.transactions.AbstractHttpTransaction;
 import com.twinsoft.convertigo.beans.transactions.HtmlTransaction;
 import com.twinsoft.convertigo.beans.transactions.SqlTransaction;
+import com.twinsoft.convertigo.beans.variables.RequestableHttpVariable;
 import com.twinsoft.convertigo.beans.variables.RequestableVariable;
 import com.twinsoft.convertigo.eclipse.ConvertigoPlugin;
+import com.twinsoft.convertigo.eclipse.dialogs.ButtonSpec;
+import com.twinsoft.convertigo.eclipse.dialogs.CustomDialog;
 import com.twinsoft.convertigo.eclipse.editors.jscript.JscriptTransactionEditor;
 import com.twinsoft.convertigo.eclipse.editors.jscript.JscriptTransactionEditorInput;
 import com.twinsoft.convertigo.eclipse.editors.xml.XMLTransactionEditorInput;
@@ -89,6 +94,45 @@ public class TransactionTreeObject extends DatabaseObjectTreeObject implements I
 		}
 	}
 
+	
+	@Override
+	public void treeObjectRemoved(TreeObjectEvent treeObjectEvent) {
+		super.treeObjectRemoved(treeObjectEvent);
+		
+		TreeObject treeObject = (TreeObject)treeObjectEvent.getSource();
+		if (!(treeObject.equals(this)) && (treeObject.getParents().contains(this))) {
+			if (treeObject instanceof DatabaseObjectTreeObject) {
+				DatabaseObject databaseObject = (DatabaseObject)treeObject.getObject();
+				
+				// A variable has been removed
+				if (databaseObject instanceof Variable) {
+					String variableName = databaseObject.getName();
+					
+					// This is an AbstractHttpTransaction
+					if (getObject() instanceof AbstractHttpTransaction) {
+						AbstractHttpTransaction httpTransaction = (AbstractHttpTransaction)getObject();
+						String transactionSubDir = httpTransaction.getSubDir();
+						List<String> pathVariableList = AbstractHttpTransaction.getPathVariableList(httpTransaction.getSubDir());
+						
+						// Update transaction SubDir property
+						if (pathVariableList.contains(variableName)) {
+    						transactionSubDir = transactionSubDir.replaceAll("\\{"+variableName+"\\}", "");
+    						httpTransaction.setSubDir(transactionSubDir);
+    						httpTransaction.hasChanged = true;
+							
+		    		    	try {
+		    					ConvertigoPlugin.getDefault().getProjectExplorerView().updateTreeObject(this);
+		    				} catch (Exception e) {
+		    					ConvertigoPlugin.logWarning(e, "Could not update in tree Transaction \""+databaseObject.getName()+"\" !");
+		    				}
+							
+						}
+					}
+				}
+			}
+		}
+	}
+
 	@Override
 	public void treeObjectPropertyChanged(TreeObjectEvent treeObjectEvent) {
 		super.treeObjectPropertyChanged(treeObjectEvent);
@@ -116,6 +160,111 @@ public class TransactionTreeObject extends DatabaseObjectTreeObject implements I
     		    				sqlTransaction.getVariablesList());
     		    		
     					ConvertigoPlugin.getDefault().getProjectExplorerView().reloadTreeObject(this);
+    				} catch (Exception e) {
+    					ConvertigoPlugin.logWarning(e, "Could not reload in tree Transaction \""+databaseObject.getName()+"\" !");
+    				}
+				}
+			}
+			else if ("subDir".equals(propertyName)) {
+				if (treeObject.equals(this)) {
+					try {
+						Object oldValue = treeObjectEvent.oldValue;
+						Object newValue = treeObjectEvent.newValue;
+						
+						AbstractHttpTransaction httpTransaction = (AbstractHttpTransaction) databaseObject;
+						List<String> oldPathVariableList = AbstractHttpTransaction.getPathVariableList(oldValue.toString());
+						List<String> newPathVariableList = AbstractHttpTransaction.getPathVariableList(newValue.toString());
+						
+						// Check for variables to be renamed
+						if (oldValue.toString().replaceAll("\\{([a-zA-Z0-9_]+)\\}", "{}").equals(
+							newValue.toString().replaceAll("\\{([a-zA-Z0-9_]+)\\}", "{}"))) {
+							for (int i=0; i<oldPathVariableList.size(); i++) {
+								String oldVariableName = oldPathVariableList.get(i);
+								String newVariableName = newPathVariableList.get(i);
+								if (!oldVariableName.equals(newVariableName)) {
+									RequestableHttpVariable httpVariable = (RequestableHttpVariable) httpTransaction.getVariable(oldVariableName);
+									if (httpVariable != null) {
+					    		    	try {
+											
+											VariableTreeObject2 vto = (VariableTreeObject2) findTreeObjectByUserObject(httpVariable);
+											int update = TreeObjectEvent.UPDATE_NONE;
+											CustomDialog customDialog = new CustomDialog(
+													viewer.getControl().getShell(),
+													"Update object references",
+													"Do you want to update "
+															+ "variable"
+															+ " references ?\n You can replace '"
+															+ oldVariableName
+															+ "' by '"
+															+ newVariableName
+															+ "' in all loaded projects \n or replace '"
+															+ oldVariableName
+															+ "' by '"
+															+ newVariableName
+															+ "' in current project only.",
+															670, 200,
+													new ButtonSpec("Replace in all loaded projects", true),
+													new ButtonSpec("Replace in current project", false),
+													new ButtonSpec("Do not replace anywhere", false));
+											int response = customDialog.open();
+											if (response == 0) {
+												update = TreeObjectEvent.UPDATE_ALL;
+											}
+											if (response == 1) {
+												update = TreeObjectEvent.UPDATE_LOCAL;
+											}
+											if (update != 0) {
+												httpVariable.setName(newVariableName);
+												httpVariable.hasChanged = true;
+												TreeObjectEvent toEvent = new TreeObjectEvent(vto, "name", oldVariableName, newVariableName, update);
+												ConvertigoPlugin.getDefault().getProjectExplorerView().fireTreeObjectPropertyChanged(toEvent);
+											}
+					    				} catch (Exception e) {
+					    					ConvertigoPlugin.logWarning(e, "Could not rename variable for Transaction \""+databaseObject.getName()+"\" !");
+					    				}
+									}
+								}
+							}
+						}
+						else {
+							// Check for variables to be added
+							for (String variableName : newPathVariableList) {
+								if (httpTransaction.getVariable(variableName) == null) {
+									RequestableHttpVariable httpVariable = new RequestableHttpVariable();
+									httpVariable.setName(variableName);
+									httpVariable.setHttpMethod("GET");
+									httpVariable.setHttpName("");
+									httpVariable.bNew = true;
+									httpVariable.hasChanged = true;
+									httpTransaction.addVariable(httpVariable);
+									httpTransaction.hasChanged = true;
+								}
+							}
+							// Check for variables to be deleted
+							for (String variableName : oldPathVariableList) {
+								RequestableHttpVariable httpVariable = (RequestableHttpVariable) httpTransaction.getVariable(variableName);
+								if (httpVariable != null) {
+									if (!newPathVariableList.contains(variableName)) {
+										try {
+											MessageBox messageBox = new MessageBox(viewer.getControl().getShell(), SWT.ICON_QUESTION | SWT.YES | SWT.NO); 
+											messageBox.setMessage("Do you want to delete the variable \""+variableName+"\"?");
+											messageBox.setText("Delete \""+variableName+"\"?");
+											if (messageBox.open() == SWT.YES) {
+												httpVariable.delete();
+												httpTransaction.hasChanged = true;
+											}
+										} catch (EngineException e) {
+											ConvertigoPlugin.logException(e, "Error when deleting the variable \""+variableName+"\"");
+										}
+									}
+								}
+							}
+						}
+						
+						if (httpTransaction.hasChanged) {
+							ConvertigoPlugin.getDefault().getProjectExplorerView().reloadTreeObject(this);
+						}
+						
     				} catch (Exception e) {
     					ConvertigoPlugin.logWarning(e, "Could not reload in tree Transaction \""+databaseObject.getName()+"\" !");
     				}
@@ -171,6 +320,33 @@ public class TransactionTreeObject extends DatabaseObjectTreeObject implements I
 	    					ConvertigoPlugin.logWarning(e, "Could not reload in tree Transaction \""+databaseObject.getName()+"\" !");
 	    				}
 	            	}
+				}
+			}
+		}
+		
+		
+		if (databaseObject instanceof Variable) {
+			String oldVariableName = oldValue.toString();
+			String newVariableName = newValue.toString();
+			
+			// A variable of this transaction has been renamed
+			if (getObject().equals(databaseObject.getParent())) {
+				if (getObject() instanceof AbstractHttpTransaction) {
+					AbstractHttpTransaction httpTransaction = (AbstractHttpTransaction)getObject();
+    		    	try {
+    		    		// Check for variables to be renamed in SubDir property
+    					String transactionSubDir = httpTransaction.getSubDir();
+    					List<String> pathVariableList = AbstractHttpTransaction.getPathVariableList(transactionSubDir);
+    					if (pathVariableList.contains(oldVariableName)) {
+    						transactionSubDir = transactionSubDir.replaceAll("\\{"+oldVariableName+"\\}", "{"+newVariableName+"}");
+    						httpTransaction.setSubDir(transactionSubDir);
+    						httpTransaction.hasChanged = true;
+    					}
+    					
+    					ConvertigoPlugin.getDefault().getProjectExplorerView().refreshTreeObject(this);
+    				} catch (Exception e) {
+    					ConvertigoPlugin.logWarning(e, "Could not reload in tree Transaction \""+databaseObject.getName()+"\" !");
+    				}
 				}
 			}
 		}
