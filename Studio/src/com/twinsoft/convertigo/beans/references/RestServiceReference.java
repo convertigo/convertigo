da5c65fb12c7466a344c7efe23b16c302be16d1f
Nathalie Lammelin <nathaliel@convertigo.com>
2015-07-21 09:09:37 +0000

    inprogress #4815 - Added new '''RestServiceReference''' related files

---
+++ Studio/src/com/twinsoft/convertigo/beans/references/RestServiceReference.java
@@ -0,0 +1,7 @@
+package com.twinsoft.convertigo.beans.references;
+
+public class RestServiceReference extends RestModelReference {
+
+	private static final long serialVersionUID = 1382101929343981006L;
+
+}
