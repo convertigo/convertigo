b0847df7851615ebcc9b77edfb9e5a23d9d50e6c
Yina Mou <yinam@convertigo.com>
2015-09-24 12:50:24 +0000

    #4866 new new feature Create LDAPAuthenticationStep

---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/LDAPAuthenticationStep.java
@@ -0,0 +1,146 @@
+/*
+* Copyright (c) 2001-2014 Convertigo. All Rights Reserved.
+*
+* The copyright to the computer  program(s) herein  is the property
+* of Convertigo.
+* The program(s) may  be used  and/or copied  only with the written
+* permission  of  Convertigo  or in accordance  with  the terms and
+* conditions  stipulated  in the agreement/contract under which the
+* program(s) have been supplied.
+*
+* Convertigo makes  no  representations  or  warranties  about  the
+* suitability of the software, either express or implied, including
+* but  not  limited  to  the implied warranties of merchantability,
+* fitness for a particular purpose, or non-infringement. Convertigo
+* shall  not  be  liable for  any damage  suffered by licensee as a
+* result of using,  modifying or  distributing this software or its
+* derivatives.
+*/
+
+/*
+ * $URL: http://sourceus.twinsoft.fr/svn/convertigo/CEMS_opensource/trunk/Studio/src/com/twinsoft/convertigo/eclipse/editors/completion/CtfCompletionProposalsComputer.java $
+ * $Author: jmc $
+ * $Revision: 37416 $
+ * $Date: 2014-06-24 15:45:16 +0200 (Tue, 24 Jun 2014) $
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaCollection;
+import org.apache.ws.commons.schema.XmlSchemaElement;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import com.twinsoft.convertigo.beans.core.IComplexTypeAffectation;
+import com.twinsoft.convertigo.beans.core.Step;
+import com.twinsoft.convertigo.engine.EngineException;
+import com.twinsoft.util.TWSLDAP;
+
+public class LDAPAuthenticationStep extends Step implements IComplexTypeAffectation {
+
+	private static final long serialVersionUID = -1894558458026853410L;
+
+	private SmartType server = new SmartType();
+	//private SmartType password = new SmartType();
+	private SmartType login = new SmartType();
+	private SmartType password = new SmartType();
+	
+	public LDAPAuthenticationStep() {
+		super();
+		setOutput(false);
+		this.xml = true;
+	}
+
+	@Override
+    public LDAPAuthenticationStep clone() throws CloneNotSupportedException {
+    	LDAPAuthenticationStep clonedObject = (LDAPAuthenticationStep) super.clone();
+        return clonedObject;
+    }
+
+	@Override
+    public LDAPAuthenticationStep copy() throws CloneNotSupportedException {
+    	LDAPAuthenticationStep copiedObject = (LDAPAuthenticationStep) super.copy();
+        return copiedObject;
+    }
+
+	@Override
+	public String toJsString() {
+		return null;
+	}
+
+	@Override
+	public String getStepNodeName() {
+		return "LDAPAuthenticated";
+	}
+	
+	@Override
+	protected boolean stepExecute(Context javascriptContext, Scriptable scope) throws EngineException {
+		if (isEnable()) {
+			
+			evaluate(javascriptContext, scope, server);
+			evaluate(javascriptContext, scope, login);
+			//evaluate(javascriptContext, scope, password);
+			//evaluate(javascriptContext, scope, ldap);
+			
+			return super.stepExecute(javascriptContext, scope);
+		}
+		return false;
+	}
+
+	@Override
+	protected void createStepNodeValue(Document doc, Element stepNode) throws EngineException {
+
+			TWSLDAP ldap = new TWSLDAP();
+			//ldap.bind(server.getSingleString(this), login.getSingleString(this), password.getSingleString(this));
+
+			String ldapserver = server.getSingleString(this);
+			String ldaplogin = login.getSingleString(this);
+			String ldappassword =password.getSingleString(this);
+			Boolean authenticated = ldap.bind(ldapserver, ldaplogin, ldappassword);
+			
+			if (authenticated) {
+				getSequence().context.setAuthenticatedUser(ldaplogin);
+			}
+			
+			Node text = doc.createTextNode(authenticated.toString());
+			stepNode.appendChild(text);
+	}
+	
+	@Override
+	public XmlSchemaElement getXmlSchemaObject(XmlSchemaCollection collection, XmlSchema schema) {
+		XmlSchemaElement element = (XmlSchemaElement) super.getXmlSchemaObject(collection, schema);
+		element.setSchemaTypeName(getSimpleTypeAffectation());
+		
+		return element;
+	}
+	
+	public SmartType getServer() {
+		return server;
+	}
+	
+	public void setServer(SmartType server) {
+		this.server = server;
+	}
+	
+	public SmartType getLogin() {
+		return login;
+	}
+	
+	public void setLogin(SmartType login) {
+		this.login = login;
+	}
+	
+	
+	public SmartType getPassword() {
+		return password;
+	}
+	
+	public void setPassword(SmartType password) {
+		this.password = password;
+	}	
+
+}
---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/LDAPAuthenticationStepBeanInfo.java
@@ -0,0 +1,73 @@
+/*
+* Copyright (c) 2001-2014 Convertigo. All Rights Reserved.
+*
+* The copyright to the computer  program(s) herein  is the property
+* of Convertigo.
+* The program(s) may  be used  and/or copied  only with the written
+* permission  of  Convertigo  or in accordance  with  the terms and
+* conditions  stipulated  in the agreement/contract under which the
+* program(s) have been supplied.
+*
+* Convertigo makes  no  representations  or  warranties  about  the
+* suitability of the software, either express or implied, including
+* but  not  limited  to  the implied warranties of merchantability,
+* fitness for a particular purpose, or non-infringement. Convertigo
+* shall  not  be  liable for  any damage  suffered by licensee as a
+* result of using,  modifying or  distributing this software or its
+* derivatives.
+*/
+
+/*
+ * $URL: http://sourceus.twinsoft.fr/svn/convertigo/CEMS_opensource/trunk/Studio/src/com/twinsoft/convertigo/eclipse/editors/completion/CtfCompletionProposalsComputer.java $
+ * $Author: jmc $
+ * $Revision: 37416 $
+ * $Date: 2014-06-24 15:45:16 +0200 (Tue, 24 Jun 2014) $
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import java.beans.PropertyDescriptor;
+
+import com.twinsoft.convertigo.beans.core.MySimpleBeanInfo;
+
+public class LDAPAuthenticationStepBeanInfo extends MySimpleBeanInfo {
+    
+	public LDAPAuthenticationStepBeanInfo() {
+		try {
+			beanClass = LDAPAuthenticationStep.class;
+			additionalBeanClass = com.twinsoft.convertigo.beans.core.Step.class;
+
+			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/setAuthenticatedUser_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/setAuthenticatedUser_32x32.png";
+			
+			resourceBundle = getResourceBundle("res/LDAPAuthenticationStep");
+			
+			displayName = resourceBundle.getString("display_name");
+			shortDescription = resourceBundle.getString("short_description");
+			
+			properties = new PropertyDescriptor[3];
+
+            properties[0] = new PropertyDescriptor("server", beanClass, "getServer", "setServer");
+            properties[0].setDisplayName(getExternalizedString("property.server.display_name"));
+            properties[0].setShortDescription(getExternalizedString("property.server.short_description"));
+            properties[0].setPropertyEditorClass(getEditorClass("SmartTypeCellEditor"));
+            
+            
+            properties[1] = new PropertyDescriptor("login", beanClass, "getLogin", "setLogin");
+            properties[1].setDisplayName(getExternalizedString("property.login.display_name"));
+            properties[1].setShortDescription(getExternalizedString("property.login.short_description"));
+            properties[1].setPropertyEditorClass(getEditorClass("SmartTypeCellEditor"));
+            
+            
+            properties[2] = new PropertyDescriptor("password", beanClass, "getPassword", "setPassword");
+            properties[2].setDisplayName(getExternalizedString("property.password.display_name"));
+            properties[2].setShortDescription(getExternalizedString("property.password.short_description"));
+            properties[2].setPropertyEditorClass(getEditorClass("SmartTypeCellEditor"));            
+            
+		}
+		catch(Exception e) {
+			com.twinsoft.convertigo.engine.Engine.logBeans.error("Exception with bean info; beanClass=" + beanClass.toString(), e);
+		}
+	}
+
+}
