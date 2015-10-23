381edfce58fb1aaba961567e34f84c7455c7999c
Nicolas Albert <nicolasa@convertigo.com>
2015-10-23 12:58:54 +0000

    Ref #4875 - !HttpTransaction must send file as base64, MTOM and also write files from MTOM responses
      * fix downloading huge attachments

---
+++ Studio/src/com/twinsoft/convertigo/engine/util/BigMimeMultipart.java
@@ -0,0 +1,92 @@
+package com.twinsoft.convertigo.engine.util;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimePart;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+
+public class BigMimeMultipart {
+	static final private Pattern pHeader = Pattern.compile("(.*?): (.*)");
+	static final private Pattern pBoundary = Pattern.compile("boundary=\"(.*?)\"");
+	static final private byte[] sLine = {'\r', '\n'};
+	
+	private PartInputStream pis;
+	private String preamble;
+	
+	public BigMimeMultipart (InputStream is, String contentType) throws IOException {
+		Matcher mBoundary = pBoundary.matcher(contentType);
+		
+		if (mBoundary.find()) {
+			byte[] boundary = ("--" + mBoundary.group(1) + "\r\n").getBytes("ascii");
+			pis = new PartInputStream(is, boundary);
+			
+			preamble = IOUtils.toString(pis, "ascii");
+			pis.nextPart(ArrayUtils.addAll(sLine, boundary));
+		} else {
+			throw new IOException("(BigMimeMultipart) boundary not found in the contentType: " + contentType);
+		}
+	}
+	
+	public String getPreamble() {
+		return preamble;
+	}
+	
+	public MimePart nextPart(OutputStream os) throws IOException, MessagingException {			
+		MimeBodyPart bodyPart = readHeaders();
+		
+		if (bodyPart != null) {
+			IOUtils.copy(pis, os);
+			
+			pis.nextPart();	
+		}
+		
+		return bodyPart;
+	}
+	
+	public MimePart nextPart(File file) throws IOException, MessagingException {			
+		MimeBodyPart bodyPart = readHeaders();
+		
+		if (bodyPart != null) {
+			FileUtils.copyInputStreamToFile(pis, file);
+			
+			pis.nextPart();	
+		}
+		
+		return bodyPart;
+	}
+	
+	private MimeBodyPart readHeaders() throws IOException, MessagingException {
+		if (pis.isStreamEnd()) {
+			return null;
+		}
+		
+		MimeBodyPart bodyPart = new MimeBodyPart();		
+		PartInputStream lis = new PartInputStream(pis, sLine);
+		
+		for (String line = IOUtils.toString(lis, "ascii"); !line.isEmpty(); line = IOUtils.toString(lis, "ascii")) {
+			Matcher mHeader = pHeader.matcher(line);
+			
+			if (mHeader.matches()) {
+				bodyPart.addHeader(mHeader.group(1), mHeader.group(2));
+				
+				if (mHeader.group(1).equalsIgnoreCase("Content-ID")) {
+					bodyPart.setContentID(mHeader.group(2));
+				}
+			}
+			
+			lis.nextPart();
+		}
+		
+		return bodyPart;
+	}
+}
---
+++ Studio/src/com/twinsoft/convertigo/engine/util/PartInputStream.java
@@ -0,0 +1,97 @@
+package com.twinsoft.convertigo.engine.util;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+
+public class PartInputStream extends InputStream {
+	private InputStream inputStream;
+	private byte[] boundary;
+	
+	private byte[] search;
+	private int iSearch = 0;
+	
+	private byte[] saved;
+	private int iSaved = 0;
+	private int lSaved = 0;
+	
+	private int lastRead = 0;
+	
+	public PartInputStream(InputStream inputStream, byte[] boundary) {
+		this.inputStream = inputStream;
+		this.boundary = boundary;
+		
+		search = new byte[boundary.length];
+		saved = new byte[boundary.length];
+	}
+	
+	public boolean isStreamEnd() {
+		return lastRead == -1 && iSaved == lSaved;
+	}
+	
+	public boolean isPartEnd() {
+		return iSearch == search.length;
+	}
+	
+	public boolean nextPart() {
+		if (iSearch == search.length) {
+			iSearch = 0;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean nextPart(byte[] boundary) {
+		if (iSearch == search.length) {
+			this.boundary = boundary;
+			
+			iSearch = 0;
+			search = new byte[boundary.length];
+			
+			saved = Arrays.copyOf(saved, boundary.length);
+			
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private int peak() throws IOException {
+		if (isPartEnd()) {
+			return -1;
+		} else if (iSaved < lSaved) {
+			return saved[iSaved++];
+		} else {
+			return lastRead = inputStream.read();
+		}
+	}
+	
+	@Override
+	public int read() throws IOException {
+		int iByte = peak();
+		
+		while (iByte != -1) {
+			byte b = (byte) iByte;
+			
+			if (b == boundary[iSearch]) {
+				search[iSearch++] = b;
+				iByte = peak();
+			} else if (iSearch == 0) {
+				return iByte;
+			} else {
+				byte[] pivot = search;
+				search = saved;
+				saved = pivot;
+				
+				saved[iSearch++] = b;
+				lSaved = iSearch;
+				iSearch = iSaved = 0;
+				
+				return saved[iSaved++];
+			}
+		}
+		
+		return -1;
+	}
+}
