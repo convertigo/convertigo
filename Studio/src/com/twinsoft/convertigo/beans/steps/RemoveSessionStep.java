5680282290d4a1cef736456a32160ca3ef4008e2
Nicolas Albert <nicolasa@convertigo.com>
2015-12-21 14:38:37 +0000

    Close #4900 - New step: Remove Session
    New step here.
    
    The contexts destructions is effective when the application server really destroy the context.

---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/RemoveSessionStep.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: $
+ * $Author: $
+ * $Revision: $
+ * $Date: $
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import com.twinsoft.convertigo.beans.core.Step;
+import com.twinsoft.convertigo.engine.Engine;
+import com.twinsoft.convertigo.engine.EngineException;
+
+public class RemoveSessionStep extends Step {
+
+	private static final long serialVersionUID = 8705533301402437269L;
+	
+	public RemoveSessionStep() {
+		super();
+	}
+
+	@Override
+	public RemoveSessionStep clone() throws CloneNotSupportedException {
+		RemoveSessionStep clonedObject = (RemoveSessionStep) super.clone();
+		return clonedObject;
+	}
+
+	@Override
+	public RemoveSessionStep copy() throws CloneNotSupportedException {
+		RemoveSessionStep copiedObject = (RemoveSessionStep) super.copy();
+		return copiedObject;
+	}
+
+	@Override
+	protected boolean stepExecute(Context javascriptContext, Scriptable scope) throws EngineException {
+		if (isEnable()) {
+			if (super.stepExecute(javascriptContext, scope) && Engine.isEngineMode()) {
+				if (sequence.context != null && sequence.context.httpSession != null) {
+					sequence.context.httpSession.setMaxInactiveInterval(1);
+				} else {
+					Engine.logBeans.warn("(RemoveSessionStep) null httpSession, cannot be removed");
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		String text = this.getComment();
+		return "removeSession" + (!text.equals("") ? " // "+text:"");
+	}
+
+	@Override
+	public String toJsString() {
+		return "";
+	}
+}
