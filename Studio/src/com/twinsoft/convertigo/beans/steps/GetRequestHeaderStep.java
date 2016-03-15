f9a3b33f325f18241ba26f956ad39bec3ec89cb6
Nathalie Lammelin <nathaliel@convertigo.com>
2016-03-15 09:39:02 +0000

    inprogress #4962 : Added new related bean files

---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/GetRequestHeaderStep.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL$
+ * $Author$
+ * $Revision$
+ * $Date$
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.ws.commons.schema.XmlSchema;
+import org.apache.ws.commons.schema.XmlSchemaCollection;
+import org.apache.ws.commons.schema.XmlSchemaComplexType;
+import org.apache.ws.commons.schema.XmlSchemaElement;
+import org.apache.ws.commons.schema.XmlSchemaSequence;
+import org.apache.ws.commons.schema.constants.Constants;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import com.twinsoft.convertigo.beans.core.IComplexTypeAffectation;
+import com.twinsoft.convertigo.beans.core.IStepSmartTypeContainer;
+import com.twinsoft.convertigo.beans.core.Step;
+import com.twinsoft.convertigo.engine.EngineException;
+import com.twinsoft.convertigo.engine.enums.SchemaMeta;
+import com.twinsoft.convertigo.engine.util.XmlSchemaUtils;
+
+public class GetRequestHeaderStep extends Step implements IStepSmartTypeContainer, IComplexTypeAffectation {
+
+	private static final long serialVersionUID = 6527937933694210582L;
+
+	public GetRequestHeaderStep() {
+		super();
+		setOutput(false);
+		this.xml = true;
+	}
+
+	@Override
+	public GetRequestHeaderStep clone() throws CloneNotSupportedException {
+		GetRequestHeaderStep clonedObject = (GetRequestHeaderStep) super.clone();
+		clonedObject.smartTypes = null;
+		return clonedObject;
+	}
+	
+	private SmartType headerName = new SmartType();
+	
+	public SmartType getHeaderName() {
+		return headerName;
+	}
+
+	public void setHeaderName(SmartType headerName) {
+		this.headerName = headerName;
+	}
+
+	private transient Set<SmartType> smartTypes = null;
+	
+	@Override
+	public Set<SmartType> getSmartTypes() {
+		if (smartTypes != null) {
+			if  (!hasChanged)
+				return smartTypes;
+			else
+				smartTypes.clear();
+		}
+		else {
+			smartTypes = new HashSet<SmartType>();
+		}
+		smartTypes.add(headerName);
+		return smartTypes;
+	}
+	
+	@Override
+	public String toJsString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean stepExecute(Context javascriptContext, Scriptable scope) throws EngineException {
+		if (isEnable()) {
+			evaluate(javascriptContext, scope, headerName);
+			
+			return super.stepExecute(javascriptContext, scope);
+		}
+		return false;
+	}
+
+	@Override
+	public String getStepNodeName() {
+		return "header";
+	}
+	
+	@Override
+	protected void createStepNodeValue(Document doc, Element stepNode) throws EngineException {
+		String hName = (hName = headerName.getSingleString(this)) == null ? "" : hName;
+		if (hName != null && hName.length() > 0) {
+			List<String> valueList = getSequence().context.getRequestHeaders().get(hName.toLowerCase());
+			int size = valueList == null ? 0:valueList.size();
+			String hValue = size == 0 ? "":(size == 1 ? valueList.get(0):valueList.toString());
+			
+			Element en = (Element) stepNode.appendChild(doc.createElement("name"));
+			en.setTextContent(hName);
+			
+			Element ev = (Element) stepNode.appendChild(doc.createElement("value"));
+			ev.setTextContent(hValue);
+		}
+	}
+	
+	@Override
+	public XmlSchemaElement getXmlSchemaObject(XmlSchemaCollection collection, XmlSchema schema) {
+		XmlSchemaElement element = (XmlSchemaElement) super.getXmlSchemaObject(collection, schema);
+		
+		XmlSchemaComplexType cType = XmlSchemaUtils.makeDynamic(this, new XmlSchemaComplexType(schema));
+		element.setType(cType);
+
+		XmlSchemaSequence sequence = XmlSchemaUtils.makeDynamic(this, new XmlSchemaSequence());
+		cType.setParticle(sequence);
+		SchemaMeta.setContainerXmlSchemaGroupBase(element, sequence);
+		
+		XmlSchemaElement elt = XmlSchemaUtils.makeDynamic(this, new XmlSchemaElement());
+		sequence.getItems().add(elt);
+		elt.setName("name");
+		elt.setMinOccurs(1);
+		elt.setMaxOccurs(1);
+		elt.setSchemaTypeName(Constants.XSD_STRING);
+		
+		elt = XmlSchemaUtils.makeDynamic(this, new XmlSchemaElement());
+		sequence.getItems().add(elt);
+		elt.setName("value");
+		elt.setMinOccurs(1);
+		elt.setMaxOccurs(1);
+		elt.setSchemaTypeName(Constants.XSD_STRING);
+		
+		return element;
+	}
+	
+}
---
+++ Studio/src/com/twinsoft/convertigo/beans/steps/GetRequestHeaderStepBeanInfo.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL$
+ * $Author$
+ * $Revision$
+ * $Date$
+ */
+
+package com.twinsoft.convertigo.beans.steps;
+
+import java.beans.PropertyDescriptor;
+
+import com.twinsoft.convertigo.beans.core.MySimpleBeanInfo;
+
+public class GetRequestHeaderStepBeanInfo extends MySimpleBeanInfo {
+
+	public GetRequestHeaderStepBeanInfo() {
+		try {
+			beanClass = GetRequestHeaderStep.class;
+			additionalBeanClass = com.twinsoft.convertigo.beans.core.Step.class;
+
+			iconNameC16 = "/com/twinsoft/convertigo/beans/steps/images/getRequestHeader_16x16.png";
+			iconNameC32 = "/com/twinsoft/convertigo/beans/steps/images/getRequestHeader_32x32.png";
+			
+			resourceBundle = getResourceBundle("res/GetRequestHeaderStep");
+			
+			displayName = resourceBundle.getString("display_name");
+			shortDescription = resourceBundle.getString("short_description");
+			
+			properties = new PropertyDescriptor[1];
+
+            properties[0] = new PropertyDescriptor("headerName", beanClass, "getHeaderName", "setHeaderName");
+            properties[0].setDisplayName(getExternalizedString("property.headerName.display_name"));
+            properties[0].setShortDescription(getExternalizedString("property.headerName.short_description"));
+            properties[0].setPropertyEditorClass(getEditorClass("SmartTypeCellEditor"));
+		}
+		catch(Exception e) {
+			com.twinsoft.convertigo.engine.Engine.logBeans.error("Exception with bean info; beanClass=" + beanClass.toString(), e);
+		}
+	}
+
+}
