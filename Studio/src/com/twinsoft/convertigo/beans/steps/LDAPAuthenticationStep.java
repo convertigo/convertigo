bd33577c6d974d6cd381932324c1d44a0572dd1f
Nathalie Lammelin <nathaliel@convertigo.com>
2016-04-12 12:56:31 +0000

    inprogress #4979 : Modified step output : added '''userDn''' attribute, set step output to '''userLogin''' value

--- Studio/src/com/twinsoft/convertigo/beans/steps/LDAPAuthenticationStep.java
+++ Studio/src/com/twinsoft/convertigo/beans/steps/LDAPAuthenticationStep.java
@@ -35,8 +35,6 @@ import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-
 import com.twinsoft.convertigo.beans.core.IComplexTypeAffectation;
 import com.twinsoft.convertigo.beans.core.Step;
 import com.twinsoft.convertigo.engine.Engine;
@@ -98,76 +96,78 @@ public class LDAPAuthenticationStep extends Step implements IComplexTypeAffectat
 
 	@Override
 	protected void createStepNodeValue(Document doc, Element stepNode) throws EngineException {
-			Boolean authenticated = false;
+		String serverUrl = null, userDn = null, userLogin = null, userPassword = null;	
+		Boolean authenticated = false;
+		
+		// Remove currently authenticated user from session
+		getSequence().context.removeAuthenticatedUser();
+		
+		serverUrl = server.getSingleString(this);
+		if (serverUrl != null && !serverUrl.isEmpty()) {
+			TWSLDAP twsLDAP = new TWSLDAP();
 			
-			// Remove currently authenticated user from session
-			getSequence().context.removeAuthenticatedUser();
-			
-			String ldap_server = server.getSingleString(this);
-			if (ldap_server != null && !ldap_server.isEmpty()) {
-				TWSLDAP twsLDAP = new TWSLDAP();
-				
-				// Search LDAP database for given user
-				String ldap_userLogin = login.getSingleString(this);
-				String ldap_userPassword = password.getSingleString(this);
-				if (ldap_userLogin != null) {
-					if (!isDistinguishedName(ldap_userLogin)) {
-						String ldap_adminLogin = adminLogin.getSingleString(this);
-						String ldap_adminPassword = adminPassword.getSingleString(this);
-						String ldap_basePath = basePath.getSingleString(this);
+			// Search LDAP database for given user
+			userLogin = login.getSingleString(this);
+			userPassword = password.getSingleString(this);
+			if (userLogin != null) {
+				if (!isDistinguishedName(userLogin)) {
+					String searchLogin = adminLogin.getSingleString(this);
+					String searchPassword = adminPassword.getSingleString(this);
+					String searchBase = basePath.getSingleString(this);
+					
+					if (searchLogin != null) {
+						int countLimit = 0, timeLimit = 0;
+						String searchHost = getHost(serverUrl);
+						String searchFilter = getFilter(userLogin);
+						String[] searchAttributes = null;
 						
-						if (ldap_adminLogin != null) {
-							int countLimit = 0, timeLimit = 0;
-							String searchBase = ldap_basePath;
-							String host = getHost(ldap_server);
-							String filter = getFilter(ldap_userLogin);
-							String[] attributes = null;
-							
-							if (host.isEmpty()) {
-								Engine.logBeans.warn("LDAP host is empty !");
-							}
-							ldap_adminPassword = ldap_adminPassword == null ? "":ldap_adminPassword;
-							
-							// Search database for given user
-							Engine.logBeans.trace("Start LDAP search");
-							twsLDAP.search(host, ldap_adminLogin, ldap_adminPassword, searchBase, filter, attributes, timeLimit, countLimit);
-							Engine.logBeans.trace("End LDAP search");
-							
-							boolean bFound = twsLDAP.hasMoreResults();
-							Engine.logBeans.debug("LDAP User "+ (bFound ? "found":"NOT found") +" by database search; searchBase:"+searchBase+", filter:"+ filter);
-							if (bFound) {
-								ldap_userLogin = twsLDAP.getNextResult() + (searchBase == null ? "":"," + searchBase);
-							}
+						if (searchHost.isEmpty()) {
+							Engine.logBeans.warn("LDAP host is empty !");
 						}
-						else {
-							Engine.logBeans.warn("Invalid LDAP admin Login \""+ldap_adminLogin+"\" !");
+						searchPassword = searchPassword == null ? "":searchPassword;
+						
+						// Search database for given user
+						Engine.logBeans.trace("Start LDAP search");
+						twsLDAP.search(searchHost, searchLogin, searchPassword, searchBase, searchFilter, searchAttributes, timeLimit, countLimit);
+						Engine.logBeans.trace("End LDAP search");
+						
+						boolean bFound = twsLDAP.hasMoreResults();
+						Engine.logBeans.debug("LDAP User "+ (bFound ? "found":"NOT found") +" by database search; searchBase:"+searchBase+", filter:"+ searchFilter);
+						if (bFound) {
+							userDn = twsLDAP.getNextResult() + (searchBase == null ? "":"," + searchBase);
 						}
 					}
-					
-					ldap_userPassword = ldap_userPassword == null ? "":ldap_userPassword; //avoid NPE in bind
-					
-					// Bind database with given/found user
-					Engine.logBeans.trace("Start LDAP bind");
-					authenticated = twsLDAP.bind(ldap_server, ldap_userLogin, ldap_userPassword);
-					Engine.logBeans.trace("End LDAP bind");
-					
-					Engine.logBeans.debug("LDAP User \""+ldap_userLogin+"\" authenticated="+ authenticated.toString());
-					
-					// Set authenticated user on session
-					if (authenticated) {
-						getSequence().context.setAuthenticatedUser(ldap_userLogin);
+					else {
+						Engine.logBeans.warn("Invalid LDAP admin Login \""+searchLogin+"\" !");
 					}
 				}
-				else {
-					Engine.logBeans.warn("Invalid LDAP user Login \""+ldap_userLogin+"\" !");
+				
+				userPassword = userPassword == null ? "":userPassword; //avoid NPE in bind
+				
+				// Bind database with given/found user
+				Engine.logBeans.trace("Start LDAP bind");
+				authenticated = twsLDAP.bind(serverUrl, userDn == null ? userLogin:userDn, userPassword);
+				Engine.logBeans.trace("End LDAP bind");
+				
+				Engine.logBeans.debug("LDAP User \""+userLogin+"\" authenticated="+ authenticated.toString());
+				
+				// Set authenticated user on session
+				if (authenticated) {
+					getSequence().context.setAuthenticatedUser(userLogin);
 				}
 			}
 			else {
-				Engine.logBeans.warn("Invalid LDAP server \""+ldap_server+"\" !");
+				Engine.logBeans.warn("Invalid LDAP user Login \""+userLogin+"\" !");
 			}
-			
-			Node text = doc.createTextNode(authenticated.toString());
-			stepNode.appendChild(text);
+		}
+		else {
+			Engine.logBeans.warn("Invalid LDAP server \""+serverUrl+"\" !");
+		}
+		
+		stepNode.setAttribute("userDn", userDn == null ? "":userDn);
+		if (userLogin != null) {
+			stepNode.appendChild(doc.createTextNode(userLogin));
+		}
 	}
 
 	private Boolean isDistinguishedName(String username) {
