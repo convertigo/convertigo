201f65a1bc5e2c37fefa5c7f24a739dec43acdba
Nicolas Albert <nicolasa@convertigo.com>
2016-05-24 15:21:25 +0000

    Starting 7.4.1_beta

--- Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ Studio/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -27,8 +27,8 @@ public class ProductVersion {
 
     public static String majorProductVersion = "7";
     public static String minorProductVersion = "4";
-    public static String servicePack = "0";
-    public static String tag = null;		/** release = null */
+    public static String servicePack = "1";
+    public static String tag = "beta";		/** release = null */
     public static String revision = null;	/** release = null */
     
     public static String productVersion =
