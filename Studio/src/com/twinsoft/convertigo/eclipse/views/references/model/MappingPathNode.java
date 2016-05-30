9bf0cb539e695186c01e99a9a8083efeabb42a0b
Nathalie Lammelin <nathaliel@convertigo.com>
2016-05-30 12:44:19 +0000

    inprogress #5008 : Added new classes for new tree nodes

---
+++ Studio/src/com/twinsoft/convertigo/eclipse/views/references/model/MappingPathNode.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2001-2011 Convertigo SA.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Affero General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see<http://www.gnu.org/licenses/>.
+ *
+ * $URL: $
+ * $Author: $
+ * $Revision: $
+ * $Date: $
+ */
+
+package com.twinsoft.convertigo.eclipse.views.references.model;
+
+import com.twinsoft.convertigo.beans.core.DatabaseObject;
+
+
+public class MappingPathNode extends AbstractNodeWithDatabaseObjectReference {
+
+	public MappingPathNode(AbstractParentNode parent, String name, DatabaseObject refDatabaseObject) {
+		super(parent, name, refDatabaseObject);
+	}
+
+}
