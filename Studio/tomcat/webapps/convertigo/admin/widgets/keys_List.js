e93e9c88e91235c262cad1834455928d35fba7b9
Jean-Michel Cohen <jmc@convertigo.com>
2017-04-19 16:50:29 +0000

    IN PROGRESS #5213 added remove key feature to administration

--- Studio/tomcat/webapps/convertigo/admin/widgets/keys_List.js
+++ Studio/tomcat/webapps/convertigo/admin/widgets/keys_List.js
@@ -20,6 +20,31 @@
  * $Date$
  */
 
+function removeKey(key) {
+	if (key.length > 0) {
+		var $keys = $("<keys/>").append($("<key/>").attr("text", key));
+		
+		callService("keys.Remove", function(xml) {
+			var message = "";
+			$(xml).find("key").each(function() {
+				var keyErrorMessage = $(this).attr("errorMessage");
+				var keyText = $(this).attr("text");
+				if (keyErrorMessage) {
+					message += "Unable to remove the key '" + keyText + "'<br/><br/>" + keyErrorMessage;
+				} else {
+					message += "The key <em>" + keyText + "</em> has been successfully removed.";
+					keys_List_update();
+				}
+			});
+			
+			if (message.length) {
+				showInfo(message);
+			}			
+		}, domToString($keys), undefined, {contentType : "application/xml"});
+	}
+	
+	return false;
+} 
 
 function keys_List_update() {
 	callService("keys.List", function(xml) {
@@ -94,6 +119,7 @@ function updateKeysList(xml) {
 			$key.addClass(i % 2 == 0 ? "main_even" : "main_odd" )
 			$key.find(".key-text").text($x_key.attr('text'));
 			$key.find(".key-value").text($x_key.attr('value'));
+			$key.find(".key-click").attr("onclick", "removeKey('" + $x_key.attr('text') + "')");
 			
 			if ($(this).attr('evaluation') != "true" || $(this).attr('expired') != "true") {
 				$key.find(".key-expired").hide();
