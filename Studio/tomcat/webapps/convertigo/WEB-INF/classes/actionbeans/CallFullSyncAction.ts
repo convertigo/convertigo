e5ece93fc19e7a7e8b0a80b3dcc1e6c27b9cb169
Olivier Picciotto <olivierp@convertigo.com>
2017-10-31 14:54:53 +0000

    inprogress #5127 Fixed CallFullSync Pseudo Action for fs://

--- Studio/tomcat/webapps/convertigo/WEB-INF/classes/actionbeans/CallFullSyncAction.ts
+++ Studio/tomcat/webapps/convertigo/WEB-INF/classes/actionbeans/CallFullSyncAction.ts
@@ -1,15 +1,18 @@
     /**
      * Function CallFullSyncAction
      *   
-     * @param page  , the current page
+     * 
      * @param props , the object which holds properties key-value pairs
      * @param vars  , the object which holds variables key-value pairs
      */
-    CallFullSyncAction(page: C8oPage, props, vars) : Promise<any> {
+    CallFullSyncAction(props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
-            let r:string = props.requestable; let v:string = props.verb; let m:string = props.marker;
+            let r:string = props.requestable.substring(props.requestable.indexOf('.')+1);
+            let v:string = props.verb;
+            let m:string = props.marker;
             let rvm:string = r + '.' + v + (m != '' ? '#':'')+ m;
-            page.call(rvm,page.merge({},vars),null,500)
+            this.call("fs://" + rvm,this.merge({},vars),null,500)
             .then((res:any) => {resolve(res)}).catch((error:any) => {reject(error)})
         });
     }
+    
