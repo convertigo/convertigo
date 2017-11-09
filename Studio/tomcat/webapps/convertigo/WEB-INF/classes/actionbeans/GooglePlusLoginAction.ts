0996911112bd64258f4524e5def0fdcbe45322b8
Olivier Picciotto <olivierp@convertigo.com>
2017-11-09 14:21:10 +0000

    close #5367 Removed commented code

--- Studio/tomcat/webapps/convertigo/WEB-INF/classes/actionbeans/GooglePlusLoginAction.ts
+++ Studio/tomcat/webapps/convertigo/WEB-INF/classes/actionbeans/GooglePlusLoginAction.ts
@@ -7,10 +7,6 @@
      */
     GooglePlusLoginAction(page: C8oPage, props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
-            /*
-            let r: string = props.requestable; let m: string = props.marker;
-            let rm: string = r + ( m != '' ? '#' : '' ) + m;
-            */
             page.getInstance(Platform).ready().then(() => {  
                 page.getInstance(GooglePlus).login({
                     
@@ -24,14 +20,5 @@
                     reject( err )
                 })
             });
-            /*
-            page.call( rm, page.merge( { __localCache_priority: props.cachePolicy, __localCache_ttl: props.cacheTtl }, vars ), null, 500 )
-                .then(( res: any ) => {
-                    resolve( res )
-                } )
-                .catch(( error: any ) => {
-                    reject( error )
-                } )
-            */
         });
     }
