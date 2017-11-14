b58131599b27a6a25d3b0a6de5707043a86cd98b
Nathalie Lammelin <nathaliel@convertigo.com>
2017-11-14 15:16:09 +0000

    ref #5317, fixed #5339 : Added config properties to '''UICustomAction'''.

--- Studio/src/com/twinsoft/convertigo/beans/mobile/components/UICustomAction.java
+++ Studio/src/com/twinsoft/convertigo/beans/mobile/components/UICustomAction.java
@@ -32,6 +32,7 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
 import com.twinsoft.convertigo.beans.common.FormatedContent;
+import com.twinsoft.convertigo.beans.common.XMLVector;
 import com.twinsoft.convertigo.beans.core.DatabaseObject;
 import com.twinsoft.convertigo.beans.mobile.components.MobileSmartSourceType.Mode;
 import com.twinsoft.convertigo.beans.mobile.components.UIControlDirective.AttrDirective;
@@ -49,13 +50,91 @@ public class UICustomAction extends UIComponent implements IAction {
 		UICustomAction cloned = (UICustomAction) super.clone();
 		return cloned;
 	}
-
+	
 	public String getFunctionName() {
 		return "ATS"+ this.priority;
 	}
 	
 	public String getActionName() {
 		return "CTS"+ this.priority;
+	}
+
+	/*
+	 * The needed page imports
+	 */
+	private XMLVector<XMLVector<String>> page_ts_imports = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getPageTsImports() {
+		return page_ts_imports;
+	}
+	
+	public void setPageTsImports(XMLVector<XMLVector<String>> page_ts_imports) {
+		this.page_ts_imports = page_ts_imports;
+	}
+	
+	/*
+	 * The needed module imports
+	 */
+	private XMLVector<XMLVector<String>> module_ts_imports = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getModuleTsImports() {
+		return module_ts_imports;
+	}
+	
+	public void setModuleTsImports(XMLVector<XMLVector<String>> module_ts_imports) {
+		this.module_ts_imports = module_ts_imports;
+	}
+
+	/*
+	 * The needed ngModule imports
+	 */
+	private XMLVector<XMLVector<String>> module_ng_imports = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getModuleNgImports() {
+		return module_ng_imports;
+	}
+	
+	public void setModuleNgImports(XMLVector<XMLVector<String>> module_ng_imports) {
+		this.module_ng_imports = module_ng_imports;
+	}
+
+	/*
+	 * The needed ngModule providers
+	 */
+	private XMLVector<XMLVector<String>> module_ng_providers = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getModuleNgProviders() {
+		return module_ng_providers;
+	}
+	
+	public void setModuleNgProviders(XMLVector<XMLVector<String>> module_ng_providers) {
+		this.module_ng_providers = module_ng_providers;
+	}
+
+	/*
+	 * The needed package dependencies
+	 */
+	private XMLVector<XMLVector<String>> package_dependencies = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getPackageDependencies() {
+		return package_dependencies;
+	}
+	
+	public void setPackageDependencies(XMLVector<XMLVector<String>> package_dependencies) {
+		this.package_dependencies = package_dependencies;
+	}
+
+	/*
+	 * The needed cordova plugins
+	 */
+	private XMLVector<XMLVector<String>> cordova_plugins = new XMLVector<XMLVector<String>>();
+	
+	public XMLVector<XMLVector<String>> getCordovaPlugins() {
+		return cordova_plugins;
+	}
+	
+	public void setCordovaPlugins(XMLVector<XMLVector<String>> cordova_plugins) {
+		this.cordova_plugins = cordova_plugins;
 	}
 
 	/*
@@ -175,6 +254,24 @@ public class UICustomAction extends UIComponent implements IAction {
 	
 	@Override
 	public void computeScripts(JSONObject jsonScripts) {
+		try {
+			PageComponent page = getPage();
+			
+			String imports = jsonScripts.getString("imports");
+			for (XMLVector<String> v : page_ts_imports) {
+				String name = v.get(0).trim();
+				String path = v.get(1).trim();
+				if (page.addImport(name, path)) {
+					imports += "import { "+name+" } from '"+path+"';" + System.lineSeparator();
+				}
+			}
+			
+			jsonScripts.put("imports", imports);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		
+		
 		DatabaseObject parent = getParent();
 		if (parent != null && !(parent instanceof IAction)) {
 			try {
@@ -331,40 +428,85 @@ public class UICustomAction extends UIComponent implements IAction {
 		return new Contributor() {
 			@Override
 			public Map<String, String> getActionTsFunctions() {
-				Map<String, String> functions = new HashMap<String, String>();
-				return functions;
+				return new HashMap<String, String>();
 			}
 
 			@Override
 			public Map<String, String> getActionTsImports() {
-				Map<String, String> imports = new HashMap<String, String>();
-				return imports;
+				return new HashMap<String, String>();
 			}
 
 			@Override
 			public Map<String, String> getModuleTsImports() {
 				Map<String, String> imports = new HashMap<String, String>();
+				for (XMLVector<String> v : module_ts_imports) {
+					String name = v.get(0).trim();
+					String path = v.get(1).trim();
+					if (!name.isEmpty() && !path.isEmpty()) {
+						if (!imports.containsKey(name)) {
+							imports.put(name, path);
+						}
+					}
+				}
 				return imports;
 			}
 
 			@Override
 			public Set<String> getModuleNgImports() {
-				return new HashSet<String>();
+				Set<String> modules = new HashSet<String>();
+				for (XMLVector<String> v : module_ng_imports) {
+					String module = v.get(0).trim();
+					if (!module.isEmpty()) {
+						if (!modules.contains(module)) {
+							modules.add(module);
+						}
+					}
+				}
+				return modules;
 			}
 
 			@Override
 			public Set<String> getModuleNgProviders() {
-				return new HashSet<String>();
+				Set<String> providers = new HashSet<String>();
+				for (XMLVector<String> v : module_ng_providers) {
+					String provider = v.get(0).trim();
+					if (!provider.isEmpty()) {
+						if (!providers.contains(provider)) {
+							providers.add(provider);
+						}
+					}
+				}
+				return providers;
 			}
 
 			@Override
 			public Map<String, String> getPackageDependencies() {
-				return new HashMap<String, String>();
+				Map<String, String> dependencies = new HashMap<String, String>();
+				for (XMLVector<String> v : package_dependencies) {
+					String pckg = v.get(0).trim();
+					String version = v.get(1).trim();
+					if (!pckg.isEmpty() && !version.isEmpty()) {
+						if (!dependencies.containsKey(pckg)) {
+							dependencies.put(pckg, version);
+						}
+					}
+				}
+				return dependencies;
 			}
 
 			@Override
 			public Map<String, String> getConfigPlugins() {
-				return new HashMap<String, String>();
+				Map<String, String> plugins = new HashMap<String, String>();
+				for (XMLVector<String> v : cordova_plugins) {
+					String plugin = v.get(0).trim();
+					String version = v.get(1).trim();
+					if (!plugin.isEmpty() && !version.isEmpty()) {
+						if (!plugins.containsKey(plugin)) {
+							plugins.put(plugin, version);
+						}
+					}
+				}
+				return plugins;
 			}
 			
 		};
--- Studio/src/com/twinsoft/convertigo/beans/mobile/components/UICustomActionBeanInfo.java
+++ Studio/src/com/twinsoft/convertigo/beans/mobile/components/UICustomActionBeanInfo.java
@@ -41,12 +41,48 @@ public class UICustomActionBeanInfo extends MySimpleBeanInfo {
 			displayName = resourceBundle.getString("display_name");
 			shortDescription = resourceBundle.getString("short_description");
 			
-			properties = new PropertyDescriptor[1];
+			properties = new PropertyDescriptor[7];
 			
 			properties[0] = new PropertyDescriptor("actionValue", beanClass, "getActionValue", "setActionValue");
 			properties[0].setDisplayName(getExternalizedString("property.actionValue.display_name"));
 			properties[0].setShortDescription(getExternalizedString("property.actionValue.short_description"));
 			properties[0].setHidden(true);
+			
+			properties[1] = new PropertyDescriptor("page_ts_imports", beanClass, "getPageTsImports", "setPageTsImports");
+			properties[1].setDisplayName(getExternalizedString("property.page_ts_imports.display_name"));
+			properties[1].setShortDescription(getExternalizedString("property.page_ts_imports.short_description"));
+			properties[1].setPropertyEditorClass(getEditorClass("MobileConfigTsImportsEditor"));
+			properties[1].setExpert(true);
+
+			properties[2] = new PropertyDescriptor("module_ts_imports", beanClass, "getModuleTsImports", "setModuleTsImports");
+			properties[2].setDisplayName(getExternalizedString("property.module_ts_imports.display_name"));
+			properties[2].setShortDescription(getExternalizedString("property.module_ts_imports.short_description"));
+			properties[2].setPropertyEditorClass(getEditorClass("MobileConfigTsImportsEditor"));
+			properties[2].setExpert(true);
+
+			properties[3] = new PropertyDescriptor("module_ng_imports", beanClass, "getModuleNgImports", "setModuleNgImports");
+			properties[3].setDisplayName(getExternalizedString("property.module_ng_imports.display_name"));
+			properties[3].setShortDescription(getExternalizedString("property.module_ng_imports.short_description"));
+			properties[3].setPropertyEditorClass(getEditorClass("MobileConfigNgImportsEditor"));
+			properties[3].setExpert(true);
+
+			properties[4] = new PropertyDescriptor("module_ng_providers", beanClass, "getModuleNgProviders", "setModuleNgProviders");
+			properties[4].setDisplayName(getExternalizedString("property.module_ng_providers.display_name"));
+			properties[4].setShortDescription(getExternalizedString("property.module_ng_providers.short_description"));
+			properties[4].setPropertyEditorClass(getEditorClass("MobileConfigNgProvidersEditor"));
+			properties[4].setExpert(true);
+
+			properties[5] = new PropertyDescriptor("package_dependencies", beanClass, "getPackageDependencies", "setPackageDependencies");
+			properties[5].setDisplayName(getExternalizedString("property.package_dependencies.display_name"));
+			properties[5].setShortDescription(getExternalizedString("property.package_dependencies.short_description"));
+			properties[5].setPropertyEditorClass(getEditorClass("MobileConfigPackagesEditor"));
+			properties[5].setExpert(true);
+
+			properties[6] = new PropertyDescriptor("cordova_plugins", beanClass, "getCordovaPlugins", "setCordovaPlugins");
+			properties[6].setDisplayName(getExternalizedString("property.cordova_plugins.display_name"));
+			properties[6].setShortDescription(getExternalizedString("property.cordova_plugins.short_description"));
+			properties[6].setPropertyEditorClass(getEditorClass("MobileConfigPluginsEditor"));
+			properties[6].setExpert(true);
 		}
 		catch(Exception e) {
 			com.twinsoft.convertigo.engine.Engine.logBeans.error("Exception with bean info; beanClass=" + beanClass.toString(), e);
