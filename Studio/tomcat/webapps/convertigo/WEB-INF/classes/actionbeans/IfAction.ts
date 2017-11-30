a1c842a78af0f10a4e272b2e2b60fa6d680b70d6
Olivier Picciotto <olivierp@convertigo.com>
2017-11-30 15:31:25 +0000

    close #5395 Added an If bean.

---
+++ Studio/tomcat/webapps/convertigo/WEB-INF/classes/actionbeans/IfAction.ts
@@ -0,0 +1,17 @@
+    /**
+     * Function ifAction
+     *   
+     * @param page  , the current page
+     * @param props , the object which holds properties key-value pairs
+     * @param vars  , the object which holds variables key-value pairs
+     */
+    IfAction(page: C8oPage, props, vars) : Promise<any> {
+        return new Promise((resolve, reject) => {
+            if (props.condition) {
+                resolve(true)
+            } else {
+                reject(false)
+            }
+        })
+    }
+    
