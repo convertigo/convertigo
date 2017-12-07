974962e8fb7d41f3daecc8763b266553cda459e7
Nicolas Albert <nicolasa@convertigo.com>
2017-12-07 10:24:48 +0000

    Ref #5401 - Select dev build mode in the mobile builder
     * fix descriptions

--- Studio/src/com/twinsoft/convertigo/engine/enums/MobileBuilderBuildMode.java
+++ Studio/src/com/twinsoft/convertigo/engine/enums/MobileBuilderBuildMode.java
@@ -1,9 +1,9 @@
 package com.twinsoft.convertigo.engine.enums;
 
 public enum MobileBuilderBuildMode {
-	fast("f", "ionic:serve:eval", "fast mode", "the fastest dev build mode but no source code debugging"),
-	debug("d", "ionic:serve:nosourcemap", "debug mode", "slower dev build mode with javascript (not typescript) debugging"),
-	debugplus("dp", "ionic:serve", "debug plus mode", "slowest dev build mode with the full typescript debugging (use external debugger Chrome for sourcemap)"),
+	fast("f", "ionic:serve:eval", "fast mode", "the fastest dev build mode but no source code debugging."),
+	debug("d", "ionic:serve:nosourcemap", "debug mode", "slower dev build mode with javascript (not typescript) debugging."),
+	debugplus("dp", "ionic:serve", "debug plus mode", "slowest dev build mode with the full typescript debugging (use external debugger Chrome to use sourcemap)."),
 	production("p", "ionic:build:prod", "production mode", "long build time > 5 mins but automatically remove debug data, unusued code, shrink and code scrambler. The application will be smaller and start faster.");
 	
 	String label;
