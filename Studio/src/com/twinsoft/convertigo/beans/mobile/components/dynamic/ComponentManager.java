dc542bf939e838cf44dd24161bdd5282b338959b
Nathalie Lammelin <nathaliel@convertigo.com>
2017-12-07 14:56:51 +0000

    inprogress #5135 : Sort mobile palette components by label

--- Studio/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/ComponentManager.java
+++ Studio/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/ComponentManager.java
@@ -410,6 +410,14 @@ public class ComponentManager {
 
 			});
 		}
+		
+		Collections.sort(components, new Comparator<Component>() {
+			@Override
+			public int compare(Component c1, Component c2) {
+				return c1.getLabel().compareTo(c2.getLabel());
+			}				
+		} );
+		
 		return Collections.unmodifiableList(components);
 	}
 	
