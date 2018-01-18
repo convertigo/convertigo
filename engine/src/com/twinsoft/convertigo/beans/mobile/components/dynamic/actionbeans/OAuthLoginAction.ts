27553a2b1101ffedaef7965b4673dc2669d31776
Olivier Picciotto <olivierp@convertigo.com>
2018-01-18 12:09:02 +0100

    inprogress #5463 Added the OAuth action bean.

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/OAuthLoginAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/OAuthLoginAction.ts
@@ -7,36 +7,49 @@
      */
     OAuthLoginAction(page: C8oPage, props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
-            /*
-            let r: string = props.requestable; let m: string = props.marker;
-            let rm: string = r + ( m != '' ? '#' : '' ) + m;
-            */
-            
-            page.routerProvider.doOAuthLogin('https://login.microsoftonline.com/common/oauth2/v2.0/authorize?' +     // Authorize URL
-                'client_id=d26692c4-4181-45e5-856c-7cae5fc78c4d' +                                  // Client ID as registred in the app portal
-                '&response_type=id_token+token' +                                                   // We ask for implicit flow
-                '&scope=openid' +                                                                   // Scopes...
-                '%20https%3A%2F%2Fgraph.microsoft.com%2FUser.Read' +
-                '%20Files.ReadWrite' +
-                '&response_mode=fragment&state=12345&nonce=678910',                                 // Implicit flow
-                'https://login.live.com/oauth20_desktop.srf',                                           // the call back URL to check (As declared in the app portal)
-                'lib_OAuth.loginAzureAdWithAccessToken',                                                // The server sequence to be launched to check the access token
-                'lib_OAuth.checkAccessToken'                                                             // The sequence to execute to check access token
-            ).then((response: any )=>{
-                resolve( response )
-            })
-            .catch((error: any) => {
-                reject( error )
-            })
-            
-            /*
-            page.call( rm, page.merge( { __localCache_priority: props.cachePolicy, __localCache_ttl: props.cacheTtl }, vars ), null, 500 )
-                .then(( res: any ) => {
-                    resolve( res )
-                } )
-                .catch(( error: any ) => {
-                    reject( error )
-                } )
-            */
+            let clientid = props.clientid;
+            let provider = props.provider;
+            let scope, response_mode, response_type, callbackurl, oAuthUrl
+            let loginRequestable
+            let checkAccessTokenRequestable
+            if (clientid && provider) {
+                page.getInstance(Platform).ready().then(() => {                                 // Wait for CDV Plugins to be initialized                
+                    switch(provider) {
+                        case 'azure':
+                            scope = 'openid' +                                                  // Scopes...
+                                    '%20https%3A%2F%2Fgraph.microsoft.com%2FUser.Read' +
+                                    '%20Files.ReadWrite'
+                            response_mode = 'fragment&state=12345&nonce=678910'                 // Ask implicitflow
+                            response_type = 'id_token+token'
+                            callbackurl = page.c8o.endpointConvertigo + "/projects/lib_OAuth/getToken.html"
+                            oAuthUrl = 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize'
+                            loginRequestable = "lib_OAuth.loginAzureAdWithAccessToken"
+                            checkAccessTokenRequestable = "lib_OAuth.checkAccessToken"
+                            break
+                            
+                        default:
+                            page.c8o.log.error("[MB] OAuth login, invalid provider type")
+                    }
+                        
+                    page.routerProvider.doOAuthLogin(oAuthUrl + '?' +                           // Authorize URL
+                        'client_id=' + clientid +                                               // Client ID as registred in the app portal
+                        '&response_type='+ response_type +                                      // We ask for implicit flow
+                        '&scope=' + scope +
+                        '&response_mode=' + response_mode,                                      // Implicit flow
+                        callbackurl,                                                            // the call back URL to check (As declared in the app portal)
+                        loginRequestable,                                                       // The server sequence to be launched to login
+                        checkAccessTokenRequestable,                                            // The server sequence to be launched to check the access token
+                    ).then((response: any )=>{
+                        resolve( response )
+                    })
+                    .catch((error: any) => {
+                        page.c8o.log.error("[MB] OAuth login, Login error" + error)
+                        reject( error )
+                    })
+                })
+            }
+            else {
+                page.c8o.log.error("[MB] OAuth login, Missing Parameters")
+            }
         });
     }
