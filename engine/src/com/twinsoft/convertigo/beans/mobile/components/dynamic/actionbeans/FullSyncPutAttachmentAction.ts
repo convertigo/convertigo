bc6af56b23d1f76f2e4da8d22690b36c0761aadb
Nicolas Albert <nicolasa@convertigo.com>
2018-02-13 16:36:05 +0100

    Ref #5513 - Mobile Builder: Camera action bean HTML5 mode
     * simplify putAttachment to auto adapt some string ( *:// as URL, data:*base64, as base 64)

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/FullSyncPutAttachmentAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/FullSyncPutAttachmentAction.ts
@@ -6,7 +6,7 @@
      * @param vars  , the object which holds variables key-value pairs
      */
     FullSyncPutAttachmentAction(page: C8oPage, props, vars) : Promise<any> {
-        let r:string = props.requestable.substring(props.requestable.indexOf('.')+1);
+        let r:string = props.requestable.substring(props.requestable.indexOf('.') + 1);
         let v:string = 'put_attachment';
         let rvm:string = r + '.' + v;
         const params: Object = {};
@@ -14,8 +14,16 @@
         params["docid"] = props.docid;
         params["name"] = props.name;
         params["content_type"] = props.content_type;
+        
         return new Promise((resolve, reject) => {
-            if(props.content instanceof URL){
+            if (typeof props.content == "string") {
+                if (new RegExp('^[a-z]+://').test(props.content)) {
+                    props.content = new URL(props.content);
+                } else {
+                    props.content = props.content.replace(new RegExp("^data:.*?base64,"), "");
+                }
+            }
+            if (props.content instanceof URL) {
                 let file: File = page.getInstance(File);
                 let fileName = props.content.href.substring(props.content.href.lastIndexOf("/") + 1);
                 let path =  props.content.href.substring(0,props.content.href.lastIndexOf("/") + 1);
@@ -24,13 +32,12 @@
                     params["content"] = new Blob([new Uint8Array(res)], { type: props.content_type });
                    resolve();
                 })
-            }
-            else{
+            } else {
                 params["content"] = props.content;
                 resolve();
             }
-        }).then(()=>{
-            return new Promise((resolve, reject)=>{
+        }).then(() => {
+            return new Promise((resolve, reject) => {
                 delete props.requestable
 
                 if(props.docid == null || props.name == null || props.content_type == null || props.content == null){
