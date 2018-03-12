62463b27dcefe416d4f12ca861bb9694b3eea63e
Charles Grimont <charlesg@convertigo.com>
2018-03-12 15:59:50 +0100

    #5473 InProgress
    * Done logout google plus

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/GooglePlusLoginAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/GooglePlusLoginAction.ts
@@ -7,18 +7,36 @@
      */
     GooglePlusLoginAction(page: C8oPage, props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
-            page.getInstance(Platform).ready().then(() => {  
-                page.getInstance(GooglePlus).login({
-                    "webClientId" : props.webClientId
-                })
-                .then((res: any) => {
-                    page.c8o.log.debug(JSON.stringify(res))
-                    resolve( res )
-                })
-                .catch((err) => {
-                    page.c8o.log.error(JSON.stringify(err))
-                    reject( err )
-                })
+            page.getInstance(Platform).ready().then(() => {
+                // If Action selected is Login
+                if(props.actionGoogle == "Login"){
+                    page.getInstance(GooglePlus).login({
+                        "webClientId" : props.webClientId
+                    })
+                    .then((res: any) => {
+                        page.c8o.log.debug(JSON.stringify(res))
+                        resolve( res )
+                    })
+                    .catch((err) => {
+                        page.c8o.log.error(JSON.stringify(err))
+                        reject( err )
+                    })
+                }
+                //If Action selected is Logout
+                else if(props.actionGoogle == "Logout"){
+                    page.getInstance(GooglePlus).logout()
+                    .then((res: any) => {
+                        page.c8o.log.debug(JSON.stringify(res))
+                        resolve( res )
+                    })
+                    .catch((err) => {
+                        page.c8o.log.error(JSON.stringify(err))
+                        reject( err )
+                    })
+                }
+                else{
+                    reject("[MB] GooglePlusLoginAction: No valid action selected");
+                }
             });
         });
     }
