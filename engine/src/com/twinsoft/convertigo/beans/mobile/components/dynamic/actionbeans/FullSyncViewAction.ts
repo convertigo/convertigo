526ab48699c6dea45edcb40eed594f019aacadbf
Olivier Picciotto <olivierp@convertigo.com>
2018-03-14 11:55:17 +0100

    close #5540: Added error message.. in c8o.log

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/FullSyncViewAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/FullSyncViewAction.ts
@@ -41,7 +41,11 @@
             page.getInstance(Platform).ready().then(() => {     // We may need the CBL plugin so wait for platform ready.
                 page.c8o.finalizeInit().then(()=>{              // To be sure that FullSync initialized properly on CBL
                     page.call("fs://" + rvm, options, null, 500)
-                    .then((res:any) => {resolve(res)}).catch((error:any) => {reject(error)})
+                    .then((res:any) => {resolve(res)}).catch((error:any) => {
+                        page.c8o.log.error("An error occured while executing view '" + ddoc + "/" + view +
+                                "'. One of the most common causes is that the view was not found in local database because data has not been synchronized.")
+                        reject(error)
+                    })
                 })
             });
         });
