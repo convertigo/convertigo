e0b8e0e127bcd05ab4c3a0301abf3b018f66383e
Charles Grimont <charlesg@convertigo.com>
2018-03-16 14:50:36 +0100

    #5547 * Done for GooglePlusLogin * Done for Uninstall

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/GooglePlusLoginAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/GooglePlusLoginAction.ts
@@ -14,28 +14,52 @@
                         "webClientId" : props.webClientId
                     })
                     .then((res: any) => {
-                        page.c8o.log.debug(JSON.stringify(res))
-                        resolve( res )
+                        page.c8o.log.debug("[MB] GooglePlusLoginAction: logged in: " + JSON.stringify(res));
+                        resolve(res);
                     })
                     .catch((err) => {
-                        page.c8o.log.error(JSON.stringify(err))
-                        reject( err )
+                        if(err == "cordova_not_available"){
+                            let resp : any = {}
+                            resp.email = props.mockedResponseE;
+                            resp.userId = props.mockedResponseU;
+                            resp.displayName = props.mockedResponseD;
+                            resp.familyName = props.mockedResponseF;
+                            resp.givenName = props.mockedResponseG;
+                            resp.imageUrl = props.mockedResponseImg;
+                            resp.idToken = props.mockedResponseId;
+                            resp.serverAuthCode = props.mockedResponseS;
+                            resp.accessToken = props.mockedResponseA;
+                            page.router.c8o.log.debug("[MB] GooglePlusLoginAction: cordova isn't available: using mocked response for logged in: " + JSON.stringify(resp));
+                            resolve(resp);
+                        }
+                        else{
+                            page.c8o.log.error("[MB] GooglePlusLoginAction: ",err);
+                            reject(err);
+                        }
+                        
                     })
                 }
                 //If Action selected is Logout
                 else if(props.actionGoogle == "Logout"){
                     page.getInstance(GooglePlus).logout()
                     .then((res: any) => {
-                        page.c8o.log.debug(JSON.stringify(res))
-                        resolve( res )
+                        page.c8o.log.debug("[MB] GooglePlusLoginAction: logged out: " + JSON.stringify(res));
+                        resolve(res);
                     })
                     .catch((err) => {
-                        page.c8o.log.error(JSON.stringify(err))
-                        reject( err )
+                        if(err == "cordova_not_available"){
+                            let resp : any = {}
+                            page.router.c8o.log.debug("[MB] GooglePlusLoginAction: cordova isn't available: using mocked response for logged out: " + props.mockedResponseLogout);
+                            resolve(props.mockedResponseLogout);
+                        }
+                        else{
+                            page.c8o.log.error("[MB] GooglePlusLoginAction: ",err);
+                            reject(err);
+                        }
                     })
                 }
                 else{
-                    reject("[MB] GooglePlusLoginAction: No valid action selected");
+                    reject("[MB] GooglePlusLoginAction: The Action selected does not exists. Please choose one from the listbox.")
                 }
             });
         });
