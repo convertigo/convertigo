ec62ae710298ab4058365116532de8d109095fca
Charles Grimont <charlesg@convertigo.com>
2018-03-16 12:00:06 +0100

    #5444
    * Disallow SMS Action

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/SMSAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/SMSAction.ts
@@ -27,3 +27,74 @@
             })
         });
     }
+    
+    /**
+    "SMSAction" : {
+    "classname": "com.twinsoft.convertigo.beans.mobile.components.UIDynamicAction",
+    "displayName": "SMS",
+    "label": "SMS",
+    "description": "Defines an <i>SMS</i> action component. | The SMS Action allows you to send sms.<br/><u>Supported platorm(s):</u> <ul><li>Android</li><li>iOS</li><li>Windows Phone 8</li></ul><br/>For more information : <a target='_blank' href='https://ionicframework.com/docs/native/sms/'>SMS</a>",
+    "group": "Native Actions",
+      "icon16": "smsaction_color_16x16.png",
+      "icon32": "smsaction_color_32x32.png",
+    "properties": {
+      "phoneNumber": {
+          "label": "phone Number",
+          "description": "The phone number to send sms",
+          "category": "@options",
+          "type": "string",
+          "value": false
+        },
+        "message": {
+          "label": "message",
+          "description": "The message to send",
+          "category": "@options",
+          "type": "string",
+          "value": false
+        },
+        "replaceLineBreaks": {
+          "label": "replace Line Breaks",
+          "description": "Set to true to replace \n by a new line. Default: false",
+          "category": "@sms options",
+          "type": "boolean",
+          "value": "false",
+          
+        },
+        "intent": {
+          "label": "intent",
+          "description": "Set to true to send SMS with the native android SMS messaging. Set to false empty will send the SMS without opening any app.",
+          "category": "@android options",
+          "type": "boolean",
+          "value": "false",
+          "values": [
+              "true",
+              "false"
+              ]
+        }
+        
+    },
+    "config": {
+      "action_ts_imports": [
+          {"from":"@ionic-native/sms","components":["SMS"]}
+      ],
+      "module_ts_imports": [
+          {"from":"@ionic-native/sms","components":["SMS"]}
+      ],
+      "module_ng_imports": [
+      ],
+      "module_ng_providers": [
+          "SMS"
+      ],
+      "package_dependencies": [
+          {"package":"@ionic-native/sms", "version":"4.5.2"}
+      ],
+      "cordova_plugins": [
+        {
+          "plugin":"cordova-sms-plugin",
+          "version":"0.1.11"
+        }
+    ]
+  }
+},
+    
+    *//
