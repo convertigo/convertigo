8bbe363f138d91ffd1d5855356636053759b7559
Nicolas Albert <nicolasa@convertigo.com>
2018-07-17 17:36:04 +0200

    Close #5608 - JS encoding failure for MobileBuilder projects
     * create a empty file to have the file editor (with line number)
     * init the editor with a string
     * refactor a lot
     * fix some template resources

---
+++ engine/src/com/twinsoft/convertigo/beans/core/IJScriptContainer.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2001-2018 Convertigo SA.
+ * 
+ * This program  is free software; you  can redistribute it and/or
+ * Modify  it  under the  terms of the  GNU  Affero General Public
+ * License  as published by  the Free Software Foundation;  either
+ * version  3  of  the  License,  or  (at your option)  any  later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY  or  FITNESS  FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.twinsoft.convertigo.beans.core;
+
+public interface IJScriptContainer {
+	public String getExpression();
+	public void setExpression(String expression);
+	public String getName();
+	
+	public default DatabaseObject getDatabaseObject() {
+		if (this instanceof DatabaseObject) {
+			return (DatabaseObject) this;
+		}
+		return null;
+	}
+	
+	public default String getFullName() {
+		DatabaseObject dbo = getDatabaseObject();
+		return dbo != null ? dbo.getQName() : getName();
+	}
+	
+	public default String getEditorName() {
+		return getName();
+	}
+}
