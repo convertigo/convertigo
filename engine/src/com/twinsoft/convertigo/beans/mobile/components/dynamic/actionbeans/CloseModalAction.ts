837bb4aa1b8307f3ae34df5520af3eb395496009
Mathieu François <mathieuf@convertigo.com>
2018-09-05 18:29:49 +0200

    Close #5637 Also cleaned up ion_object.json for extra commas, breaking JSON Syntax. (Was not causing issues but is much cleaner...)

---
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/CloseModalAction.ts
@@ -0,0 +1,11 @@
+	/*
+     * @param page  , the current page
+     * @param props , the object which holds properties key-value pairs
+     * @param vars  , the object which holds variables key-value pairs
+     */
+    CloseModalAction(page: C8oPageBase, props, vars) : Promise<any> {
+        return new Promise((resolve, reject) => {
+            let view = page.getInstance(ViewController)
+            view.dismiss();
+      	});
+    }
