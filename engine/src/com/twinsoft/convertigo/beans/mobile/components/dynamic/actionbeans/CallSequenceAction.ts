62d604cdb4d2cc8666d9a09c0bf29080adab8cee
Grégory VORBE <fluidz91@yahoo.fr>
2019-01-30 15:17:46 +0100

    Added in CallSequence Action bean two properties : timeout and threshold

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/CallSequenceAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/CallSequenceAction.ts
@@ -7,9 +7,15 @@
      */
     CallSequenceAction(page: C8oPageBase, props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
+            if (props.timeout) {
+                setTimeout(() => {
+                    reject("Connection timeout. Maybe no connection or network too slow.");
+                }, props.timeout);
+            }
             let r:string = props.requestable; let m:string = props.marker;
             let rm:string = r + (m != '' ? '#':'')+ m;
-            page.call(rm,C8oCafUtils.merge({__localCache_priority: props.cachePolicy, __localCache_ttl: props.cacheTtl},vars),null,500)
+            let td:number = props.threshold || 500;
+            page.call(rm,C8oCafUtils.merge({__localCache_priority: props.cachePolicy, __localCache_ttl: props.cacheTtl},vars),null,td)
             .then((res:any) => {resolve(res)}).catch((error:any) => {reject(error)})
         });
     }
