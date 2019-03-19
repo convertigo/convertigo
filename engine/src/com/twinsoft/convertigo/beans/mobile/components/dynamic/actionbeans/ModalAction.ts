26743dc9af93c353cf7140eb077223ab20f760ef
Charles Grimont <grimont.c@gmail.com>
2019-03-19 14:58:39 +0100

    Close #89

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/ModalAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/ModalAction.ts
@@ -11,14 +11,31 @@
             let p:string = q.substring(q.lastIndexOf('.')+1);
             let data = props.data
         
-            let Modal = page.getInstance(ModalController)
-            Modal.create(page.getPageByName(p), data, {
+            let Modal = page.getInstance(ModalController);
+            let modal = Modal.create(page.getPageByName(p), data, {
                 showBackdrop            : props.showBackdrop,
                 enableBackdropDismiss   : props.enableBackdropDismiss,
                 cssClass                : props.cssClass
-            }).present().then((data) => {
-                page.c8o.log.debug("[MB] Modal Page displayed: " + JSON.stringify(data));
-                resolve();
-            })
+            });
+            if(props.blockWhileDisplayed){
+                modal.onDidDismiss(data => {
+                    let dataStr = "";
+                    try{
+                        dataStr = JSON.Stringify(data);
+                    }
+                    catch(err){
+                        // Do nothing
+                    }
+                    page.c8o.log.debug("[MB] Modal page Did dismiss: " + dataStr);
+                    resolve(data);
+                  });
+            }
+            
+            modal.present();
+            page.c8o.log.debug("[MB] Modal Page displayed");
+            if(!props.blockWhileDisplayed){
+                resolve();    
+            }
+            
         });
     }
