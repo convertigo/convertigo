e4f8fed6ad41b403644c985d7f2763b920928120
nathaliel <nathaliel@convertigo.com>
2019-06-21 11:16:34 +0200

    progress #133 - Made a fix for scope in shared action

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/UIActionStack.java
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/UIActionStack.java
@@ -193,9 +193,9 @@ public class UIActionStack extends UIComponent implements IShared {
 			computed += cartridge;
 			computed += "\t"+ functionName + "("+ parameters +"): Promise<any> {" + System.lineSeparator();
 			//computed += "\t\tlet stack = props[\"stack\"];" + System.lineSeparator();
-			//computed += "\t\tlet scope = stack[\"root\"].scope;" + System.lineSeparator();
 			computed += "\t\tlet hasStack = props[\"stack\"] != undefined;" + System.lineSeparator();
 			computed += "\t\tlet stack = hasStack ? props[\"stack\"] : {root: {scope: {}, in: {}, out: event}};" + System.lineSeparator();
+			computed += "\t\tlet scope = stack[\"root\"].scope;" + System.lineSeparator();
 			computed += "\t\tlet parent = props[\"parent\"];" + System.lineSeparator();
 			computed += "\t\tlet out = event;" + System.lineSeparator();
 			computed += "\t\tlet self;" + System.lineSeparator();
