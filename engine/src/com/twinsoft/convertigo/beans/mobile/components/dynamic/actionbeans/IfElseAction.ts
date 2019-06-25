15a1e36712d96eecd97fd3862ebb7978a9d54275
nathaliel <nathaliel@convertigo.com>
2019-06-25 16:34:59 +0200

    progress #137 - Renamed IfThenElse to IfElse

--- engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/IfElseAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/mobile/components/dynamic/actionbeans/IfElseAction.ts
@@ -1,11 +1,11 @@
     /**
-     * Function IfThenElseAction
+     * Function IfElseAction
      *   
      * @param page  , the current page
      * @param props , the object which holds properties key-value pairs
      * @param vars  , the object which holds variables key-value pairs
      */
-    IfThenElseAction(page: C8oPageBase, props, vars) : Promise<any> {
+    IfElseAction(page: C8oPageBase, props, vars) : Promise<any> {
         return new Promise((resolve, reject) => {
             let ok = (props.condition) ? true : false;
             if (props.negate) {
