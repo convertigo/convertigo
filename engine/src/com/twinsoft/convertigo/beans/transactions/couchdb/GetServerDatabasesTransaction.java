362cd21f38acc9dbdb21841bae3aa6811ff41a60
Nicolas Albert <nicolas.albert@gmail.com>
2019-10-01 10:55:24 +0200

    close #206 added new query parameters (startkey, endkey ...)

--- engine/src/com/twinsoft/convertigo/beans/transactions/couchdb/GetServerDatabasesTransaction.java
+++ engine/src/com/twinsoft/convertigo/beans/transactions/couchdb/GetServerDatabasesTransaction.java
@@ -19,12 +19,23 @@
 
 package com.twinsoft.convertigo.beans.transactions.couchdb;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 import javax.xml.namespace.QName;
+
+import com.twinsoft.convertigo.engine.providers.couchdb.CouchClient;
 
 public class GetServerDatabasesTransaction extends AbstractCouchDbTransaction {
 
 	private static final long serialVersionUID = -665777000227521501L;
-
+	
+	private String q_descending = "";
+	private String q_endkey = "";
+	private String q_limit = "";
+	private String q_skip = "";
+	private String q_startkey = "";
+	
 	public GetServerDatabasesTransaction() {
 		super();
 	}
@@ -37,11 +48,61 @@ public class GetServerDatabasesTransaction extends AbstractCouchDbTransaction {
 
 	@Override
 	protected Object invoke() throws Exception {
-		return getCouchClient().getAllDbs();
+		Map<String, String> query = getQueryVariableValues();
+		
+		for (Entry<String, String> entry: query.entrySet()) {
+			if (entry.getKey().contains("key")) {
+				String value = entry.getValue();
+				value = CouchClient.quoteValue(value);
+				entry.setValue(value);
+			}
+		}
+		
+		return getCouchClient().getAllDbs(query);
 	}
 	
 	@Override
 	public QName getComplexTypeAffectation() {
 		return new QName(COUCHDB_XSD_NAMESPACE, "getServerDatabasesType");
+	}
+	
+	public String getQ_descending() {
+		return q_descending;
+	}
+
+	public void setQ_descending(String q_descending) {
+		this.q_descending = q_descending;
+	}
+
+	public String getQ_endkey() {
+		return q_endkey;
+	}
+
+	public void setQ_endkey(String q_endkey) {
+		this.q_endkey = q_endkey;
+	}
+
+	public String getQ_limit() {
+		return q_limit;
+	}
+
+	public void setQ_limit(String q_limit) {
+		this.q_limit = q_limit;
+	}
+
+	public String getQ_skip() {
+		return q_skip;
+	}
+
+	public void setQ_skip(String q_skip) {
+		this.q_skip = q_skip;
+	}
+
+	public String getQ_startkey() {
+		return q_startkey;
+	}
+
+	public void setQ_startkey(String q_startkey) {
+		this.q_startkey = q_startkey;
 	}
 }
