a4abb2b676e5e54c056c4d30d03b5e82eabd07c2
Nicolas Albert <nicolas.albert@gmail.com>
2019-11-15 10:31:18 +0100

    fix #254 new service to prepare local revision added and used

--- engine/src/com/twinsoft/convertigo/engine/admin/services/projects/GetTestPlatform.java
+++ engine/src/com/twinsoft/convertigo/engine/admin/services/projects/GetTestPlatform.java
@@ -43,7 +43,6 @@ import com.twinsoft.convertigo.engine.Engine;
 import com.twinsoft.convertigo.engine.admin.services.XmlService;
 import com.twinsoft.convertigo.engine.admin.services.at.ServiceDefinition;
 import com.twinsoft.convertigo.engine.admin.services.at.ServiceParameterDefinition;
-import com.twinsoft.convertigo.engine.admin.services.mobiles.MobileResourceHelper;
 import com.twinsoft.convertigo.engine.enums.Accessibility;
 import com.twinsoft.convertigo.engine.enums.Visibility;
 import com.twinsoft.convertigo.engine.util.CachedIntrospector;
@@ -123,10 +122,7 @@ public class GetTestPlatform extends XmlService {
 				e_device.setAttribute("classname", platform.getClass().getSimpleName());
 				e_device.setAttribute("displayName", CachedIntrospector.getBeanInfo(platform.getClass()).getBeanDescriptor().getDisplayName());
 				e_device.setAttribute("packageType", platform.getPackageType());
-				
-				MobileResourceHelper mobileResourceHelper = new MobileResourceHelper(request, "mobile/flashupdate", projectName, platform.getName());
-				mobileResourceHelper.prepareFilesForFlashupdate();
-				e_device.setAttribute("revision", mobileResourceHelper.getRevision());
+				e_device.setAttribute("revision", "computing...");
 				
 				e_mobileApplication.appendChild(e_device);
 			}
