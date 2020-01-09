4949b41ce41ab72f565c3a3005568271538d4b86
Nicolas Albert <nicolas.albert@gmail.com>
2020-01-09 11:38:32 +0100

    ref #290 fix minor doc issues

--- eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/dialogs/CouchVariablesComposite.java
+++ eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/dialogs/CouchVariablesComposite.java
@@ -77,14 +77,13 @@ public class CouchVariablesComposite extends ScrolledComposite {
 		
 		/* Fill the Group widget */
 		for (PropertyDescriptor property : propertyDescriptors) {
-			final String name = property.getName();
-			final String description = property.getShortDescription();
-			if (!parametersCouch.contains(name) ){
-
+			String name = property.getName();
+			String description = property.getShortDescription();
+			if (!parametersCouch.contains(name)) {
 				if ( (name.startsWith("q_") || name.startsWith("p_")) ) {
-					if (!parentObject.getClass().getCanonicalName().equals(property.getValue( 
-								MySimpleBeanInfo.BLACK_LIST_PARENT_CLASS))) {
+					if (!parentObject.getClass().getCanonicalName().equals(property.getValue(MySimpleBeanInfo.BLACK_LIST_PARENT_CLASS))) {
 						Group choosenGroup = name.startsWith("q_") ? groupQueries : groupParameters;
+						description = description.replaceFirst("\\|", "");
 						addToComposite(choosenGroup, name, description, false);
 					}
 				} 
