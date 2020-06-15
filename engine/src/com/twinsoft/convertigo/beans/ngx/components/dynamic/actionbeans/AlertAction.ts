1832d59f2bccc77a6f50ba4396af4915e42d375b
nathaliel <nathaliel@convertigo.com>
2020-06-15 16:58:37 +0200

    #183 - Updated ALertAction

--- engine/src/com/twinsoft/convertigo/beans/ngx/components/dynamic/actionbeans/AlertAction.ts
+++ engine/src/com/twinsoft/convertigo/beans/ngx/components/dynamic/actionbeans/AlertAction.ts
@@ -6,68 +6,93 @@
      * @param vars  , the object which holds variables key-value pairs
      */
     AlertAction(page: C8oPageBase, props, vars) : Promise<any> {
-        return new Promise((resolve, reject)=> {
+        
+        function toString(data) {
+            if (data) {
+                try {
+                    return JSON.stringify(data);
+                } catch(e) {
+                    return data.toString();
+                }
+            } else {
+               return "no data"; 
+            }
+        }
+        
+        const presentAlert = async (resolve) => {
             let num = 0;
-            let alertCtrl = page.getInstance(AlertController);
-            let objAlert = {buttons:[]};
-            if(props.title != null){
-                objAlert['title'] = props.title;
-            }
-            if(props.subTitle != null){
-                objAlert['subTitle'] = props.subTitle;
-            }
-            if(props.cssClass != null){
-                objAlert['cssClass'] = props.cssClass;
-            }
-            if(props.message != null){
-                objAlert['message'] = props.message;
-            }
-            if(props.enableBackdropDismiss != null){
-                objAlert['enableBackdropDismiss'] = props.enableBackdropDismiss;
-            }
-            if(props.button1 != null){
-                objAlert['buttons'][num] = {};
-                objAlert['buttons'][num]['text'] = props.button1;
-                if(props.value1 != null){
-                    objAlert['buttons'][num]['handler'] = ()=>{resolve(props.value1)};
+            let buttons = [];
+            if (props.button1 != null) {
+                buttons[num] = {};
+                buttons[num]['text'] = props.button1;
+                if (props.value1 != null) {
+                    buttons[num]['handler'] = () => { return props.value1};
                 }
-                if(props.cancel1 != null){
-                    objAlert['buttons'][num]['role'] = props.cancel1;
+                if (props.cancel1 != null) {
+                    buttons[num]['role'] = props.cancel1;
                 }
-                if(props.css1 != null){
-                    objAlert['buttons'][num]['cssClass'] = props.css1;
+                if (props.css1 != null) {
+                    buttons[num]['cssClass'] = props.css1;
                 }
                 num += 1;
             }
-            if(props.button2 != null){
-                objAlert['buttons'][num] = {};
-                objAlert['buttons'][num]['text'] = props.button2;
-                if(props.value2 != null){
-                    objAlert['buttons'][num]['handler'] = ()=>{resolve(props.value2)};
+            if (props.button2 != null) {
+                buttons[num] = {};
+                buttons[num]['text'] = props.button2;
+                if (props.value2 != null) {
+                    buttons[num]['handler'] = () => { return props.value2};
                 }
-                if(props.cancel2 != null){
-                    objAlert['buttons'][num]['role'] = props.cancel2;
+                if (props.cancel2 != null) {
+                    buttons[num]['role'] = props.cancel2;
                 }
-                if(props.css2 != null){
-                    objAlert['buttons'][num]['cssClass'] = props.css2;
+                if (props.css2 != null) {
+                    buttons[num]['cssClass'] = props.css2;
                 }
                 num +=1;
             }
-            if(props.button3 != null){
-                objAlert['buttons'][num] = {};
-                objAlert['buttons'][num]['text'] = props.button3;
-                if(props.value3 != null){
-                    objAlert['buttons'][num]['handler'] = ()=>{resolve(props.value3)};
+            if (props.button3 != null) {
+                buttons[num] = {};
+                buttons[num]['text'] = props.button3;
+                if (props.value3 != null) {
+                    buttons[num]['handler'] = () => { return props.value3};
                 }
-                if(props.cancel3 != null){
-                    objAlert['buttons'][num]['role'] = props.cancel3;
+                if (props.cancel3 != null) {
+                    buttons[num]['role'] = props.cancel3;
                 }
-                if(props.css3 != null){
-                    objAlert['buttons'][num]['cssClass'] = props.css3;
+                if (props.css3 != null) {
+                    buttons[num]['cssClass'] = props.css3;
                 }
             }
- 
-            let alert = alertCtrl.create(objAlert);
-            alert.present();
+            
+            let alertController = page.getInstance(AlertController)
+            const alert = await alertController.create({
+                mode              : props.mode ? props.mode : undefined,
+                header            : props.header,
+                subHeader         : props.subHeader,
+                message           : props.message,
+                keyboardClose     : props.keyboardClose,
+                backdropDismiss   : props.backdropDismiss,
+                animated          : props.animated,
+                enterAnimation    : props.enterAnimation ? props.enterAnimation : undefined,
+                leaveAnimation    : props.leaveAnimation ? props.leaveAnimation : undefined,
+                cssClass          : props.cssClass ? props.cssClass : '',
+                translucent       : props.translucent,
+                buttons           : buttons
+            });
+
+            alert.onDidDismiss().then((data) => {
+                page.c8o.log.debug("[MB] Alert  dismissed: " + toString(data));
+                resolve(data)
+            })
+            
+            return await alert.present();
+          }
+        
+        
+        return new Promise((resolve, reject)=> {
+            Promise.resolve(presentAlert(resolve))
+            .then(() => {
+                page.c8o.log.debug("[MB] Alert displayed: " + toString(props.message));
+            }).catch((error:any) => {reject(error)})
         });
     }
