ad3e456a6ac2d1ff9d566b031dbe2f5dc3b392df
Convertigo <olivierp@convertigo.com>
2020-07-03 18:41:30 +0200

    #183 show "build in progress..." message when HMR updates occurs.

--- convertigo-templates/mobilebuilder_tpl_7_9_0_Ionic5/ionicTpl/src/hmr.ts
+++ convertigo-templates/mobilebuilder_tpl_7_9_0_Ionic5/ionicTpl/src/hmr.ts
@@ -12,4 +12,38 @@ export const hmrBootstrap = (module: any, bootstrap: () => Promise<NgModuleRef<a
     ngModule.destroy();
     makeVisible();
   });
+  module.hot.addStatusHandler(status => {
+	  // React to the current status...
+	  if (window["HmrBuildStatus"] == undefined || window["HmrBuildStatus"] != "idle") {
+		window["HmrBuildStatus"] = status;
+		if (status == "check") {
+		  	console.log("*** HMR Building =" + status)
+			var buildDiv = document.createElement('span')
+			buildDiv.id = "BuildDiv"
+			buildDiv.innerHTML = `
+				<span style="
+					font-size: 30px;
+				    color: white;
+				    background-color: #222222;
+				    z-index: 100;
+				    position: absolute;
+				    top: 50%;
+				    left: 50%;
+				    transform: translate(-50%,-50%);					
+					border: solid 1px white;
+					padding: 5px;
+					border-radius: 5px">Build in progress..
+				</span>"
+			`
+			document.body.appendChild(buildDiv)
+		}
+	  } else {
+		window["HmrBuildStatus"] = status;
+		console.log("*** HMR Build done")
+		var buildDiv = document.getElementById("BuildDiv")
+		if (buildDiv) {
+			buildDiv.parentNode.removeChild(buildDiv)
+		}
+	 }
+  });
 };
