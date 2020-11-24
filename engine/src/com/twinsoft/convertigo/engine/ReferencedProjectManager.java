cc97eebd256d6152fdf7ec77fd8d11d7406d0590
Nicolas Albert <nicolas.albert@gmail.com>
2020-11-24 17:37:07 +0100

    ref #413 default use the github zip reference for missing tpl

--- engine/src/com/twinsoft/convertigo/engine/ReferencedProjectManager.java
+++ engine/src/com/twinsoft/convertigo/engine/ReferencedProjectManager.java
@@ -117,7 +117,7 @@ public class ReferencedProjectManager {
 		if (prjRef == null) {
 			prjRef = new ProjectSchemaReference();
 			if (projectName.startsWith("mobilebuilder_tpl_")) {
-				prjRef.setProjectName(projectName + "=https://github.com/convertigo/c8oprj-mobilebuilder-tpl.git:branch=" + projectName);
+				prjRef.setProjectName(projectName + "=https://github.com/convertigo/c8oprj-mobilebuilder-tpl/archive/" + projectName + ".zip");
 			} else {
 				prjRef.setProjectName(projectName);
 			}
