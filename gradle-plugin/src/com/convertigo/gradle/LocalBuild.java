d23b6674200f618297dc04eafba372a81206812a
Nicolas Albert <nicolas.albert@gmail.com>
2021-02-08 12:35:28 +0100

    ref #382 fix the default path for ios and android signing info

--- gradle-plugin/src/com/convertigo/gradle/LocalBuild.java
+++ gradle-plugin/src/com/convertigo/gradle/LocalBuild.java
@@ -131,7 +131,7 @@ public class LocalBuild extends ConvertigoTask {
 			setPlatforms(platforms);
 		} catch (Exception e) {}
 		
-		iosProvisioningProfile = new File(getProject().getBuildDir(), "ios.mobileprovision");
+		iosProvisioningProfile = new File(getProject().getProjectDir(), "ios.mobileprovision");
 		try {
 			iosProvisioningProfile = new File(getProject().getProperties().get("convertigo.localBuild.iosProvisioningProfile").toString());
 		} catch (Exception e) {}
@@ -140,7 +140,7 @@ public class LocalBuild extends ConvertigoTask {
 			iosSignIdentity = getProject().getProperties().get("convertigo.localBuild.iosSignIdentity").toString();
 		} catch (Exception e) {}
 		
-		androidKeystore = new File(getProject().getBuildDir(), "android.keystore");
+		androidKeystore = new File(getProject().getProjectDir(), "android.keystore");
 		try {
 			androidKeystore = new File(getProject().getProperties().get("convertigo.localBuild.androidKeystore").toString());
 		} catch (Exception e) {}
