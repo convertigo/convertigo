db886cb26a26bc315b94223abc73d2fcbdaa2b5b
Nicolas Albert <nicolas.albert@gmail.com>
2021-03-10 16:23:34 +0100

    ref #400 detect cluster mode and perform a mango check before listener

--- engine/src/com/twinsoft/convertigo/beans/couchdb/FullSyncFilterListener.java
+++ engine/src/com/twinsoft/convertigo/beans/couchdb/FullSyncFilterListener.java
@@ -62,7 +62,7 @@ public class FullSyncFilterListener extends AbstractFullSyncFilterListener {
 			
 			int len = ids.length();
 			
-			Map<String, String> query = new HashMap<String, String>(2);
+			Map<String, String> query = new HashMap<String, String>(3);
 			query.put("filter", filter);
 			query.put("include_docs", "true");
 			query.put("conflicts", "true");
@@ -74,6 +74,25 @@ public class FullSyncFilterListener extends AbstractFullSyncFilterListener {
 					JSONArray doc_ids = getChunk(ids, i);
 					int ids_len = doc_ids.length();
 					i += ids_len;
+					
+					if (client.isServerCluster()) {
+						JSONObject body = new JSONObject();
+						body.put("update", false);
+						body.put("stable", true);
+						body.put("r", 100);
+						JSONObject in = new JSONObject();
+						in.put("$in", doc_ids);
+						JSONObject selector = new JSONObject();
+						selector.put("_id", in);
+						body.put("selector", selector);
+						JSONArray fields = new JSONArray();
+						fields.put("_id");
+						fields.put("_rev");
+						body.put("fields", fields);
+						JSONObject r = client.postFind(db, body);
+						Engine.logBeans.debug("(FullSyncListener) Listener \"" + getName() + "\" : [" + db + "] post find with " + body + "\n" + r);
+						Thread.sleep(50);
+					}
 					
 					Engine.logBeans.debug("(FullSyncFilterListener) Listener \"" + getName() + "\" : [" + db + "] post filter '" + ddoc + "/" + filter + "' for _id keys " + doc_ids);
 					JSONObject json = client.postChanges(db, query, CouchKey.doc_ids.put(new JSONObject(), doc_ids));
