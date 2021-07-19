8ef49bc59f45bfac76f7b7fe36b774399eb74d0a
nathaliel <nathaliel@convertigo.com>
2021-07-19 17:58:00 +0200

    ref #505 - Fixed sources refactoring and viewer refresh (multiple app)

--- eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/projectexplorer/model/MobileUIComponentTreeObject.java
+++ eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/projectexplorer/model/MobileUIComponentTreeObject.java
@@ -1146,6 +1146,17 @@ public class MobileUIComponentTreeObject extends MobileComponentTreeObject imple
 									}
 								}
 							}
+							if (dbo instanceof UIComponent) {
+								if (!newValue.equals(oldValue)) {
+									try {
+										String oldName = (String)oldValue;
+										String newName = (String)newValue;
+										if (getObject().updateSmartSource("\\?\\."+oldName, "?."+newName)) {
+											sourcesUpdated = true;
+										}
+									} catch (Exception e) {}
+								}
+							}
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -1258,6 +1269,7 @@ public class MobileUIComponentTreeObject extends MobileComponentTreeObject imple
 												uicv.setName((String) newValue);
 												uicv.hasChanged = true;
 												
+												hasBeenModified(true);
 												viewer.refresh();
 												markMainAsDirty(udi);
 												
@@ -1298,6 +1310,7 @@ public class MobileUIComponentTreeObject extends MobileComponentTreeObject imple
 												uicv.setName((String) newValue);
 												uicv.hasChanged = true;
 												
+												hasBeenModified(true);
 												viewer.refresh();
 												markMainAsDirty(uus);
 												
@@ -1342,6 +1355,7 @@ public class MobileUIComponentTreeObject extends MobileComponentTreeObject imple
 														uicv.setName((String) newValue);
 														uicv.hasChanged = true;
 														
+														hasBeenModified(true);
 														viewer.refresh();
 														markMainAsDirty(uia);
 														
