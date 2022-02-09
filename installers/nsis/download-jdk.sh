d4cde759cb0f2886d3e61047df1c8a53eb0c8b86
Nicolas Albert <nicolas.albert@gmail.com>
2022-02-09 16:29:42 +0100

    ref #506 update browser, tomcat and jdk17

--- installers/nsis/download-jdk.sh
+++ installers/nsis/download-jdk.sh
@@ -4,7 +4,7 @@
 mkdir -p /tmp/c8o_jre
 cd /tmp/c8o_jre
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_windows_hotspot_17.0.1_12.zip
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_windows_hotspot_17.0.2_8.zip
 
 curl -sfSL -o jdk.zip ${JDK_URL}
 unzip -q jdk.zip
@@ -12,13 +12,13 @@ rm jdk.zip
 mv jdk* win
 rm -rf win/jmods win/lib/src.zip
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_mac_hotspot_17.0.1_12.tar.gz
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_mac_hotspot_17.0.2_8.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac
 rm -rf jdk* mac/lib/src.zip mac/man mac/jmods
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_17.0.1_12.tar.gz
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.2%2B8/OpenJDK17U-jdk_x64_linux_hotspot_17.0.2_8.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 rm jdk.tar.gz
