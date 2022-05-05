9eecc66c2feeb6ff198d4394c6c9a88b36fc7818
Nicolas Albert <nicolas.albert@gmail.com>
2022-05-05 11:12:17 +0200

    Official 8.0.0 release

--- engine/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ engine/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -25,7 +25,7 @@ public class ProductVersion {
     public static String majorProductVersion = "8";
     public static String minorProductVersion = "0";
     public static String servicePack = "0";
-    public static String tag = "beta";
+    public static String tag = null;
     public static String revision = null;
     
     public static String productVersion =
