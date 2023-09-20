21225c46a965c005eb304dcfd05a63f3fb807c06
Nicolas Albert <nicolas.albert@gmail.com>
2023-09-20 15:55:34 +0200

    ref #750 use latest Eclipse, JVM and tomcat

--- installers/nsis/download-jdk.sh
+++ installers/nsis/download-jdk.sh
@@ -4,26 +4,26 @@
 mkdir -p /tmp/c8o_jre
 cd /tmp/c8o_jre
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.7_7.zip
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8.1%2B1/OpenJDK17U-jdk_x64_windows_hotspot_17.0.8.1_1.zip
 curl -sfSL -o jdk.zip ${JDK_URL}
 unzip -q jdk.zip
 rm jdk.zip
 mv jdk* win
 rm -rf win/jmods win/lib/src.zip
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_mac_hotspot_17.0.7_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8.1%2B1/OpenJDK17U-jdk_x64_mac_hotspot_17.0.8.1_1.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac
 rm -rf jdk* mac/lib/src.zip mac/man mac/jmods
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_aarch64_mac_hotspot_17.0.7_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8.1%2B1/OpenJDK17U-jdk_aarch64_mac_hotspot_17.0.8.1_1.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac-arm
 rm -rf jdk* mac-arm/lib/src.zip mac-arm/man mac-arm/jmods
 
-JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.7%2B7/OpenJDK17U-jdk_x64_linux_hotspot_17.0.7_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.8.1%2B1/OpenJDK17U-jdk_x64_linux_hotspot_17.0.8.1_1.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 rm jdk.tar.gz
