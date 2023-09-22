df0e66ac8103cec6fe0f3de74ff303d391d41ff0
Nicolas Albert <nicolas.albert@gmail.com>
2023-09-22 10:39:35 +0200

    InProgress #766 add a blank project for the new Studio UI

---
+++ convertigo-studio-web/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '../app.postcss';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<slot />
---
+++ convertigo-studio-web/tailwind.config.js
@@ -0,0 +1,63 @@
+import { join } from 'path'
+
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+import { skeleton } from '@skeletonlabs/tw-plugin'
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./src/**/*.{html,js,svelte,ts}', join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		forms,
+		typography,
+		skeleton({
+			themes: {
+				preset: [
+					{
+						name: 'skeleton',
+						enhancements: true,
+					},
+					{
+						name: 'wintry',
+						enhancements: true,
+					},
+					{
+						name: 'modern',
+						enhancements: true,
+					},
+					{
+						name: 'hamlindigo',
+						enhancements: true,
+					},
+					{
+						name: 'rocket',
+						enhancements: true,
+					},
+					{
+						name: 'sahara',
+						enhancements: true,
+					},
+					{
+						name: 'gold-nouveau',
+						enhancements: true,
+					},
+					{
+						name: 'vintage',
+						enhancements: true,
+					},
+					{
+						name: 'seafoam',
+						enhancements: true,
+					},
+					{
+						name: 'crimson',
+						enhancements: true,
+					},
+				],
+			},
+		}),
+	],
+};
