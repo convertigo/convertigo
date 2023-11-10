67b89353488c125784452671ab796707b1df509f
Nicolas Albert <nicolas.albert@gmail.com>
2023-11-10 15:12:20 +0100

    ref #766 "npm run dev" default to the 18080 port for convertigo proxy

--- convertigo-studio-web/vite.config.js
+++ convertigo-studio-web/vite.config.js
@@ -3,21 +3,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
-let c8oPort = '18080';
-try {
-	c8oPort = process.argv.filter(s => s.startsWith('--c8oPort=')).map(s => s.substring(10)).join();
-} catch (e) {
-	console.error('dynamic c8oPort failed', e);
-}
-
 function determineProxy() {
+	const c8oPort = process.argv.filter(s => s.startsWith('--c8oPort=')).map(s => s.substring(10))[0] ?? '18080';
 	const isWSL = process.platform !== 'win32' && process.env.WSL_DISTRO_NAME != undefined;
+	let convertigoUrl = `http://localhost:${c8oPort}`
 	if (isWSL) {
 		// Configuration de proxy pour WSL2
-		return `http://172.29.80.1:${c8oPort}`;
+		convertigoUrl = `http://172.29.80.1:${c8oPort}`;
 	}
-	// Configuration par défaut pour d'autres environnements
-	return `http://localhost:${c8oPort}`;
+	console.log(`Proxy for /convertigo → ${convertigoUrl}`);
+	return convertigoUrl;
 }
 
 export default defineConfig({
