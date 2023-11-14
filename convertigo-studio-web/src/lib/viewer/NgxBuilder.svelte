6c2675cfee0399e2e95496672c837dcee08dc72c
Nicolas Albert <nicolas.albert@gmail.com>
2023-11-14 18:26:59 +0100

    ref #766 fist version of ngx builder with WebSocket and iFrame

--- convertigo-studio-web/src/lib/viewer/NgxBuilder.svelte
+++ convertigo-studio-web/src/lib/viewer/NgxBuilder.svelte
@@ -1,18 +1,27 @@
 <script>
 	import { selectedId } from '$lib/treeview/treeStore';
 	import { getUrl } from '$lib/utils/service';
-	import { onMount } from 'svelte';
+	import { AppBar, AppRail, AppRailAnchor, AppShell, ProgressRadial } from '@skeletonlabs/skeleton';
+	import { afterUpdate, onMount } from 'svelte';
 
 	let project = '';
 	let message = '';
 	/** @type {WebSocket|null} */
 	let ws = null;
 
+	/** @type {HTMLIFrameElement} */
+	let iframe;
+
+	let iframeUrl = null;
+	let progress = 0;
+	let output = '';
+
 	$: project = $selectedId.replace(/(.*?)[\.:].*/, '$1');
 
 	$: {
 		if (project != '' && ws?.readyState == WebSocket.OPEN) {
-			ws.send('changed to ' + project);
+			message = '';
+			runAction('attach');
 		}
 	}
 
@@ -24,25 +33,112 @@
 			)}studio.ngxbuilder.WsBuilder?__xsrfToken=${localStorage.getItem('x-xsrf-token')}`
 		);
 		ws.onopen = () => {
-			ws?.send('hello ' + project);
+			ws = ws;
 		};
 		ws.onmessage = (msg) => {
-			message = message + `${msg.data}\n`;
+			try {
+				const { type, value } = JSON.parse(msg.data);
+				switch (type) {
+					case 'output':
+						output = value;
+					case 'log':
+						message = message + `${value}\n`;
+						break;
+					case 'progress':
+						progress = Number.parseFloat(value);
+						break;
+					case 'load':
+						iframeUrl = value;
+						break;
+				}
+			} catch (e) {
+				message = message + `Exception: ${e}\n`;
+			}
 		};
 		return () => {
 			ws?.close();
 			ws = null;
 		};
 	});
+
+	let textarea;
+	let startY, startHeight;
+
+	/** @param {MouseEvent} e*/
+	function onDrag(e) {
+		startY = e.clientY;
+		startHeight = parseInt(document.defaultView?.getComputedStyle(textarea)?.height ?? '', 10);
+		document.documentElement.addEventListener('mousemove', doDrag, false);
+		document.documentElement.addEventListener('mouseup', stopDrag, false);
+	}
+
+	/** @param {MouseEvent} e*/
+	function doDrag(e) {
+		textarea.style.height = startHeight - (e.clientY - startY) + 'px';
+	}
+
+	function stopDrag() {
+		document.documentElement.removeEventListener('mousemove', doDrag, false);
+		document.documentElement.removeEventListener('mouseup', stopDrag, false);
+	}
+
+	afterUpdate(() => {
+		if (textarea) {
+			textarea.scrollTop = textarea.scrollHeight;
+		}
+	});
+
+	function runAction(action) {
+		progress = 0;
+		iframeUrl = null;
+		output = 'Not running';
+		ws?.send(
+			JSON.stringify({
+				action,
+				project
+			})
+		);
+	}
 </script>
 
 {#if project == ''}
 	<h1>No project selected!</h1>
-{:else if message != ''}
-	<h1>Building project <u>{project}</u>!</h1>
-{:else}
+{:else if message == ''}
 	<h1>Checking for project <u>{project}</u>!</h1>
 {/if}
-{#if message != ''}
-	<textarea readonly={true} class="bg-white dark:bg-gray-700 h-full">{message}</textarea>
+{#if project != '' && message != ''}
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
+	<AppShell>
+		<svelte:fragment slot="header">
+			<AppBar>NGX Builder for <u>{project}</u></AppBar>
+		</svelte:fragment>
+		<svelte:fragment slot="sidebarLeft">
+			<AppRail>
+				<AppRailAnchor on:click={() => runAction('build_dev')}>Run</AppRailAnchor>
+				<AppRailAnchor on:click={() => runAction('kill')}>Kill</AppRailAnchor>
+			</AppRail>
+		</svelte:fragment>
+		{#if iframeUrl == null}
+			<div class="flex flex-col justify-around items-center h-full w-full">
+				<ProgressRadial value={progress}>{progress}%</ProgressRadial>
+				<div class="card variant-ghost w-3/4 py-1 text-center">{output}</div>
+			</div>
+		{:else}
+			<iframe bind:this={iframe} class="h-full w-full" title="test" src={iframeUrl} />
+		{/if}
+		<svelte:fragment slot="footer">
+			<div class="draggable border-4 w-full" on:mousedown={onDrag} />
+			<textarea
+				bind:this={textarea}
+				readonly={true}
+				class="bg-white dark:bg-gray-700 h-full w-full resize-none">{message}</textarea
+			>
+		</svelte:fragment>
+	</AppShell>
 {/if}
+
+<style>
+	.draggable {
+		cursor: grab;
+	}
+</style>
--- engine/src/com/twinsoft/convertigo/engine/admin/services/studio/ngxbuilder/WsBuilder.java
+++ engine/src/com/twinsoft/convertigo/engine/admin/services/studio/ngxbuilder/WsBuilder.java
@@ -1,40 +1,326 @@
 package com.twinsoft.convertigo.engine.admin.services.studio.ngxbuilder;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.nio.charset.Charset;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.websocket.Session;
 
-import com.twinsoft.convertigo.engine.Engine;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.twinsoft.convertigo.beans.core.Project;
 import com.twinsoft.convertigo.engine.AuthenticatedSessionManager.Role;
+import com.twinsoft.convertigo.engine.Engine;
 import com.twinsoft.convertigo.engine.admin.services.WebSocketService;
 import com.twinsoft.convertigo.engine.admin.services.at.ServiceDefinition;
+import com.twinsoft.convertigo.engine.mobile.MobileBuilder;
+import com.twinsoft.convertigo.engine.util.NetworkUtils;
+import com.twinsoft.convertigo.engine.util.ProcessUtils;
 
-
-@ServiceDefinition(name = "WsBuilder", roles = { Role.WEB_ADMIN}, parameters = {}, returnValue = "")
+@ServiceDefinition(name = "WsBuilder", roles = { Role.WEB_ADMIN }, parameters = {}, returnValue = "")
 public class WsBuilder extends WebSocketService {
+	static class Build implements Runnable {
+		private static final Set<Integer> usedPort = new HashSet<>();
+		private static Pattern pRemoveEchap = Pattern.compile("\\x1b\\[\\d+m");
+		private static Pattern pIsBrowserOpenable = Pattern.compile(".*?open your browser on (http\\S*).*");
+
+		String projectName;
+		Project project;
+		String url;
+		Thread thread;
+		int portNode;
+		String baseUrl;
+		private Collection<Process> processes = new LinkedList<>();
+
+		Set<WsBuilder> clients = new HashSet<>();
+
+		Build(String projectName) {
+			this.projectName = projectName;
+		}
+
+		public void start() {
+			if (thread != null) {
+				return;
+			}
+			thread = new Thread(this);
+			thread.setName("Build of " + projectName);
+			thread.setDaemon(true);
+			thread.start();
+		}
+
+		@Override
+		public void run() {
+			log("build thread started");
+			var mutex = new Object();
+			MobileBuilder mb = null;
+			try {
+				project = Engine.theApp.databaseObjectsManager.getOriginalProjectByName(projectName);
+				mb = project.getMobileBuilder();
+				if (!mb.isInitialized()) {
+					throw new Exception("Mobile Builder not initialized");
+				}
+				mb.setBuildMutex(mutex);
+				mb.startBuild();
+
+				var displayObjectsMobile = new File(project.getDirPath(), "DisplayObjects/mobile");
+				displayObjectsMobile.mkdirs();
+
+				var ionicDir = new File(project.getDirPath(), "_private/ionic");
+//				var nodeModules = new File(ionicDir, "node_modules");
+
+				var nodeVersion = ProcessUtils.getNodeVersion(project);
+				var nodeDir = ProcessUtils.getDefaultNodeDir();
+				try {
+					nodeDir = ProcessUtils.getNodeDir(nodeVersion, (r, t, x) -> {
+						appendOutput("Downloading nodejs " + nodeVersion + ": " + Math.round((r * 100f) / t) + "%");
+					});
+				} catch (Exception e1) {
+				}
+
+				{
+					String versions = "Will use nodejs " + ProcessUtils.getNodeVersion(nodeDir) + " and npm "
+							+ ProcessUtils.getNpmVersion(nodeDir);
+					appendOutput(versions);
+					Engine.logStudio.info(versions);
+				}
+
+				String path = nodeDir.getAbsolutePath();
+
+				terminateNode(false);
+
+				var pb = ProcessUtils.getNpmProcessBuilder(path, "npm", "run", "ionic:serve");
+
+				List<String> cmd = pb.command();
+				synchronized (usedPort) {
+					int port = (Math.abs(ionicDir.getAbsolutePath().hashCode()) % 10000) + 40000;
+					cmd.add("--");
+					usedPort.clear();
+					portNode = NetworkUtils.nextAvailable(port, usedPort);
+					cmd.add("--port=" + portNode);
+					cmd.add("--host=0.0.0.0");
+				}
+
+				// #183 add useless option to help terminateNode method to find the current path
+				cmd.add("--ssl-key=" + new File(project.getDirFile(), "DisplayObjects/mobile").getAbsolutePath());
+
+				pb.redirectErrorStream(true);
+				pb.directory(ionicDir);
+				Process p = pb.start();
+				processes.add(p);
+
+				var matcher = Pattern.compile("(\\d+)% (.*)").matcher("");
+				var br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line;
+
+				StringBuilder sb = null;
+
+				while ((line = br.readLine()) != null) {
+					line = pRemoveEchap.matcher(line).replaceAll("");
+					if (StringUtils.isNotBlank(line)) {
+						Engine.logStudio.info(line);
+						if (line.startsWith("Error: ")) {
+							sb = new StringBuilder();
+						}
+						if (sb != null) {
+							if (line.contains("Failed to compile.")) {
+								sb.append(line);
+								error(sb.toString());
+								sb = null;
+							} else {
+								sb.append(line + "\n");
+							}
+						}
+
+						matcher.reset(line);
+						if (matcher.find()) {
+							progress(Integer.parseInt(matcher.group(1)));
+							appendOutput(matcher.group(2));
+						} else {
+							appendOutput(line);
+						}
+						if (line.matches(".*Compiled .*successfully.*")) {
+							progress(100);
+							error(null);
+							synchronized (mutex) {
+								mutex.notify();
+							}
+							mb.buildFinished();
+						}
+
+						Matcher m = pIsBrowserOpenable.matcher(line);
+						if (m.matches()) {
+							String sGroup = m.group(1);
+							baseUrl = sGroup.substring(0, sGroup.lastIndexOf("/"));
+							doLoad();
+						}
+					}
+				}
+
+//				if (buildCount == this.buildCount) {
+//					appendOutput("\\o/");
+//				} else {
+//					appendOutput("previous build canceled !");
+//				}
+			} catch (Exception e) {
+				log("Exception: " + e);
+			} finally {
+				synchronized (mutex) {
+					mutex.notify();
+				}
+				mb.setBuildMutex(null);
+				if (mb != null) {
+					mb.buildFinished();
+				}
+			}
+			synchronized (clients) {
+				clients.forEach(c -> remove(c));
+			}
+		}
+
+		private void doLoad() {
+			send("load", baseUrl);
+		}
+
+		private void progress(int parseInt) {
+			send("progress", "" + parseInt);
+		}
+
+		private void error(String string) {
+			if (string != null) {
+				log("error: " + string);
+			}
+		}
+
+		private void appendOutput(String string) {
+			send("output", string);
+		}
+
+		private void log(String log) {
+			send("log", log);
+		}
+
+		private void send(String type, String value) {
+			synchronized (clients) {
+				for (var client : clients) {
+					try {
+						WsBuilder.send(client.session, type, value);
+					} catch (Exception e) {
+					}
+				}
+			}
+		}
+
+		public void add(WsBuilder client) {
+			synchronized (clients) {
+				client.build = this;
+				clients.add(client);
+			}
+		}
+
+		public void remove(WsBuilder client) {
+			synchronized (clients) {
+				client.build = null;
+				clients.remove(client);
+			}
+		}
+
+		void terminateNode(boolean prodOnly) {
+			baseUrl = null;
+			String projectName = new File(project.getDirPath()).getName();
+			int retry = 10;
+			try {
+				while (retry-- > 0) {
+					if (Engine.isWindows()) {
+						String prod = prodOnly ? " AND CommandLine Like '%--watch%'" : "";
+						Process process = new ProcessBuilder("wmic", "PROCESS", "WHERE",
+								"Name='node.exe' AND CommandLine Like '%\\\\" + projectName + "\\\\DisplayObjects\\\\%'"
+										+ prod,
+								"CALL", "TERMINATE").redirectError(Redirect.DISCARD).start();
+						String output = IOUtils.toString(process.getInputStream(), Charset.defaultCharset());
+						process.waitFor();
+						int id = output.indexOf('\n');
+						if (id == -1 || output.indexOf('\n', id) == -1) {
+							retry = 0;
+						}
+
+						process = new ProcessBuilder("wmic", "PROCESS", "WHERE",
+								("Name='node.exe' AND CommandLine Like '%\\\\" + projectName
+										+ "\\\\DisplayObjects\\\\%'" + prod).replace("\\", "\\\\"),
+								"CALL", "TERMINATE").redirectError(Redirect.DISCARD).start();
+						output = IOUtils.toString(process.getInputStream(), Charset.defaultCharset());
+						process.waitFor();
+						id = output.indexOf('\n');
+						if (id == -1 || output.indexOf('\n', id) == -1) {
+							retry = 0;
+						}
+					} else {
+						String prod = prodOnly ? " | grep -e \"--watch\" -e \":watch\"" : "";
+						Process process = new ProcessBuilder("/bin/bash", "-c",
+								"ps -e" + (Engine.isLinux() ? "f" : "") + " | grep -v \"sed -n\"" + prod
+										+ " | sed -n -E \"s,[^0-9]*([0-9]+).*(node|npm|ng).*/" + projectName
+										+ "/DisplayObjects/.*,\\1,p\" | xargs kill")
+								.redirectError(Redirect.DISCARD).redirectOutput(Redirect.DISCARD).start();
+						int code = process.waitFor();
+						if (code == 0) {
+							retry = 0;
+						}
+					}
+				}
+				synchronized (usedPort) {
+					usedPort.remove(portNode);
+				}
+			} catch (Exception e) {
+				Engine.logStudio.warn("Failed to terminate the node server", e);
+			}
+		}
+	}
+
+	static Map<String, Build> builds = new HashMap<>();
+
+	Session session;
+	String project;
+	Build build;
 
 	@Override
 	public void onOpen(Session session) {
 		Engine.logAdmin.warn("onOpen");
-		new Thread(() -> {
-			while (session.isOpen()) {
-				try {
-					Thread.sleep(2000);
-					send("pong");
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-		}).start();
+		this.session = session;
 	}
 
 	@Override
 	public void onMessage(String message, Session session) {
 		Engine.logAdmin.warn("onMessage: " + message);
 		try {
-			send(message);
-		} catch (IOException e) {
+			var json = new JSONObject(message);
+			project = json.getString("project");
+			var action = json.getString("action");
+			switch (action) {
+			case "attach":
+				onAttach();
+				break;
+			case "build_dev":
+				onBuildDev();
+				break;
+			case "kill":
+				onKill();
+				break;
+			}
+		} catch (Exception e) {
 			Engine.logAdmin.error("failed to send back message", e);
 		}
 	}
@@ -42,11 +328,67 @@ public class WsBuilder extends WebSocketService {
 	@Override
 	public void onClose(Session session) {
 		Engine.logAdmin.warn("onClose");
+		if (build != null) {
+			build.remove(this);
+		}
 	}
 
 	@Override
 	public void onError(Throwable throwable, Session session) {
 		Engine.logAdmin.warn("onError", throwable);
+		if (build != null) {
+			build.remove(this);
+		}
 	}
 
+	void send(String action, String message) throws JSONException, IOException {
+		send(session, action, message);
+	}
+
+	private void onAttach() throws Exception {
+		if (build != null) {
+			build.remove(this);
+		}
+		synchronized (builds) {
+			build = builds.get(project);
+		}
+
+		if (build == null) {
+			send("log", "No builder running for " + project);
+			return;
+		}
+
+		build.add(this);
+		send("log", "Builder attached for " + project);
+		if (build.baseUrl != null) {
+			send("load", build.baseUrl);
+		}
+	}
+
+	private void onBuildDev() throws Exception {
+		if (build != null) {
+			send("log", "prevents starting a new build of " + project);
+//			return;
+		}
+		synchronized (builds) {
+			build = new Build(project);
+			builds.put(project, build);
+		}
+		build.add(this);
+		build.start();
+	}
+
+	private void onKill() {
+		if (build != null) {
+			build.log("killing builds for " + project);
+			build.terminateNode(false);
+		}
+	}
+
+	static void send(Session session, String action, String message) throws JSONException, IOException {
+		var json = new JSONObject();
+		json.put("type", action);
+		json.put("value", message);
+		session.getBasicRemote().sendText(json.toString());
+	}
 }
