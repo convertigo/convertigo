02f556964709a7222dfa309306eb7830c080ef64
Nicolas Albert <nicolas.albert@gmail.com>
2023-12-12 19:19:16 +0100

    ref #750 fix JDK links and remove arm32 target

--- installers/nsis/download-jdk.sh
+++ installers/nsis/download-jdk.sh
@@ -10,19 +10,19 @@ rm jdk.zip
 mv jdk* win
 rm -rf win/jmods win/lib/src.zip
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK17U-jdk_x64_mac_hotspot_21.0.1_12.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK21U-jdk_x64_mac_hotspot_21.0.1_12.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac
 rm -rf jdk* mac/lib/src.zip mac/man mac/jmods
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK17U-jdk_aarch64_mac_hotspot_21.0.1_12.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK21U-jdk_aarch64_mac_hotspot_21.0.1_12.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac-arm
 rm -rf jdk* mac-arm/lib/src.zip mac-arm/man mac-arm/jmods
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK17U-jdk_x64_linux_hotspot_21.0.1_12.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.1%2B12/OpenJDK21U-jdk_x64_linux_hotspot_21.0.1_12.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 rm jdk.tar.gz
