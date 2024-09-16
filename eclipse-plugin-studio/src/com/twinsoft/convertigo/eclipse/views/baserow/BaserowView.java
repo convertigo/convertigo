3c9a19102389817fdfef3011adc1dc5c86b57791
Nicolas Albert <nicolas.albert@gmail.com>
2024-09-16 14:39:04 +0200

    lib_BaseRow install from 8.3.X branch now

--- eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/baserow/BaserowView.java
+++ eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/baserow/BaserowView.java
@@ -110,7 +110,7 @@ import com.twinsoft.convertigo.engine.enums.JsonFieldType;
 import com.twinsoft.convertigo.engine.util.ProjectUrlParser;
 
 public class BaserowView extends ViewPart {
-	private final static String LIB_BASEROW_URL = "lib_BaseRow=https://github.com/convertigo/c8oprj-lib-baserow/archive/refs/heads/8.2.x.zip";
+	private final static String LIB_BASEROW_URL = "lib_BaseRow=https://github.com/convertigo/c8oprj-lib-baserow/archive/refs/heads/8.3.X.zip";
 	
 	private Cursor handCursor;
 	private Composite main;
