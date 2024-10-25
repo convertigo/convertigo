16935a91910344808a8ce1bbcc2d27067c0cf109
Nicolas Albert <nicolas.albert@gmail.com>
2024-10-25 11:25:21 +0200

    fix #859 keeps safe operators for JS expressions

--- engine/src/com/twinsoft/convertigo/beans/ngx/components/UICustomAction.java
+++ engine/src/com/twinsoft/convertigo/beans/ngx/components/UICustomAction.java
@@ -427,7 +427,6 @@ public class UICustomAction extends UIComponent implements IAction {
 								smartValue = "\'" + MobileSmartSourceType.escapeStringForTs(smartValue) + "\'";
 							}
 							
-							smartValue = smartValue.replaceAll("\\?\\.\\[", "[").replaceAll("\\?\\.", ".");
 							smartValue = smartValue.replaceAll("this\\.", "c8oPage.");
 							if (paramsPattern.matcher(smartValue).lookingAt()) {
 								smartValue = "scope."+ smartValue;
