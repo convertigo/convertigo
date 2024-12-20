5a697c76580d0e56fc836ed2ec84e66ad890bcc6
Nicolas Albert <nicolas.albert@gmail.com>
2024-12-20 20:08:04 +0100

    ref #791 rename some bezel files

--- convertigo-studio-web/src/lib/dashboard/Bezels.js
+++ convertigo-studio-web/src/lib/dashboard/Bezels.js
@@ -11,24 +11,18 @@ const devices = {
 		bezel: { width: 487, height: 983 },
 		type: 'phone'
 	},
-	'Google-Pixel-8': {
-		title: 'Google Pixel 8',
+	'Google-Pixel-8-Pro': {
+		title: 'Google Pixel 8 Pro',
 		iframe: { width: 412, height: 915, marginTop: 17, marginLeft: 16, borderRadius: 20 },
 		bezel: { width: 452, height: 950 },
 		type: 'phone'
 	},
-	'Desktop-HIDPI': {
-		title: 'Desktop HIPDI',
-		iframe: { width: 1920, height: 1080, marginTop: 63, marginLeft: 63 },
-		bezel: { width: 2046, height: 1361 },
-		type: 'desktop'
-	},
-	none: {
-		title: 'None',
-		iframe: { width: '100%', height: '100%', position: 'relative' },
-		bezel: { width: '100%', height: '100%', position: 'relative' },
-		type: 'desktop'
-	},
+	// 'Desktop-HIDPI': {
+	// 	title: 'Desktop HIPDI',
+	// 	iframe: { width: 1920, height: 1080, marginTop: 63, marginLeft: 63 },
+	// 	bezel: { width: 2046, height: 1361 },
+	// 	type: 'desktop'
+	// },
 	'Galaxy-S24': {
 		title: 'Galaxy S24',
 		iframe: { width: 360, height: 780, marginTop: 13, marginLeft: 13, borderRadius: 20 },
@@ -41,19 +35,19 @@ const devices = {
 		bezel: { width: 415, height: 858 },
 		type: 'phone'
 	},
-	'Galaxy-S23-Ultra': {
-		title: 'Galaxy S23 Ultra',
+	'Galaxy-S24-Ultra': {
+		title: 'Galaxy S24 Ultra',
 		iframe: { width: 384, height: 824, marginTop: 13, marginLeft: 13, borderRadius: 5 },
 		bezel: { width: 408, height: 846 },
 		type: 'phone'
 	},
-	'iPad-Pro-11-(2021)': {
+	'iPad-Pro-11-2021': {
 		title: 'iPad Pro 11 (2021)',
 		iframe: { width: 834, height: 1194, marginTop: 46, marginLeft: 46, borderRadius: 20 },
 		bezel: { width: 925, height: 1283 },
 		type: 'phone'
 	},
-	'iPad-Pro-12-(2021)': {
+	'iPad-Pro-12-2021': {
 		title: 'iPad Pro-12 (2021))',
 		iframe: { width: 1024, height: 1366, marginTop: 46, marginLeft: 46, borderRadius: 20 },
 		bezel: { width: 1120, height: 1460 },
@@ -76,7 +70,13 @@ const devices = {
 		iframe: { width: 375, height: 667, marginTop: 120, marginLeft: 21, borderRadius: 5 },
 		bezel: { width: 443, height: 912 },
 		type: 'phone'
-	}
+	},
+	none: {
+		title: 'None',
+		iframe: { width: '100%', height: '100%', position: 'relative' },
+		bezel: { width: '100%', height: '100%', position: 'relative' },
+		type: 'desktop'
+	},
 };
 
 let index = 0;
