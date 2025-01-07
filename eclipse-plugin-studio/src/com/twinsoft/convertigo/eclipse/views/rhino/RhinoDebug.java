33741c0e52ec9eaa0244c4a2489efaa5eabdc919
Nicolas Albert <nicolas.albert@gmail.com>
2025-01-07 18:48:01 +0100

    update Copyright for 2025

--- eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/rhino/RhinoDebug.java
+++ eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/views/rhino/RhinoDebug.java
@@ -1,8 +1,22 @@
-/* -*- Mode: java; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*-
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+/*
+ * Copyright (c) 2001-2025 Convertigo SA.
+ * 
+ * This program  is free software; you  can redistribute it and/or
+ * Modify  it  under the  terms of the  GNU  Affero General Public
+ * License  as published by  the Free Software Foundation;  either
+ * version  3  of  the  License,  or  (at your option)  any  later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ * MERCHANTABILITY  or  FITNESS  FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program;
+ * if not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.twinsoft.convertigo.eclipse.views.rhino;
 
 import java.awt.AWTEvent;
