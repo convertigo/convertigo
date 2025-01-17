2fdb38709c5ce7bedeb424d8375fcf64ef46d2de
Nicolas Albert <nicolas.albert@gmail.com>
2025-01-17 15:01:29 +0100

    ref #842 fix built filename in admin + upgrade gradle and dependencies

--- installers/nsis/download-jdk.sh
+++ installers/nsis/download-jdk.sh
@@ -3,26 +3,26 @@
 
 mkdir -p /tmp/c8o_jre
 cd /tmp/c8o_jre
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.4%2B7/OpenJDK21U-jdk_x64_windows_hotspot_21.0.4_7.zip
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_windows_hotspot_21.0.5_11.zip
 curl -sfSL -o jdk.zip ${JDK_URL}
 unzip -q jdk.zip
 rm jdk.zip
 mv jdk* win
 rm -rf win/jmods win/lib/src.zip
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.4%2B7/OpenJDK21U-jdk_x64_mac_hotspot_21.0.4_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_mac_hotspot_21.0.5_11.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac
 rm -rf jdk* mac/lib/src.zip mac/man mac/jmods
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.4%2B7/OpenJDK21U-jdk_aarch64_mac_hotspot_21.0.4_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_aarch64_mac_hotspot_21.0.5_11.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac-arm
 rm -rf jdk* mac-arm/lib/src.zip mac-arm/man mac-arm/jmods
 
-JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.4%2B7/OpenJDK21U-jdk_x64_linux_hotspot_21.0.4_7.tar.gz
+JDK_URL=https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.5%2B11/OpenJDK21U-jdk_x64_linux_hotspot_21.0.5_11.tar.gz
 curl -sfSL -o jdk.tar.gz ${JDK_URL}
 tar -xf jdk.tar.gz
 rm jdk.tar.gz
