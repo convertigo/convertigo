ddaf262924e3167de9a7f4b54359a21113fe5b8f
Nicolas Albert <nicolas.albert@gmail.com>
2025-04-25 17:03:50 +0200

    Prevents exception if no keys.

--- engine/src/com/twinsoft/convertigo/engine/admin/services/keys/List.java
+++ engine/src/com/twinsoft/convertigo/engine/admin/services/keys/List.java
@@ -51,124 +51,127 @@ import com.twinsoft.tas.KeyManager;
 		parameters = {},
 		returnValue = "",
 		cloud_forbidden = true
-	)
+		)
 public class List extends XmlService{
 
 	protected void getServiceResult(HttpServletRequest request, Document document) throws Exception {
 		Element rootElement = document.getDocumentElement();
-        
-        String tasRoot = EnginePropertiesManager.getProperty(PropertyName.CARIOCA_URL);
-        
-        KeyManager.init(tasRoot);
-        
-    	java.util.List<Key> keys = new ArrayList<Key>(GenericUtils.<Collection<Key>>cast(KeyManager.keys.values()));
-    	Collections.sort(keys, new Comparator<Key>() {
+
+		String tasRoot = EnginePropertiesManager.getProperty(PropertyName.CARIOCA_URL);
+
+		KeyManager.init(tasRoot);
+
+		java.util.List<Key> keys = new ArrayList<Key>(GenericUtils.<Collection<Key>>cast(KeyManager.keys.values()));
+		Collections.sort(keys, new Comparator<Key>() {
 			public int compare(Key o1, Key o2) {
 				return o1.getQuickSortValue().toString().compareTo(o2.getQuickSortValue().toString());
 			}
 		});
-    	
-    	Iterator<Key> iKey = keys.iterator();
-    	int nbValidKey = 0;
-    	if (iKey.hasNext()) {
-        	Key key = iKey.next();
-        	while (key != null) {
-            	int total = 0;
-        		int emulatorID = key.emulatorID;
-        		String emulatorName = KeyManager.getEmulatorName(emulatorID);
 
-        		// Count number of valid keys
-        		nbValidKey += KeyManager.hasExpired(emulatorID) ? 0 : (key.bDemo ? 0 : 1);
+		Iterator<Key> iKey = keys.iterator();
+		int nbValidKey = 0;
+		if (iKey.hasNext()) {
+			Key key = iKey.next();
+			while (key != null) {
+				int total = 0;
+				int emulatorID = key.emulatorID;
+				String emulatorName = KeyManager.getEmulatorName(emulatorID);
 
-        		Element keysElement = document.createElement("keys");
+				// Count number of valid keys
+				nbValidKey += KeyManager.hasExpired(emulatorID) ? 0 : (key.bDemo ? 0 : 1);
 
-        		do {
-       				total += key.cv;
-        			
-        			Element keyElement = document.createElement("key");
-        			keyElement.setAttribute("text", key.sKey);
-        			keyElement.setAttribute("value", Integer.toString(key.cv));
-        			keyElement.setAttribute("evaluation", key.bDemo ? "true" : "false");
-        			
-        			/**
-        			 * for demo keys, adjust expiration date to milliEvalDate (startDate + 30 days, see KeyManager)
-        			 */
-        			String expiration;
-        			
-        			if (key.bDemo)
-        				keyElement.setAttribute("expiration", expiration = ""+(KeyManager.getFirstStartDate()/(1000*3600*24)));
-        			else
-        				keyElement.setAttribute("expiration", expiration = Integer.toString(key.expiration));
+				Element keysElement = document.createElement("keys");
 
-        			if (emulatorID == Session.EmulIDSE) {
-            			if ((Integer.parseInt(expiration) != 0) && (System.currentTimeMillis()/(1000*3600*24) > Integer.parseInt(expiration)))
-            				keyElement.setAttribute("expired", "true");
-            			else
-            				keyElement.setAttribute("expired", "false");
-        			} else {
-        				keyElement.setAttribute("expired", key.cv == 0 ? "true" : "false");
-        			}
+				do {
+					total += key.cv;
 
-        			NodeList keyList =  keysElement.getChildNodes();
+					Element keyElement = document.createElement("key");
+					keyElement.setAttribute("text", key.sKey);
+					keyElement.setAttribute("value", Integer.toString(key.cv));
+					keyElement.setAttribute("evaluation", key.bDemo ? "true" : "false");
 
-        			if (keyList.getLength() == 0)
-        				keysElement.appendChild(keyElement);
-        			else {
-	        			for(int i=0; i<keyList.getLength(); i++) {
-	        				Element el = ((Element)keyList.item(i));
-	        				expiration = el.getAttribute("expiration");
-	        				
-	        				if (expiration.equals("0") || expiration.compareTo(Integer.toString(key.expiration)) > 0) {
-	        					keysElement.insertBefore(keyElement,  (Element)keyList.item(i));
-	        					break;
-	        				}
-	        				
-	        				if (i == keyList.getLength()-1)
-	        					keysElement.appendChild(keyElement);
-	        			}
-        			}
-        				
-        			key = iKey.hasNext() ? iKey.next() : null;
-        		} while (key != null && emulatorID == key.emulatorID);
-        		
-        		/*
-        		 * parse final licence array to adjust sessions keys where the latest one overrides the previous
-        		 */
-    			NodeList keyList =  keysElement.getChildNodes();
-        		if ((emulatorID == Session.EmulIDSE) && (keyList.getLength() > 0)) { 
-        			// if Session CV still 0, default to maxCVValue from KeyManager, 
-        			// session key CV cannot be less than 10
-        			// and remaining-used will not appear negative
-        			if (total == 0) 
-        				total = KeyManager.getMaxCV(Session.EmulIDSE);
-        		}
-        		
-        		Element emulatorNameElement = document.createElement("category");
-       			emulatorNameElement.setAttribute("overflow", KeyManager.isOverflow(emulatorID) ? "true" : "false");
-        		emulatorNameElement.setAttribute("name", emulatorName);
-        		emulatorNameElement.setAttribute("remaining", Integer.toString(KeyManager.getCV(emulatorID)));
-        		emulatorNameElement.setAttribute("total", Integer.toString(total));
-        		
-        		emulatorNameElement.appendChild(keysElement);
-        		rootElement.appendChild(emulatorNameElement);
-        	}    		
-    	}
-    	
-    	var xpath = JXPathContext.newContext(rootElement);
-    	for (var name: Arrays.asList("Convertigo SDK", "Mobile Builder", "Standard Edition")) {
-	    	var cat = (Node) xpath.selectSingleNode("category[@name='" + name + "']");
-	    	if (cat != null && rootElement.getFirstChild() != cat) {
-	    		rootElement.insertBefore(cat, rootElement.getFirstChild());
-	    	}
-    	}
-    	
-    	//We add the number of valid into the XML response
-    	Element countValidKey = document.createElement("nb_valid_key");
-    	countValidKey.setTextContent(nbValidKey + "");
+					/**
+					 * for demo keys, adjust expiration date to milliEvalDate (startDate + 30 days, see KeyManager)
+					 */
+					String expiration;
+
+					if (key.bDemo)
+						keyElement.setAttribute("expiration", expiration = ""+(KeyManager.getFirstStartDate()/(1000*3600*24)));
+					else
+						keyElement.setAttribute("expiration", expiration = Integer.toString(key.expiration));
+
+					if (emulatorID == Session.EmulIDSE) {
+						if ((Integer.parseInt(expiration) != 0) && (System.currentTimeMillis()/(1000*3600*24) > Integer.parseInt(expiration)))
+							keyElement.setAttribute("expired", "true");
+						else
+							keyElement.setAttribute("expired", "false");
+					} else {
+						keyElement.setAttribute("expired", key.cv == 0 ? "true" : "false");
+					}
+
+					NodeList keyList =  keysElement.getChildNodes();
+
+					if (keyList.getLength() == 0)
+						keysElement.appendChild(keyElement);
+					else {
+						for(int i=0; i<keyList.getLength(); i++) {
+							Element el = ((Element)keyList.item(i));
+							expiration = el.getAttribute("expiration");
+
+							if (expiration.equals("0") || expiration.compareTo(Integer.toString(key.expiration)) > 0) {
+								keysElement.insertBefore(keyElement,  (Element)keyList.item(i));
+								break;
+							}
+
+							if (i == keyList.getLength()-1)
+								keysElement.appendChild(keyElement);
+						}
+					}
+
+					key = iKey.hasNext() ? iKey.next() : null;
+				} while (key != null && emulatorID == key.emulatorID);
+
+				/*
+				 * parse final licence array to adjust sessions keys where the latest one overrides the previous
+				 */
+				NodeList keyList =  keysElement.getChildNodes();
+				if ((emulatorID == Session.EmulIDSE) && (keyList.getLength() > 0)) { 
+					// if Session CV still 0, default to maxCVValue from KeyManager, 
+					// session key CV cannot be less than 10
+					// and remaining-used will not appear negative
+					if (total == 0) 
+						total = KeyManager.getMaxCV(Session.EmulIDSE);
+				}
+
+				Element emulatorNameElement = document.createElement("category");
+				emulatorNameElement.setAttribute("overflow", KeyManager.isOverflow(emulatorID) ? "true" : "false");
+				emulatorNameElement.setAttribute("name", emulatorName);
+				emulatorNameElement.setAttribute("remaining", Integer.toString(KeyManager.getCV(emulatorID)));
+				emulatorNameElement.setAttribute("total", Integer.toString(total));
+
+				emulatorNameElement.appendChild(keysElement);
+				rootElement.appendChild(emulatorNameElement);
+			}
+		}
+
+		var xpath = JXPathContext.newContext(rootElement);
+		for (var name: Arrays.asList("Convertigo SDK", "Mobile Builder", "Standard Edition")) {
+			try {
+				var cat = (Node) xpath.selectSingleNode("category[@name='" + name + "']");
+				if (cat != null && rootElement.getFirstChild() != cat) {
+					rootElement.insertBefore(cat, rootElement.getFirstChild());
+				}
+			} catch (Exception e) {
+			}
+		}
+
+		//We add the number of valid into the XML response
+		Element countValidKey = document.createElement("nb_valid_key");
+		countValidKey.setTextContent(nbValidKey + "");
 		rootElement.appendChild(countValidKey);
-		
-    	Element firstStartDate = document.createElement("firstStartDate");
-    	firstStartDate.setTextContent(KeyManager.getFirstStartDate() + "");
+
+		Element firstStartDate = document.createElement("firstStartDate");
+		firstStartDate.setTextContent(KeyManager.getFirstStartDate() + "");
 		rootElement.appendChild(firstStartDate);
 	}
 }
