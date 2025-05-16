fa2408d27b572bfe7cf20bbe2692f480bfcbd5c5
nathaliel <nathaliel@convertigo.com>
2025-05-16 18:22:37 +0200

    ref #948 - handle 'edit' message : replace component's children with clipboard data

--- engine/src/com/twinsoft/convertigo/engine/util/Clipboard.java
+++ engine/src/com/twinsoft/convertigo/engine/util/Clipboard.java
@@ -19,12 +19,21 @@
 
 package com.twinsoft.convertigo.engine.util;
 
+import java.io.IOException;
+import java.io.StringReader;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.w3c.dom.ProcessingInstruction;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import com.twinsoft.convertigo.beans.core.BlockFactory;
 import com.twinsoft.convertigo.beans.core.Criteria;
@@ -61,6 +70,10 @@ public class Clipboard {
 		clipboardDocument.appendChild(clipboardRootElement);
 	}
 	
+	public boolean isEmpty() {
+		return dboList.isEmpty();
+	}
+	
 	public void reset() {
 		dboList.clear();
 		init();
@@ -80,6 +93,36 @@ public class Clipboard {
 		return XMLUtils.prettyPrintDOM(clipboardDocument);
 	}
 
+	public List<Object> fromXml(String xmlData) throws SAXException, IOException {
+		List<Object> objectList = new ArrayList<Object>();
+		if (xmlData != null) {
+			DocumentBuilder builder = XMLUtils.getDefaultDocumentBuilder();
+			builder.setErrorHandler(null); // avoid 'content not allowed in prolog' to be printed out
+			
+			Document document = builder.parse(new InputSource(new StringReader(xmlData)));
+			
+			Element rootElement = document.getDocumentElement();
+			NodeList nodeList = rootElement.getChildNodes();
+			int len = nodeList.getLength();
+			Object object;
+			Node node;
+			for (int i = 0 ; i < len ; i++) {
+				node = (Node) nodeList.item(i);
+				if (node.getNodeType() != Node.TEXT_NODE) {
+					try {
+						object = from(node, null);
+						if (object != null) {
+							objectList.add(object);
+						}
+					} catch (EngineException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return objectList;
+	}
+	
 	private void copyDatabaseObject(DatabaseObject databaseObject) throws EngineException {
 		currentScreenClassOrTransaction = null;
 		
@@ -180,6 +223,57 @@ public class Clipboard {
 			}
 		} catch (Exception ex) {}
 		element.appendChild(dnd);
+	}
+	
+	private Object read(Node node) throws EngineException {
+		Class<?> objectClass = null;
+		Object object = null;
+		Element element = (Element) node;
+		String objectClassName = element.getAttribute("classname");
+		try {
+			objectClass = Class.forName(objectClassName);
+			Method readMethod = objectClass.getMethod("read", new Class[] { Node.class});
+			object = readMethod.invoke(null, new Object[] { node } );
+		} catch(Exception e) {
+			throw new EngineException("Unable to read object", e);
+		}
+		return object;
+	}
+	
+	private Object from(Node node, DatabaseObject parentDatabaseObject) throws EngineException {
+		Object object = read(node);
+		if (object instanceof DatabaseObject) {
+			DatabaseObject databaseObject = (DatabaseObject) object;
+			if (parentDatabaseObject != null) {
+				parentDatabaseObject.add(databaseObject);
+			}
+			
+			NodeList childNodes = node.getChildNodes();
+			int len = childNodes.getLength();
+			
+			Node childNode;
+			String childNodeName;
+			
+			for (int i = 0 ; i < len ; i++) {
+				childNode = childNodes.item(i);
+				if (childNode.getNodeType() != Node.ELEMENT_NODE) {
+					continue;
+				}
+				childNodeName = childNode.getNodeName();
+				if (!(childNodeName.equalsIgnoreCase("property")) && 
+						!(childNodeName.equalsIgnoreCase("handlers")) &&
+						!(childNodeName.equalsIgnoreCase("wsdltype")) &&
+						!(childNodeName.equalsIgnoreCase("docdata")) &&
+						!(childNodeName.equalsIgnoreCase("dnd"))) {
+					from(childNode, databaseObject);
+				}
+			}
+			
+			databaseObject.isImporting = false; // needed
+			databaseObject.isSubLoaded = true;
+			return databaseObject;
+		}
+		return null;
 	}
 	
 }
