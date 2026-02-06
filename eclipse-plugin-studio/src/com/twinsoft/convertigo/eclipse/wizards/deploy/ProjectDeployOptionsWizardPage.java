fb413221e52ff638de6e547d1e4e95c79406fb71
SAILESH4406 <c.n.s.s.sailesh@gmail.com>
2026-02-06 16:39:53 +0530

    Fix: remember last used deployment URL in Deploy wizard

--- eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/wizards/deploy/ProjectDeployOptionsWizardPage.java
+++ eclipse-plugin-studio/src/com/twinsoft/convertigo/eclipse/wizards/deploy/ProjectDeployOptionsWizardPage.java
@@ -21,66 +21,75 @@ package com.twinsoft.convertigo.eclipse.wizards.deploy;
 
 import java.util.HashMap;
 
+import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
+import com.twinsoft.convertigo.eclipse.ConvertigoPlugin;
+
 class ProjectDeployOptionsWizardPage extends WizardPage {
+	private static final String PREF_LAST_DEPLOY_URL = "lastDeployUrl";
+
 	private HashMap<String, Object> data = null;
-	
 	private ProjectDeployOptionsComposite composite = null;
-	
+
 	ProjectDeployOptionsWizardPage() {
 		super("ProjectDeployOptionsWizardPage", "Deployment options", null);
 		this.data = new HashMap<String, Object>();
 		this.setPageComplete(false);
 	}
-	
+
 	@Override
 	public void createControl(Composite parent) {
 		composite = new ProjectDeployOptionsComposite(this, parent, SWT.NONE);
 		composite.setOkButton(null);
 		setControl(composite);
+
+		// Restore last used deployment URL
+		IPreferenceStore store = ConvertigoPlugin.getDefault().getPreferenceStore();
+		String lastUrl = store.getString(PREF_LAST_DEPLOY_URL);
+		if (lastUrl != null && !lastUrl.isEmpty()) {
+			composite.convertigoServer.setText(lastUrl);
+		}
 	}
 
 	private HashMap<String, Object> getData() {
 		if (composite != null) {
 			String convertigoServer = composite.convertigoServer.getText();
-			//if ((convertigoServer == null) || (convertigoServer.equals(""))) return;
-			
 			String convertigoUserName = composite.convertigoAdmin.getText();
-			if (convertigoUserName == null) convertigoUserName = "";
-	
 			String convertigoUserPassword = composite.convertigoPassword.getText();
+
+			if (convertigoUserName == null) convertigoUserName = "";
 			if (convertigoUserPassword == null) convertigoUserPassword = "";
-			
+
 			boolean isHttps = composite.checkBox.getSelection();
 			boolean trustAllCertificates = composite.checkTrustAllCertificates.getSelection();
 			boolean bAssembleXsl = composite.assembleXsl.getSelection();
-			
+
 			data.put("convertigoServer", convertigoServer);
 			data.put("convertigoUserName", convertigoUserName);
 			data.put("convertigoUserPassword", convertigoUserPassword);
 			data.put("isHttps", Boolean.valueOf(isHttps));
 			data.put("trustAllCertificates", Boolean.valueOf(trustAllCertificates));
 			data.put("bAssembleXsl", Boolean.valueOf(bAssembleXsl));
+
+			// Save last used deployment URL
+			IPreferenceStore store = ConvertigoPlugin.getDefault().getPreferenceStore();
+			store.setValue(PREF_LAST_DEPLOY_URL, convertigoServer);
 		}
 		return data;
 	}
-	
-	@Override
-	public void setPageComplete(boolean complete) {
-		super.setPageComplete(complete);
-	}
-	
+
 	@Override
 	public void setVisible(boolean visible) {
 		super.setVisible(visible);
 		if (!visible && isControlCreated() && !isCurrentPage()) {
 			try {
 				if (getContainer().getCurrentPage().equals(getWizard().getNextPage(this))) {
-					((ProjectDeployResultWizardPage)getWizard().getNextPage(this)).setOptionsData(getData());
+					((ProjectDeployResultWizardPage) getWizard().getNextPage(this))
+							.setOptionsData(getData());
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -101,5 +110,6 @@ class ProjectDeployOptionsWizardPage extends WizardPage {
 	@Override
 	public boolean isPageComplete() {
 		return super.isPageComplete();
-	}	
+	}
 }
+
