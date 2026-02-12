f118207bd08d23dcf41a83bd50a6235753d2f69a
Nicolas Albert <nicolas.albert@gmail.com>
2026-02-12 14:57:23 +0100

    ref #842 enhance upgrade script + handle jdk update

--- installers/nsis/download-jdk.sh
+++ installers/nsis/download-jdk.sh
@@ -1,29 +1,42 @@
 #!/bin/sh
-# see https://adoptopenjdk.net
+# see https://adoptium.net
+
+set -eu
+
+# Keep this single value up to date with the bundled Studio JDK release.
+JDK_RELEASE=25.0.2+10
+JDK_VERSION=${JDK_RELEASE%%+*}
+JDK_BUILD=${JDK_RELEASE#*+}
+JDK_MAJOR=${JDK_VERSION%%.*}
+JDK_FILE_VERSION=${JDK_VERSION}_${JDK_BUILD}
+JDK_BASE_URL=https://github.com/adoptium/temurin${JDK_MAJOR}-binaries/releases/download/jdk-${JDK_RELEASE}
+JDK_FILE_PREFIX=OpenJDK${JDK_MAJOR}U-jdk
+
+JDK_URL_WIN=${JDK_BASE_URL}/${JDK_FILE_PREFIX}_x64_windows_hotspot_${JDK_FILE_VERSION}.zip
+JDK_URL_MAC=${JDK_BASE_URL}/${JDK_FILE_PREFIX}_x64_mac_hotspot_${JDK_FILE_VERSION}.tar.gz
+JDK_URL_MAC_ARM=${JDK_BASE_URL}/${JDK_FILE_PREFIX}_aarch64_mac_hotspot_${JDK_FILE_VERSION}.tar.gz
+JDK_URL_LINUX=${JDK_BASE_URL}/${JDK_FILE_PREFIX}_x64_linux_hotspot_${JDK_FILE_VERSION}.tar.gz
 
 mkdir -p /tmp/c8o_jre
 cd /tmp/c8o_jre
-JDK_URL=https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_x64_windows_hotspot_25.0.2_10.zip
-curl -sfSL -o jdk.zip ${JDK_URL}
+
+curl -sfSL -o jdk.zip "${JDK_URL_WIN}"
 unzip -q jdk.zip
 rm jdk.zip
 mv jdk* win
 rm -rf win/jmods win/lib/src.zip
 
-JDK_URL=https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_x64_mac_hotspot_25.0.2_10.tar.gz
-curl -sfSL -o jdk.tar.gz ${JDK_URL}
+curl -sfSL -o jdk.tar.gz "${JDK_URL_MAC}"
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac
 rm -rf jdk* mac/lib/src.zip mac/man mac/jmods
 
-JDK_URL=https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_aarch64_mac_hotspot_25.0.2_10.tar.gz
-curl -sfSL -o jdk.tar.gz ${JDK_URL}
+curl -sfSL -o jdk.tar.gz "${JDK_URL_MAC_ARM}"
 tar -xf jdk.tar.gz
 mv jdk*/Contents/Home mac-arm
 rm -rf jdk* mac-arm/lib/src.zip mac-arm/man mac-arm/jmods
 
-JDK_URL=https://github.com/adoptium/temurin25-binaries/releases/download/jdk-25.0.2%2B10/OpenJDK25U-jdk_x64_linux_hotspot_25.0.2_10.tar.gz
-curl -sfSL -o jdk.tar.gz ${JDK_URL}
+curl -sfSL -o jdk.tar.gz "${JDK_URL_LINUX}"
 tar -xf jdk.tar.gz
 rm jdk.tar.gz
 mv jdk* lin
