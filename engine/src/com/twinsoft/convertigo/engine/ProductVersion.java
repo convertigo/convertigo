2a7d922ff4d2ed0076e064d32f02306ac5e20dc3
Nicolas Albert <nicolas.albert@gmail.com>
2026-09-25 18:35:43 +0200

    Starting Convertigo 8.4.6 beta!

--- engine/src/com/twinsoft/convertigo/engine/ProductVersion.java
+++ engine/src/com/twinsoft/convertigo/engine/ProductVersion.java
@@ -24,7 +24,7 @@ public class ProductVersion {
 
     public static String majorProductVersion = "8";
     public static String minorProductVersion = "4";
-    public static String servicePack = "5";
+    public static String servicePack = "6";
     public static String tag = "beta";
     public static String revision = null;
     
